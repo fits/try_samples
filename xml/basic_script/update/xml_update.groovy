bd2867e9e862e2c30c5c640af4e2d86631b900eb
fits <wadays@.(none)>
2010-06-05 13:16:12 +0900

    要素の削除・更新処理を追加

--- xml/basic_script/update/xml_update.groovy
+++ xml/basic_script/update/xml_update.groovy
@@ -10,9 +10,36 @@ doc.appendNode {
 	}
 }
 
+//属性の追加 <data id="2"> に type="node" を追加
+doc.data.find {it.@id == "2"}.@type = "node"
+//以下でも可
+doc.data[1].@type = "node"
+
+//以下は不可。appendNode した要素に find や配列でアクセスできない模様
+//doc.data.find {it.@id == "3"}.@type = "node1"
+//doc.data[2].@type = "node2"
 
 
-//文字列でXMLを取得して出力
+//要素の削除 <data id="1">・・・</data> を削除
+doc.data.find {it.@id == "1"}.replaceNode {}
+
+
+//要素の変更
+doc.data.find {it.@id == "2"}.children()[0].replaceNode {
+	text("update test")
+}
+//以下でも可
+/*
+doc.data[1].details[0].replaceNode {
+	text("update test")
+}
+*/
+
+//属性の変更 <data id="2" ext="none"> を ext="updated" に変更
+doc.data.find {it.@id == "2"}.@ext = "updated"
+
+
+//文字列化して出力
 println new StreamingMarkupBuilder().bind{
 	mkp.yield doc
 }
