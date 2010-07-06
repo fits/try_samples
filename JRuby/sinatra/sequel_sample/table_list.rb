09836e316a67b742536438a55cf9bbba1c161edf
fits <wadays@.(none)>
2010-07-06 16:43:26 +0900

    Sinatra + Sequel + ExtJS Sample

---
+++ JRuby/sinatra/sequel_sample/table_list.rb
@@ -0,0 +1,19 @@
+require "rubygems"
+require "sinatra"
+require "sequel"
+require "mysql-connector-java-5.1.6-bin.jar"
+require "active_support"
+
+DB = Sequel.connect("jdbc:mysql://localhost/information_schema?user=root")
+
+#DBリスト取得
+get '/databases' do
+	DB["SELECT DISTINCT table_schema FROM tables ORDER BY table_schema"].to_json
+end
+
+#指定DBのテーブルリスト取得
+get '/tables/:table_schema' do |t|
+	sql = "SELECT table_name, table_type, engine, avg_row_length, create_time FROM tables WHERE table_schema=?"
+	DB[sql, t].to_json
+end
+
