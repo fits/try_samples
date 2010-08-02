540b3f4b29d4f4a9f2c7e7de8fc8f445cbc0915d
fits <wadays@.(none)>
2010-08-02 16:43:44 +0900

    neo4j sample code for blog 2010/08/02

---
+++ blog/20100802/neo4j/local_add_node.groovy
@@ -0,0 +1,46 @@
+
+import org.neo4j.graphdb.DynamicRelationshipType
+import org.neo4j.kernel.EmbeddedGraphDatabase
+
+def db = new EmbeddedGraphDatabase("local-sample")
+
+def tx = db.beginTx()
+
+try {
+	def know = DynamicRelationshipType.withName("knows")
+
+	def n1 = db.createNode()
+	n1.setProperty("name", "tester1")
+
+	println("tester1 id = ${n1.id}")
+
+	n11 = db.createNode()
+	n11.setProperty("name", "tester1-1")
+	n1.createRelationshipTo(n11, know)
+
+	n111 = db.createNode()
+	n111.setProperty("name", "tester1-1-1")
+	n11.createRelationshipTo(n111, know)
+
+	n1111 = db.createNode()
+	n1111.setProperty("name", "tester1-1-1-1")
+	n111.createRelationshipTo(n1111, know)
+
+	n1112 = db.createNode()
+	n1112.setProperty("name", "tester1-1-1-2")
+	n111.createRelationshipTo(n1112, know)
+
+	n12 = db.createNode()
+	n12.setProperty("a", "tester1-2")
+	n1.createRelationshipTo(n12, know)
+
+	tx.success()
+} catch (ex) {
+	ex.printStackTrace()
+} finally {
+	tx.finish()
+}
+
+println("shutdown db ...")
+db.shutdown()
+
