ca0cf2fa6c2ba05885ff0412ad1b89676ddb2c1f
fits <wadays@.(none)>
2010-08-04 19:53:44 +0900

    try-catch の削除

--- noSQL/neo4j/sample1/find_node.groovy
+++ noSQL/neo4j/sample1/find_node.groovy
@@ -6,8 +6,6 @@ def id = (args.length > 0)? args[0].toInteger(): 1
 
 def db = new EmbeddedGraphDatabase("sample-store1")
 
-def tx = db.beginTx()
-
 //Node の name プロパティの値を出力
 def printName = {n -> 
 	if (n.hasProperty("name")) {
@@ -18,47 +16,42 @@ def printName = {n ->
 	}
 }
 
-try {
-	//id=1 の Node 取得
-	def n = db.getNodeById(id)
+//id=1 の Node 取得
+def n = db.getNodeById(id)
 
-	//id=1 の Node の名前出力
-	printName(n)
+//id=1 の Node の名前出力
+printName(n)
 
-	//Node の全Relationship取得
-	n.relationships.each {
-		//End Node の名前出力
-		printName(it.endNode)
-	}
+//Node の全Relationship取得
+n.relationships.each {
+	//End Node の名前出力
+	printName(it.endNode)
+}
 
-	println "--------------------"
+println "--------------------"
 
-	def know = DynamicRelationshipType.withName("knows")
+def know = DynamicRelationshipType.withName("knows")
 
-	//2層目で探索を停止
-	def s = {pos -> pos.depth() == 2} as StopEvaluator
-	//name プロパティを持つNodeのみ返す。
-	def r = {pos -> pos.currentNode().hasProperty("name")} as ReturnableEvaluator
+//2層目で探索を停止
+def s = {pos -> pos.depth() == 2} as StopEvaluator
+//name プロパティを持つNodeのみ返す。
+def r = {pos -> pos.currentNode().hasProperty("name")} as ReturnableEvaluator
 
-	//Relationship=know で繋がっている Node を 2層目まで探索
-	//（Relationship の向きはどちらでもよい）
-	def t = n.traverse(Traverser.Order.BREADTH_FIRST, s, r, know, Direction.BOTH)
+//Relationship=know で繋がっている Node を 2層目まで探索
+//（Relationship の向きはどちらでもよい）
+def t = n.traverse(Traverser.Order.BREADTH_FIRST, s, r, know, Direction.BOTH)
 
-	t.each {
-		printName(it)
-	}
+t.each {
+	printName(it)
+}
 
-	println "---------------"
+println "---------------"
 
-	//全 Node 探索
-	def all = n.traverse(Traverser.Order.BREADTH_FIRST, StopEvaluator.END_OF_GRAPH, ReturnableEvaluator.ALL, know, Direction.BOTH)
+//全 Node 探索
+def all = n.traverse(Traverser.Order.BREADTH_FIRST, StopEvaluator.END_OF_GRAPH, ReturnableEvaluator.ALL, know, Direction.BOTH)
 
-	all.each {
-		printName(it)
-	}
-
-} finally {
-	tx.finish()
+all.each {
+	printName(it)
 }
 
 println("shutdown db ...")
