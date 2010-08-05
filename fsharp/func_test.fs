71caa2694e887b57ab7cc78bddf5861ca2d5567a
fits <wadays@.(none)>
2010-08-05 21:58:30 +0900

    再帰関数の例を追加

--- fsharp/func_test.fs
+++ fsharp/func_test.fs
@@ -1,7 +1,12 @@
-let apply (f: int -> int -> int) x y = f x y
-
 let plus x y = x + y
+printfn "10 + 15 = %i" (plus 10 15)
 
-printfn "10 + 15 = %i" (apply plus 10 15)
-
+let apply (f: int -> int -> int) x y = f x y
+//ƒ‰ƒ€ƒ_®
 printfn "100 * 2 = %i" (apply (fun x y -> x * y) 100 2)
+
+//Ä‹AŠÖ”
+let rec fact n = if n = 1 then 1 else n * fact(n - 1)
+printfn "5! = %i" (fact 5)
+
+
