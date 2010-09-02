a925aae93c427c8c746b6a1a192ede60dbf0f83f
fits <wadays_wozx@nifty.com>
2010-09-02 18:18:26 +0900

    簡単なパラメータ処理を実装

--- php/limonade/hello/index.php
+++ php/limonade/hello/index.php
@@ -1,8 +1,20 @@
 <?php
 require_once dirname(__FILE__) . '/lib/limonade.php';
 
+function configure() {
+	option('env', ENV_DEVELOPMENT);
+	option('debug', true);
+}
+
 dispatch('/', function() {
-	return "hello world";
+	return "hello";
+});
+
+dispatch('/:name/:value', function() {
+	$name = params("name");
+	$value = params("value");
+
+	return "sample page name: $name, value: $value";
 });
 
 run();
