e8e9f319f90e7da73436efe509539b19daa6364a
fits <wadays_wozx@nifty.com>
2010-09-13 16:30:23 +0900

    sbt-eclipsify plugin の設定をプロジェクトに追加

--- android/sbt/listSample/project/build/ListSampleProject.scala
+++ android/sbt/listSample/project/build/ListSampleProject.scala
@@ -1,6 +1,7 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class ListSampleProject(info: ProjectInfo) extends AndroidProject(info) {
+class ListSampleProject(info: ProjectInfo) extends AndroidProject(info) with Eclipsify {
 
 	override def androidPlatformName = "android-8"
 
--- android/sbt/listSample/project/plugins/Plugins.scala
+++ android/sbt/listSample/project/plugins/Plugins.scala
@@ -2,4 +2,6 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
 	val android = "org.scala-tools.sbt" % "sbt-android-plugin" % "0.5.0"
+	lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.6.0"
+
 }
