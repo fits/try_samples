e01fe0fad6b6b3a0d978c781f82823125213f604
fits <wadays_wozx@nifty.com>
2010-09-15 12:54:09 +0900

    SimpleAdapter を使用するように変更

--- android/sbt/jsonListSample/src/main/scala/DbActivity.scala
+++ android/sbt/jsonListSample/src/main/scala/DbActivity.scala
@@ -3,15 +3,17 @@ package fits.sample
 import android.app.ListActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
 import android.widget.ListView
 import android.widget.Toast;
 
+import scala.collection.JavaConversions._
 import scala.io.Source
 
 import org.json._
 
 class DbActivity extends ListActivity {
+
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 
@@ -32,18 +34,30 @@ class DbActivity extends ListActivity {
 			val url = "http://169.254.118.149:4567/tables/" + db
 			val json = Source.fromURL(url).mkString
 
-			val obj = new JSONArray(json)
+			val jsonList = new JSONArray(json)
 
-			val dbList = for (i <- 0 until obj.length()) 
-				yield obj.optJSONObject(i).getString("table_name")
+			val dbList = for (i <- 0 until jsonList.length()) 
+				yield toMap(jsonList.optJSONObject(i))
 
-			val adapter = new ArrayAdapter(this, R.layout.item, R.id.name, dbList.toArray)
+			val adapter = new SimpleAdapter(this, dbList, R.layout.item, Array("table_name"), Array(R.id.name))
 			setListAdapter(adapter)
 		}
 		catch {
 			case e: Exception => 
 				Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
 		}
+	}
+
+	private def toMap(jsonObj: JSONObject): java.util.Map[String, String] = {
+		val result = new java.util.HashMap[String, String]()
+
+		for (k <- jsonObj.keys) {
+			k match {
+				case key: String => result.put(key, jsonObj.optString(key))
+			}
+		}
+
+		result
 	}
 
 }
