d4d1d468595836eef9fda99f18321840b662279f
fits <wadays_wozx@nifty.com>
2010-09-15 11:12:05 +0900

    指定DBの全テーブルをリスト表示する処理を追加

--- android/sbt/jsonListSample/src/main/scala/DbActivity.scala
+++ android/sbt/jsonListSample/src/main/scala/DbActivity.scala
@@ -1,7 +1,6 @@
 package fits.sample
 
 import android.app.ListActivity
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
@@ -12,20 +11,31 @@ import scala.io.Source
 
 import org.json._
 
-class JsonListActivity extends ListActivity {
+class DbActivity extends ListActivity {
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 
-		setContentView(R.layout.main)
+		setContentView(R.layout.db)
 
+		val extras = getIntent().getExtras()
+
+		if (extras != null) {
+			val db = extras.getCharSequence("DB")
+
+			setTitle(db)
+			loadTables(db)
+		}
+	}
+
+	private def loadTables(db: CharSequence) {
 		try {
-			val url = "http://169.254.118.149:4567/databases"
+			val url = "http://169.254.118.149:4567/tables/" + db
 			val json = Source.fromURL(url).mkString
 
 			val obj = new JSONArray(json)
 
 			val dbList = for (i <- 0 until obj.length()) 
-				yield obj.optJSONObject(i).getString("table_schema")
+				yield obj.optJSONObject(i).getString("table_name")
 
 			val adapter = new ArrayAdapter(this, R.layout.item, R.id.name, dbList.toArray)
 			setListAdapter(adapter)
@@ -36,15 +46,5 @@ class JsonListActivity extends ListActivity {
 		}
 	}
 
-	//リストアイテムをクリックした際の処理
-	override def onListItemClick(l: ListView, v: View, p: Int, id: Long) {
-		//クリックしたアイテムを取得
-		val selectedItem = l.getItemAtPosition(p)
-
-		val intent = new Intent(this, classOf[DbActivity])
-		intent.putExtra("DB", selectedItem.toString())
-
-		startActivity(intent)
-	}
 }
 
