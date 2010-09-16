d2be6cd03e86051038e5574fd16d99698336c279
fits <wadays_wozx@nifty.com>
2010-09-16 21:37:35 +0900

    コメント追加

--- blog/20100916/src/main/java/fits/sample/SingleAsyncTask.java
+++ blog/20100916/src/main/java/fits/sample/SingleAsyncTask.java
@@ -3,12 +3,11 @@ package fits.sample;
 import android.os.AsyncTask;
 
 class SingleAsyncTask<T, U> extends AsyncTask<T, Void, U> {
-
-	@Override
 	protected U doInBackground(T... params) {
 		return (params.length > 0)? doSingleTask(params[0]): null;
 	}
 
+	//Scala のサブクラス側でオーバーライドするためのメソッド
 	protected U doSingleTask(T param) {
 		return null;
 	}
