269758e0495fd274538638adbea6e1da325c080d
fits <wadays_wozx@nifty.com>
2010-09-18 19:42:12 +0900

    Sinatra風のルーティングをASP.NETルーティングで実現しようとするためのサンプル

---
+++ asp.net/web_routing/RoutingExtend/RoutingExtendLib/RoutingExtension.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+
+namespace Fits.Routing
+{
+    public class RoutingExtension
+    {
+        public static void Get(string pattern, Func<RequestContext, string> proc)
+        {
+            Action("GET", pattern, proc);
+        }
+
+        public static void Post(string pattern, Func<RequestContext, string> proc)
+        {
+            Action("POST", pattern, proc);
+        }
+
+        public static void Action(string methodType, string pattern, Func<RequestContext, string> proc)
+        {
+            RouteTable.Routes.Add(new Route(pattern, new ExtensionRouteHandler(methodType, proc)));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private class ExtensionRouteHandler : IRouteHandler
+        {
+            private string methodType;
+            private Func<RequestContext, string> proc;
+
+            public ExtensionRouteHandler(string methodType, Func<RequestContext, string> proc)
+            {
+                this.methodType = methodType;
+                this.proc = proc;
+            }
+
+            public IHttpHandler GetHttpHandler(RequestContext requestContext)
+            {
+                if (requestContext.HttpContext.Request.HttpMethod == this.methodType)
+                {
+                    return new ExtensionHttpHandler(this.proc, requestContext);
+                }
+                else
+                {
+                    return new DefaultHttpHandler();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private class ExtensionHttpHandler : IHttpHandler
+        {
+            private Func<RequestContext, string> proc;
+            private RequestContext reqCtx;
+
+            public ExtensionHttpHandler(Func<RequestContext, string> proc, RequestContext reqCtx)
+            {
+                this.proc = proc;
+                this.reqCtx = reqCtx;
+            }
+
+            public bool IsReusable
+            {
+                get { return true; }
+            }
+
+            public void ProcessRequest(HttpContext context)
+            {
+                string res = this.proc(this.reqCtx);
+
+                if (res != null)
+                {
+                    context.Response.Write(res);
+                }
+            }
+        }
+
+    }
+}
---
+++ asp.net/web_routing/RoutingExtend/SampleWebApp/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+using Fits.Routing;
+
+namespace SampleWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+//            RoutingExtension.Get("test/page", ctx =>
+            RoutingExtension.Post("test/page", ctx =>
+            {
+                ctx.HttpContext.Response.Redirect("/WebForm1.aspx");
+                return null;
+            });
+
+            RoutingExtension.Get("{name}/{index}", ctx =>
+            {
+                var param = ctx.RouteData.Values;
+                return string.Format("hello {0} - {1}", param["name"], param["index"]);
+            });
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
