f72ad1600363a4c1c5b0b17dd3df11434a3d0475
fits <wadays_wozx@nifty.com>
2010-09-22 22:52:23 +0900

    アップキャスト用のメソッド追加

--- fsharp/interface/object_test.fs
+++ fsharp/interface/object_test.fs
@@ -1,10 +1,22 @@
 
+//インターフェース定義
 type ISample = 
     abstract member Print : unit -> unit
 
+//実装オブジェクト定義
 type SampleObj(x: int) = 
+    //インターフェースの実装
     interface ISample with
         member this.Print() = printfn "x = %d" x
+
+//実装オブジェクト定義（アップキャスト不要にするためのメソッド定義版）
+type SampleObj2(x: int) = 
+    //アップキャストを不要にするために実装側でメソッドを用意
+    member this.Print() = (this :> ISample).Print()
+    //インターフェースの実装
+    interface ISample with
+        member this.Print() = printfn "x = %d" x
+
 
 let s1 = new SampleObj(10)
 
@@ -12,6 +24,9 @@ let s1 = new SampleObj(10)
 //インターフェース型にアップキャストして呼び出す必要あり
 (s1 :> ISample).Print()
 
+//オブジェクト側で定義したメソッド呼び出し
+let s2 = new SampleObj2(76)
+s2.Print()
 
 //オブジェクト式を使ってメソッド呼び出し1
 let createSample(x: int) =
