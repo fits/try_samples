5f204a69e04cb42d82f912b9456a0fdf68fece10
fits <wadays_wozx@nifty.com>
2010-09-22 22:44:06 +0900

    オブジェクト式のサンプル追加

--- fsharp/interface/object_test.fs
+++ fsharp/interface/object_test.fs
@@ -7,6 +7,23 @@ type SampleObj(x: int) =
         member this.Print() = printfn "x = %d" x
 
 let s1 = new SampleObj(10)
+
+//インターフェースメソッドを呼び出すには、
+//インターフェース型にアップキャストして呼び出す必要あり
 (s1 :> ISample).Print()
 
+
+//オブジェクト式を使ってメソッド呼び出し1
+let createSample(x: int) =
+    { new ISample with
+        member this.Print() = printfn "create sample %d" x
+    }
+
+createSample(50).Print()
+
+
+//オブジェクト式を使ってメソッド呼び出し2
+{new ISample with
+    member this.Print() = printfn "testdata"
+}.Print()
 
