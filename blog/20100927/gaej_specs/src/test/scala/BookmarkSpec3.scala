f0af08614044b01fe29e4ee9ade76e5b35a5eb8d
fits <wadays_wozx@nifty.com>
2010-09-30 13:59:58 +0900

    改良版

---
+++ blog/20100927/gaej_specs/src/test/scala/BookmarkSpec3.scala
@@ -0,0 +1,46 @@
+package fits.sample
+
+import org.specs._
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig
+
+class BookmarkSpec3 extends Specification {
+	val helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig)
+	val testerId = "TESTER01"
+	lazy val setUp = helper.setUp()
+	lazy val tearDown = helper.tearDown()
+
+	"初期状態" should {
+		doBefore(setUp)
+		doAfter(tearDown)
+
+		"ブックマークは空" in {
+			Bookmark.getEntryList(testerId).length must_==0
+		}
+	}
+
+	"ブックマーク追加" should {
+		var list: List[BookmarkEntry] = List()
+
+		//doBefore と doAfter は Example（"XXXX" in {・・・}）毎に実施される
+		doBefore {
+			setUp
+
+			Bookmark.addEntry(testerId, BookmarkEntry("http://localhost/", "default"))
+			list = Bookmark.getEntryList(testerId).toList
+		}
+
+		doAfter(tearDown)
+
+		"サイズは 1" in {
+			list must haveSize(1)
+		}
+
+		"ブックマークは http://localhost/ で default" in {
+			val entry = list.head
+			entry.url must beEqual("http://localhost/")
+			entry.description must beEqual("default")
+		}
+	}
+}
+
