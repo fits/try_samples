690f4ec21ce746f8c1eaf43544b36be33e0420b4
fits <wadays_wozx@nifty.com>
2010-10-16 21:46:01 +0900

    アプリケーションのファイル名を変更し src ディレクトリに移動

--- silverlight/4.0/fsharp/ListSample/build.bat
+++ silverlight/4.0/fsharp/ListSample/build.bat
@@ -1,5 +1,5 @@
 rem build ListSample
 
-fsc -a --noframework --out:app\ListSample.dll -r:"C:\FSharp-2.0.0.0\Silverlight\2.0\bin\FSharp.Core.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\Silverlight\v4.0\System.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\Silverlight\v4.0\System.Windows.dll" MainPage.fs
+fsc -a --noframework --out:app\ListSample.dll -r:"C:\FSharp-2.0.0.0\Silverlight\2.0\bin\FSharp.Core.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\Silverlight\v4.0\System.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\Silverlight\v4.0\System.Windows.dll" src\ListSampleApp.fs
 
 copy C:\FSharp-2.0.0.0\Silverlight\2.0\bin\FSharp.Core.dll app\
