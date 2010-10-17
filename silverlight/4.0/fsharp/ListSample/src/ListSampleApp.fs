0562971d6c52a6f45e25ba4db190a3b8f953ae1f
fits <wadays_wozx@nifty.com>
2010-10-17 09:47:53 +0900

    リストへのデータ表示処理を実装

--- silverlight/4.0/fsharp/ListSample/src/ListSampleApp.fs
+++ silverlight/4.0/fsharp/ListSample/src/ListSampleApp.fs
@@ -4,15 +4,33 @@ open System
 open System.Windows
 open System.Windows.Controls
 
+//レコードの定義
+type Data = {
+    Id: string
+    Title: string
+}
+
+//アプリケーションクラスの定義
 type ListSampleApp() as this =
     inherit Application()
 
+    //初期化処理
     do
         let xamlUri = new Uri("MainPage.xaml", UriKind.Relative)
+        
+        //Startup イベント発生時の処理を追加
         this.Startup.AddHandler(fun _ _ -> 
             let control = new UserControl()
             Application.LoadComponent(control, xamlUri)
 
             this.RootVisual <- control
+
+            let listBox = control.FindName("listBox1") :?> ListBox
+
+            listBox.ItemsSource <- [
+                {Id = "A001"; Title = "XAMLファイルを出力する方法"}
+                {Id = "A002"; Title = "WPF レイアウト"}
+                {Id = "B001"; Title = "F# ラムダ式の記法"}
+            ] 
         )
 
