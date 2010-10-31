38f75394283ee970019cf7cfa385996acfe3b796
fits <wadays_wozx@nifty.com>
2010-10-31 20:50:37 +0900

    メール送信サンプル（ただし、日本語は文字化けする）

---
+++ scala/mail/send_input.scala
@@ -0,0 +1,17 @@
+
+import scala.io.Source
+import org.apache.commons.mail.SimpleEmail
+
+if (args.length != 4) {
+	println("引数： <SMTPサーバー> <From> <To> <Subject>")
+	exit
+}
+
+val body = Source.stdin.mkString
+
+val smtp = new SimpleEmail
+smtp.setHostName(args(0))
+smtp.setFrom(args(1))
+smtp.addTo(args(2))
+smtp.setSubject(args(3))
+smtp.setMsg(body).send
