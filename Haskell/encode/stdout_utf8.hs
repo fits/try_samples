8f387813227ad00be8d30707adbcbb9a26ef775d
fits <wadays_wozx@nifty.com>
2010-11-05 18:50:18 +0900

    動作確認用の処理を追加

--- Haskell/encode/stdout_utf8.hs
+++ Haskell/encode/stdout_utf8.hs
@@ -1,9 +1,28 @@
 import System
 import System.IO
+import Codec.Binary.UTF8.String (encodeString, decodeString)
+import Data.ByteString.Lazy.Char8 (pack, unpack)
 
 main = do
+	-- sjis <- mkTextEncoding "CP932"
+	-- hSetEncoding stdin utf8
+
 	hSetEncoding stdout utf8
 
+	print "テスト"
 	putStrLn "テスト"
 	hPutStrLn stdout "テスト"
 
+	input <- getContents
+	print input
+	putStrLn input
+	hPutStrLn stdout input
+
+	args <- getArgs
+	print $ head args
+	putStrLn $ head args
+	hPutStrLn stdout $ head args
+
+	print $ decodeString $ head args
+	putStrLn $ encodeString $ head args
+	
