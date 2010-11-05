c1e287ba5d1f2951556437477ddd6366e52cad28
fits <wadays_wozx@nifty.com>
2010-11-05 18:28:29 +0900

    比較のためにUTF-8での出力を追加

--- blog/20101105/haskell_iconv/to_sjis.hs
+++ blog/20101105/haskell_iconv/to_sjis.hs
@@ -7,4 +7,7 @@ main = do
 	-- 内部文字を encodeString で UTF-8 化してから Shift_JIS に変換
 	let sjisMsg = C.unpack $ convert "UTF-8" "Shift_JIS" $ C.pack $ encodeString msg
 
+	-- UTF-8 で出力
+	putStrLn $ encodeString msg
+	-- Shift_JIS で出力
 	putStrLn sjisMsg
