e5e269b98f5993720fcca74acc35bcc91a8aa28e
fits <wadays_wozx@nifty.com>
2010-11-05 18:24:13 +0900

    Haskellのiconvパッケージを使った文字コードの変換サンプル

---
+++ blog/20101105/haskell_iconv/to_sjis.hs
@@ -0,0 +1,10 @@
+import Codec.Text.IConv (convert)
+import Codec.Binary.UTF8.String (encodeString)
+import qualified Data.ByteString.Lazy.Char8 as C
+
+main = do
+	let msg = "UTF-8 から Shift_JIS への文字コード変換テスト"
+	-- 内部文字を encodeString で UTF-8 化してから Shift_JIS に変換
+	let sjisMsg = C.unpack $ convert "UTF-8" "Shift_JIS" $ C.pack $ encodeString msg
+
+	putStrLn sjisMsg
