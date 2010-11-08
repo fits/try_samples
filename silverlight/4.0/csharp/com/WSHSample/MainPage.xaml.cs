24677bf9f4b276a6116de2983028e43f24f36a18
fits <wadays_wozx@nifty.com>
2010-11-08 13:04:30 +0900

    WSH.Shellを使った Silverlight4 COMオートメーション連携機能サンプルの雛形追加

---
+++ silverlight/4.0/csharp/com/WSHSample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices.Automation;
+
+namespace WSHSample
+{
+    public partial class MainPage : UserControl
+    {
+        private dynamic wsh;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (AutomationFactory.IsAvailable)
+            {
+                MessageBox.Show("AutomationFactory 使用可");
+
+                this.wsh = AutomationFactory.CreateObject("WScript.Shell");
+
+            }
+            else
+            {
+                MessageBox.Show("AutomationFactory 使用不可");
+            }
+        }
+    }
+}
