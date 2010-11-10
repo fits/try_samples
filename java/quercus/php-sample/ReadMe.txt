efe455a5f68aeb924606b4c8d2e17ed48fbb8944
fits <wadays_wozx@nifty.com>
2010-11-10 18:43:44 +0900

    Java Servlet Engine 上での PHP 実行サンプル

---
+++ java/quercus/php-sample/ReadMe.txt
@@ -0,0 +1,6 @@
+
+■ 前準備
+
+	main/webapp/WEB-INF/lib に quercus-4.0.8.war 内の
+	WEB-INF/lib ディレクト内の全 JAR ファイルをコピー
+
---
+++ java/quercus/php-sample/src/main/webapp/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP Sample on JVM</title>
+</head>
+<body>
+<?php echo("Java Servlet Engine 上での PHP テスト"); ?>
+</body>
+</html>
