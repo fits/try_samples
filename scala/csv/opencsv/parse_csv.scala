dce986832da23b777e1a8cc17351daad64e9737f
fits <wadays_wozx@nifty.com>
2010-11-28 22:56:48 +0900

    改行・カンマ・ダブルクォーテーション含みのcsvファイルをパースするサンプル

---
+++ scala/csv/opencsv/parse_csv.scala
@@ -0,0 +1,15 @@
+import scala.collection.JavaConversions._
+import java.io.FileReader
+import au.com.bytecode.opencsv.CSVReader
+
+if (args.length < 1) {
+	println("scala parse_csv.scala [csv file]")
+	exit
+}
+
+val reader = new CSVReader(new FileReader(args(0)))
+
+reader.readAll().map(l => l.toList).foreach {
+	case no :: title :: content :: _ => println(title + " : " + content)
+	case _ =>
+}
