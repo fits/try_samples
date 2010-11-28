159ed0be04e18eaa8af2266d547201a1e4056c42
fits <wadays_wozx@nifty.com>
2010-11-29 07:59:52 +0900

    CSVファイルをパースするサンプル。（パターンマッチングなし）

---
+++ scala/csv/opencsv/parse_csv3.scala
@@ -0,0 +1,8 @@
+import java.io.FileReader
+import au.com.bytecode.opencsv.CSVReader
+
+val reader = new CSVReader(new FileReader(args(0)))
+
+Iterator.continually(reader.readNext).takeWhile(_ != null).foreach {r =>
+	println(r(1) + " : " + r(2))
+}
