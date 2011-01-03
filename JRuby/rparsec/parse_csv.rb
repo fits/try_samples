9185b3bfaa799ce2aa73bcb44eab97fe28266a95
fits <wadays_wozx@nifty.com>
2011-01-03 12:02:54 +0900

    要素内のダブルクォーテーションに対応（ただし、結果に"が2重で残る）

--- JRuby/rparsec/parse_csv.rb
+++ JRuby/rparsec/parse_csv.rb
@@ -5,8 +5,8 @@ require 'rparsec'
 include RParsec::Parsers
 
 eol = string "\r\n"
-quotedChar = not_char('"').many.fragment
-quotedCell = char('"') >> quotedChar << char('"')
+quotedChar = not_char('"') | string('""')
+quotedCell = char('"') >> quotedChar.many.fragment << char('"')
 cell = quotedCell | regexp(/[^,\r\n]*/).fragment
 line = cell.separated(char ',')
 csvFile = (line << eol).many
@@ -15,3 +15,4 @@ cs = $stdin.readlines.join
 res = csvFile.parse cs
 
 p res
+puts res
