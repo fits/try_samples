00e7a7bea12f330a1e71254707d87ad8861d35c3
fits <wadays_wozx@nifty.com>
2011-01-03 19:33:12 +0900

    不要な処理の削除

--- JRuby/rparsec/parse_simple_csv.rb
+++ JRuby/rparsec/parse_simple_csv.rb
@@ -5,7 +5,7 @@ require 'rparsec'
 include RParsec::Parsers
 
 eol = string "\r\n"
-cell =  regexp(/[^,\r\n]*/).fragment
+cell =  regexp(/[^,\r\n]*/)
 line = cell.separated(char ',')
 csvFile = (line << eol).many
 
