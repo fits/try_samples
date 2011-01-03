60177eafc905393a76045c406701582765081633
fits <wadays_wozx@nifty.com>
2011-01-03 19:14:29 +0900

    ""を含んだ要素に対応

--- JRuby/rparsec/parse_csv.rb
+++ JRuby/rparsec/parse_csv.rb
@@ -5,9 +5,9 @@ require 'rparsec'
 include RParsec::Parsers
 
 eol = string "\r\n"
-quotedChar = not_char('"') | string('""')
-quotedCell = char('"') >> quotedChar.many.fragment << char('"')
-cell = quotedCell | regexp(/[^,\r\n]*/).fragment
+quotedChar = not_char('"') | string('""') >> value('"'[0])
+quotedCell = char('"') >> quotedChar.many.bind {|s| value(s.pack("c*"))} << char('"')
+cell = quotedCell | regexp(/[^,\r\n]*/)
 line = cell.separated(char ',')
 csvFile = (line << eol).many
 
