693196d4208340471f5a7a6ad1e60578c4b11d91
fits <wadays_wozx@nifty.com>
2011-01-18 00:47:53 +0900

    無視するライブラリの設定を追加

--- scala/spring-data/mongodb/sample1/project/build/SpringDataDocumentSample.scala
+++ scala/spring-data/mongodb/sample1/project/build/SpringDataDocumentSample.scala
@@ -6,4 +6,14 @@ class SpringDataDocumentSample(info: ProjectInfo) extends DefaultProject(info) {
 	lazy val springData = "org.springframework.data" % "spring-data-mongodb" % "1.0.0.BUILD-SNAPSHOT"
 
 	override def mainClass = Some("fits.sample.Sample")
+
+	override def ivyXML =
+		<dependencies>
+			<dependency org="org.springframework.data" name="spring-data-mongodb" rev="1.0.0.BUILD-SNAPSHOT">
+				<exclude org="com.sun.jdmk"/>
+				<exclude org="com.sun.jmx"/>
+				<exclude org="javax.jms"/>
+			</dependency>
+		</dependencies>
+
 }
