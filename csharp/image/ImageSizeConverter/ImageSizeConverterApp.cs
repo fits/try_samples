0b04d885948a8f1fa63fc1a445438c64edc241f0
fits <wadays_wozx@nifty.com>
2011-01-25 01:10:43 +0900

    画像のサイズ変換サンプル

---
+++ csharp/image/ImageSizeConverter/ImageSizeConverterApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Fits.Sample
+{
+    class ImageSizeConverterApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("ImageSizeConverter.exe [変換後の横幅] [入力ファイル] [出力ファイル]");
+                return;
+            }
+
+            int toWidth = int.Parse(args[0]);
+            var inputImg = new Bitmap(args[1]);
+
+            double rate = (double)toWidth / inputImg.Width;
+
+            int toHeight = (int)(inputImg.Height * rate);
+
+            var outputImg = inputImg.GetThumbnailImage(toWidth, toHeight, () => true, new IntPtr());
+
+            outputImg.Save(args[2]);
+        }
+    }
+}
