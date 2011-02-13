8f5af39298ff04611bbc9a39f45635babd7c470c
fits <wadays_wozx@nifty.com>
2011-02-13 10:41:51 +0900

    URLの処理を追加

--- JavaScript/node.js/sample.js
+++ JavaScript/node.js/sample.js
@@ -2,8 +2,14 @@
 var http = require("http");
 
 http.createServer(function(req, res) {
+	var url = require("url").parse(req.url, true);
+
+	console.log("requested : " + url.href);
+
+	var title = (url.query && url.query["test"])? url.query["test"]: "test sample";
+
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.end("<html><body><h1>test sample</h1></body></html>");
+	res.end("<html><body><h1>" + title + "</h1></body></html>");
 
 }).listen(8080, "127.0.0.1");
 
