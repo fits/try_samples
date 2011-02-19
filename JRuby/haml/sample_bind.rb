e349056a1aa1cd34825abf7d56701ef9586e400d
fits <wadays_wozx@nifty.com>
2011-02-19 10:54:49 +0900

    指定オブジェクトのメソッド呼び出しとループ処理の追加

--- JRuby/haml/sample_bind.rb
+++ JRuby/haml/sample_bind.rb
@@ -2,6 +2,12 @@
 require "rubygems"
 require "haml"
 
+class TestData
+	def to_list
+		["a", "b", "c"]
+	end
+end
+
 s = <<EOS
 !!! 5
 %html
@@ -10,8 +16,11 @@ s = <<EOS
 	%body
 		.content= content
 		%input{:type => 'hidden', :name => 'aaa'}
+		%ul
+			- to_list.each do |l|
+				%li.data= l
 EOS
 
 eng = Haml::Engine.new(s)
 
-puts eng.render(Object.new, {:title => "haml test", :content => 'テスト'})
+puts eng.render(TestData.new, {:title => "haml test", :content => 'テスト'})
