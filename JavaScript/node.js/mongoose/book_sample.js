0baf7607892f08341ecfda3aaa977bb6723fd184
fits <wadays_wozx@nifty.com>
2011-03-09 01:43:58 +0900

    Book の保存処理を実装

--- JavaScript/node.js/mongoose/book_sample.js
+++ JavaScript/node.js/mongoose/book_sample.js
@@ -1,15 +1,20 @@
 
 var mongoose = require('mongoose');
 
-
-var Book = new mongoose.Schema({
+var BookModel = new mongoose.Schema({
 	title: String,
 	isbn: String
 });
 
-var db = mongoose.createConnection('127.0.0.1', 'book_review');
+mongoose.model('Book', BookModel);
 
+var db = mongoose.createConnection('mongodb://127.0.0.1/book_review');
 
+var Book = db.model('Book');
 
-db.close();
+new Book({title: 'test'}).save(function(err) {
+	console.log("saved : " + err);
+
+	db.close();
+});
 
