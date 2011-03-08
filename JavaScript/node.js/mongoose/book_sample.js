f76991c9e7faf048629f7260efc4daa18a4d46c9
fits <wadays_wozx@nifty.com>
2011-03-09 01:57:08 +0900

    組み込みドキュメントの実装

--- JavaScript/node.js/mongoose/book_sample.js
+++ JavaScript/node.js/mongoose/book_sample.js
@@ -1,9 +1,15 @@
 
 var mongoose = require('mongoose');
 
+var CommentModel = new mongoose.Schema({
+	content: String,
+	created_date: Date
+});
+
 var BookModel = new mongoose.Schema({
 	title: String,
-	isbn: String
+	isbn: String,
+	comments: [CommentModel]
 });
 
 mongoose.model('Book', BookModel);
@@ -12,7 +18,10 @@ var db = mongoose.createConnection('mongodb://127.0.0.1/book_review');
 
 var Book = db.model('Book');
 
-new Book({title: 'test'}).save(function(err) {
+var b = new Book({title: 'test'});
+b.comments.push({content: 'test data', created_date: Date.now()});
+
+b.save(function(err) {
 	console.log("saved : " + err);
 
 	db.close();
