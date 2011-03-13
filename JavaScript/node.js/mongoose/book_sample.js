88e7fe68ad57d13a6a5783cd505cce51f91a3030
fits <wadays_wozx@nifty.com>
2011-03-13 11:56:37 +0900

    User の取得が完了した場合にコールバックを返すメソッドを追加

--- JavaScript/node.js/mongoose/book_sample.js
+++ JavaScript/node.js/mongoose/book_sample.js
@@ -15,6 +15,8 @@ var CommentSchema = new mongoose.Schema({
 
 CommentSchema.virtual('user')
 	.get(function() {
+		return this.get('userobj');
+	/*
 		var result = null;
 
 		User.findById(this.user_id, function(err, item) {
@@ -25,6 +27,7 @@ CommentSchema.virtual('user')
 
 		//findById ‚ª”ñ“¯Šúˆ—‚Ì‚½‚ß null ‚ª•Ô‚Á‚Ä‚µ‚Ü‚¤
 		return result;
+	*/
 	})
 	.set(function(u) {
 		this.set("user_id", u._id)
@@ -32,7 +35,24 @@ CommentSchema.virtual('user')
 
 //‰Šú‰»Œã‚Ìˆ—
 CommentSchema.post('init', function() {
-	console.log("--- post init ---");
+	this.initCompleted = function(fn) {
+		if (this.get('userobj') == null) {
+			this.completedFunc = fn;
+		}
+		else {
+			fn();
+		}
+	};
+	
+	var thisObj = this;
+
+	User.findById(this.user_id, function(err, item) {
+		thisObj.set('userobj', item.doc);
+
+		if (thisObj.completedFunc) {
+			thisObj.completedFunc();
+		}
+	});
 });
 
 
@@ -55,6 +75,7 @@ u.save();
 
 var b = new Book({title: 'test4'});
 b.comments.push({content: 'test4 data', created_date: Date.now(), user: u});
+b.comments.push({content: 'aaaa', created_date: Date.now(), user: u});
 
 b.save(function(err) {
 	console.log("saved : " + err);
@@ -64,24 +85,27 @@ Book.where('comments.content', 'test3 data').find(function(err, list) {
 
 	for (var i = 0; i < list.length; i++) {
 		var doc = list[i].doc;
-		console.log("--- " + doc.title + " ---");
 
 		for (var j = 0; j < doc.comments.length; j++) {
 			var c = doc.comments[j];
-			console.log(c.content + ", " + c.user);
+
+			c.initCompleted(function() {
+				console.log(doc.title + ", " + c.content + ", " + c.user.name);
+			});
 		}
 	}
 });
 
-
 Book.find({'comments.content' : 'test3 data'}, function(err, list) {
 	for (var i = 0; i < list.length; i++) {
 		var doc = list[i].doc;
-		console.log("--- " + doc.title + " ---");
 
 		for (var j = 0; j < doc.comments.length; j++) {
 			var c = doc.comments[j];
-			console.log(c.content + ", " + c.user);
+
+			c.initCompleted(function() {
+				console.log(doc.title + ", " + c.content + ", " + c.user.name);
+			});
 		}
 	}
 
