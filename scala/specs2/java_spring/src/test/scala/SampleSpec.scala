e036dd069f1c6870c171c98010e46cf3990f97f4
fits <wadays_wozx@nifty.com>
2011-03-18 02:17:29 +0900

    クラス名変更とコメント追加

--- scala/specs2/java_spring/src/test/scala/SampleSpec.scala
+++ scala/specs2/java_spring/src/test/scala/SampleSpec.scala
@@ -5,8 +5,14 @@ import org.specs2.runner._
 
 import org.junit.runner._
 
+/** 
+ * Maven + JUnit4 で実行する場合 Test で終わるクラス名にするか、
+ * mvn コマンドの実行時に "-Dtest=クラス名" で指定する必要がある
+ * 
+ * （例）mvn -Dtest=*Spec test
+ */
 @RunWith(classOf[JUnitRunner])
-class SampleTest extends Specification {
+class SampleSpecTest extends Specification {
 
 	"Sample" should {
 		"Sample can set name, get name" in {
@@ -17,3 +23,4 @@ class SampleTest extends Specification {
 		}
 	}
 }
+
