82fd579fada65198291b932feb3168c3ad0d6cf1
fits <wadays_wozx@nifty.com>
2011-03-19 11:12:16 +0900

    サンプル追加

--- scala/specs/java_maven/src/test/scala/BookSpec.scala
+++ scala/specs/java_maven/src/test/scala/BookSpec.scala
@@ -1,5 +1,7 @@
 package fits.sample
 
+import scala.collection.JavaConversions._
+
 import org.specs._
 import org.specs.runner.JUnit4
 
@@ -7,13 +9,39 @@ class BookSpecTest extends JUnit4(BookSpec)
 
 object BookSpec extends Specification {
 
-	"Book に title を指定" should {
+	"初期状態" should {
+		val b = new Book()
+
+		"title は null" in {
+			b.getTitle() must beNull
+		}
+
+		"comments は null ではない" in {
+			b.getComments() must notBeNull
+		}
+
+		"comments は空" in {
+			b.getComments() must haveSize(0)
+		}
+	}
+
+	"title を指定" should {
 		val b = new Book()
 		b.setTitle("テスト")
 
 		"title が設定されている" in {
-			b.getTitle() must be equalTo("テスト")
+			b.getTitle() must beEqualTo("テスト")
 		}
+	}
+
+	"Comment を追加" should {
+		val b = new Book()
+		b.getComments().add(new Comment())
+
+		"Comment が追加されている" in {
+			b.getComments() must haveSize(1)
+		}
+
 	}
 }
 
