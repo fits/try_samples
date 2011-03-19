539c2ef3b1384ada5a3fe1334081460d96413e3b
fits <wadays_wozx@nifty.com>
2011-03-19 19:06:00 +0900

    Maven RSpec Sample（ただし、target/rspec-runner.rb の48行目をコメントアウトしないと正常に動作しない）

---
+++ java/maven_bdd/rspec/spec/book_spec.rb
@@ -0,0 +1,47 @@
+require "java"
+
+module Fits
+	include_package "fits.sample"
+end
+
+describe "Book" do
+	context "‰Šúó‘Ô" do
+		before do
+			@b = Fits::Book.new
+		end
+
+		it "title ‚Í nil" do
+			@b.title.should be_nil
+		end
+
+		it "comments ‚Í nil ‚Å‚Í‚È‚¢" do
+			@b.comments.should_not be_nil
+		end
+
+		it "comments ‚Í‹ó" do
+			@b.comments.size.should == 0
+		end
+	end
+
+	context "title ‚ğw’è" do
+		before do
+			@b = Fits::Book.new
+			@b.title = "test"
+		end
+
+		it "title ‚ªİ’è‚³‚ê‚Ä‚¢‚é" do
+			@b.title.should == "test"
+		end
+	end
+
+	context "Comment ‚ğ’Ç‰Á" do
+		before do
+			@b = Fits::Book.new
+			@b.comments.add(Fits::Comment.new)
+		end
+
+		it "Comment ‚ª’Ç‰Á‚³‚ê‚Ä‚¢‚é" do
+			@b.comments.size.should == 1
+		end
+	end
+end
