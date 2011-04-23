47006a375f7f85d1e2b23d12f11e5d8a10d01121
fits <wadays_wozx@nifty.com>
2011-04-23 19:25:23 +0900

    リファクタリング

--- scala/grizzly/http_sample/src/main/scala/fits.sample/Sample.scala
+++ scala/grizzly/http_sample/src/main/scala/fits.sample/Sample.scala
@@ -3,18 +3,26 @@ package fits.sample
 import scala.io.Source
 import org.glassfish.grizzly.http.server._
 
+object Handler {
+	def apply(proc: (Request, Response) => Unit) = {
+		new HttpHandler {
+			override def service(req: Request, res: Response) = {
+				proc(req, res)
+			}
+		}
+	}
+}
+
 object Sample {
 	def main(args: Array[String]) {
 
 		val server = HttpServer.createSimpleServer
+		val conf = server.getServerConfiguration
 
-		server.getServerConfiguration.addHttpHandler(
-			new HttpHandler {
-				override def service(req: Request, res: Response) = {
-					res.getWriter.write("http server sample")
-				}
-			}, 
-			"/test"
+		conf.addHttpHandler(
+			Handler { (req, res) => 
+				res.getWriter.write("http server sample")
+			}, "/test"
 		)
 
 		server.start
