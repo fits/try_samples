5f87f7bd6d903b41842040f1d3e45588bba14c3c
fits <wadays_wozx@nifty.com>
2011-04-24 19:53:42 +0900

    出力する文字列の内容を変更

--- blog/20110424/grizzly_scala_sample/src/main/scala/fits.sample/Sample.scala
+++ blog/20110424/grizzly_scala_sample/src/main/scala/fits.sample/Sample.scala
@@ -3,25 +3,23 @@ package fits.sample
 import scala.io.Source
 import org.glassfish.grizzly.http.server._
 
-object Handler {
-	def apply(proc: (Request, Response) => Unit) = {
+object Sample {
+	def handler(proc: (Request, Response) => Unit) = {
 		new HttpHandler {
 			override def service(req: Request, res: Response) = {
 				proc(req, res)
 			}
 		}
 	}
-}
 
-object Sample {
 	def main(args: Array[String]) {
 
 		val server = HttpServer.createSimpleServer
 		val conf = server.getServerConfiguration
 
 		conf.addHttpHandler(
-			Handler { (req, res) => 
-				res.getWriter.write("http server sample")
+			handler { (req, res) => 
+				res.getWriter.write("hello")
 			}, "/test"
 		)
 
