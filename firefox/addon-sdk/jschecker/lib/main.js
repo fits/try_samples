3dfd83bd85895f502de199810615618f03dec7ec
fits <wadays_wozx@nifty.com>
2011-04-30 08:16:03 +0900

    JavaScript の有効・無効を切り替える処理を実装

--- firefox/addon-sdk/jschecker/lib/main.js
+++ firefox/addon-sdk/jschecker/lib/main.js
@@ -1,13 +1,22 @@
+
+const {Cc, Ci} = require("chrome");
 const widgets = require("widget");
+
+var pservice = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("");
+
+var displayJSStatus = function(w) {
+  var jsenable = pservice.getBoolPref("javascript.enabled", false);
+  w.content = (jsenable)? "js": "--";
+}
 
 var widget = widgets.Widget({
   id: "jschecker",
   label: "JavaScript checker",
-  contentURL: "about:blank",
   content: "--",
   onClick: function() {
-	console.log("click");
+	pservice.setBoolPref("javascript.enabled", !pservice.getBoolPref("javascript.enabled", false));
+
+	displayJSStatus(this);
   }
 });
 
-console.log("jschecker is running.");
