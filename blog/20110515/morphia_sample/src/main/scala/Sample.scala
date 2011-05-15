09e1262b81791ac3b30c3c30423be0aa8efc50a6
fits <wadays_wozx@nifty.com>
2011-05-15 12:15:40 +0900

    @Reference が正常に機能するように修正

--- blog/20110515/morphia_sample/src/main/scala/Sample.scala
+++ blog/20110515/morphia_sample/src/main/scala/Sample.scala
@@ -17,17 +17,27 @@ object Sample {
 		db.save[User](u2)
 
 		val b1 = new Book("ドメイン駆動設計", "9784798121963")
+
 		b1.comments.add(new Comment("test", u1))
 		b1.comments.add(new Comment("test2", u2))
 
 		db.save[Book](b1)
+
+		//更新
+		val tempU1 = db.get(classOf[User], u1.id)
+		tempU1.name = "user1-updated"
+		db.save[User](tempU1)
+
+		val tempB1 = db.get(classOf[Book], b1.id)
+		tempB1.comments.add(new Comment("aaaaaaaa", tempU1))
+		db.save[Book](tempB1)
 
 		//全件取得
 		db.find(classOf[Book]).asList.foreach {b =>
 			printf("book: id = %s, title = %s, isbn = %s\n", b.id, b.title, b.isbn)
 
 			b.comments.foreach {c =>
-				printf("  comment: content = %s, date = %s, user = %s\n", c.content, c.createdDate, c.user.name)
+				printf("  comment: %s, date = %s, user = %s\n", c.content, c.createdDate, c.user.name)
 			}
 		}
 	}
