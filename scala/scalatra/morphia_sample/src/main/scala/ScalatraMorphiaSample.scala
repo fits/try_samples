26f4853e6a2ca262ec0452c6af4b03795f2d818a
fits <wadays_wozx@nifty.com>
2011-05-17 01:16:00 +0900

    基本的な処理を実装

--- scala/scalatra/morphia_sample/src/main/scala/ScalatraMorphiaSample.scala
+++ scala/scalatra/morphia_sample/src/main/scala/ScalatraMorphiaSample.scala
@@ -5,6 +5,7 @@ import org.scalatra.ScalatraServlet
 import org.scalatra.scalate.ScalateSupport
 import com.google.code.morphia.Morphia
 import com.mongodb.Mongo
+import org.bson.types.ObjectId
 
 import fits.sample.models._
 
@@ -14,6 +15,42 @@ class ScalatraMorphiaSample extends ScalatraServlet with ScalateSupport {
 
 	beforeAll {
 		contentType = "text/html"
+	}
+
+	get("/") {
+		val books: Iterable[Book] = db.find(classOf[Book]).asList.asScala
+		val users: Iterable[User] = db.find(classOf[User]).asList.asScala
+
+		templateEngine.layout("index.scaml", Map(
+			"books" -> books,
+			"users" -> users,
+			"action" -> "/comments"
+		))
+	}
+
+	get("/books") {
+		val books: Iterable[Book] = db.find(classOf[Book]).asList.asScala
+
+		templateEngine.layout("book.scaml", Map(
+			"books" -> books,
+			"action" -> "/books"
+		))
+	}
+
+	post("/books") {
+		db.save[Book](new Book(params("title"), params("isbn")))
+
+		redirect("/books")
+	}
+
+	post("/comments") {
+		val book = db.get(classOf[Book], new ObjectId(params("book")))
+		val user = db.get(classOf[User], new ObjectId(params("user")))
+
+		book.comments.add(new Comment(params("content"), user))
+		db.save[Book](book)
+
+		redirect("/")
 	}
 
 	get("/users") {
