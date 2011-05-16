3947fa27c90f6f186d02695ec2de4c612567ffd3
fits <wadays_wozx@nifty.com>
2011-05-17 00:03:43 +0900

    ユーザーの追加処理を実装

--- scala/scalatra/morphia_sample/src/main/scala/ScalatraMorphiaSample.scala
+++ scala/scalatra/morphia_sample/src/main/scala/ScalatraMorphiaSample.scala
@@ -1,23 +1,34 @@
 package fits.sample
 
+import scala.collection.JavaConverters._
 import org.scalatra.ScalatraServlet
 import org.scalatra.scalate.ScalateSupport
+import com.google.code.morphia.Morphia
+import com.mongodb.Mongo
+
+import fits.sample.models._
 
 class ScalatraMorphiaSample extends ScalatraServlet with ScalateSupport {
+
+	val db = new Morphia().createDatastore(new Mongo("localhost"), "book_review")
 
 	beforeAll {
 		contentType = "text/html"
 	}
 
 	get("/users") {
-/*		renderTemplate("user.scaml", 
-			"action" -> "/users"
-		)
-*/
-		templateEngine.layout("user.scaml", 
-			Map("action" -> "/users")
-		)
+		val users: Iterable[User] = db.find(classOf[User]).asList.asScala
 
+		templateEngine.layout("user.scaml", Map(
+			"users" -> users,
+			"action" -> "/users"
+		))
+	}
+
+	post("/users") {
+		db.save[User](new User(params("name")))
+
+		redirect("/users")
 	}
 }
 
