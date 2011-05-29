cd080c40faffcba03b150bf3a93d56a3abb195ba
fits <wadays_wozx@nifty.com>
2011-05-29 17:33:31 +0900

    依存関係を変更

--- scala/jetty/embed/sample/project/build/EmbedJettySampleProject.scala
+++ scala/jetty/embed/sample/project/build/EmbedJettySampleProject.scala
@@ -2,8 +2,12 @@ import sbt._
 
 class EmbedJettySampleProject(info: ProjectInfo) extends DefaultProject(info) {
 
-	val jetty = "org.eclipse.jetty" % "jetty-distribution" % "8.0.0.M3"
-	val servletapi = "javax.servlet.jsp" % "jsp-api" % "2.1"
+	val jettyVersion = "8.0.0.M3"
+
+	val jettyWebapp = "org.eclipse.jetty" % "jetty-webapp" % jettyVersion
+	val jettySecurity = "org.eclipse.jetty" % "jetty-security" % jettyVersion
+	val jettyServlets = "org.eclipse.jetty" % "jetty-servlets" % jettyVersion
+	val jettyDeploy = "org.eclipse.jetty" % "jetty-deploy" % jettyVersion
 
 	override def mainClass = Some("fits.sample.Sample")
 }
