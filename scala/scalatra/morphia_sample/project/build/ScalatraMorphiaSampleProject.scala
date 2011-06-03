c428d5b5b3a269ce51bf62f9ad91eb3191cf4edb
fits <wadays_wozx@nifty.com>
2011-06-03 20:00:21 +0900

    Jettyのバージョン変更

--- scala/scalatra/morphia_sample/project/build/ScalatraMorphiaSampleProject.scala
+++ scala/scalatra/morphia_sample/project/build/ScalatraMorphiaSampleProject.scala
@@ -1,14 +1,15 @@
 import sbt._
 
 class ScalatraMorphiaSampleProject(info: ProjectInfo) extends DefaultWebProject(info) {
-
+	//以下の設定で war ファイルに scala-compiler.jar が入るようになる
 	override def webappClasspath = super.webappClasspath +++ buildCompilerJar
 
+	val jettyVersion = "8.0.0.M3"
 	val scalatraVersion = "2.0.0-SNAPSHOT"
 	val scalateVersion = "1.6.0-SNAPSHOT"
 	val morphiaVersion = "1.00-SNAPSHOT"
 
-	val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.26" % "test"
+	val jettyWebapp = "org.eclipse.jetty" % "jetty-webapp" % jettyVersion % "test"
 	val servletapi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
 
 	val scalatra = "org.scalatra" %% "scalatra" % scalatraVersion
