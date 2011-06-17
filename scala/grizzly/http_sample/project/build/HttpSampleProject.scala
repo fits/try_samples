d8e9b36410ba850936feb00a4c1a3c8e46fc48ae
fits <wadays_wozx@nifty.com>
2011-06-18 02:04:54 +0900

    バージョン変更

--- scala/grizzly/http_sample/project/build/HttpSampleProject.scala
+++ scala/grizzly/http_sample/project/build/HttpSampleProject.scala
@@ -7,6 +7,6 @@ class HttpSampleProject(info: ProjectInfo) extends DefaultProject(info) {
 	lazy val glassFishRepo = "Java.net Maven 2 Repository for GlassFish" at "http://download.java.net/maven/glassfish/"
 	lazy val javaNetRepo = "Java.net Repository for Grizzly" at "http://download.java.net/maven/2"
 
-	lazy val grizzly = "org.glassfish.grizzly" % "grizzly-http-server" % "2.0.1"
+	lazy val grizzly = "org.glassfish.grizzly" % "grizzly-http-server" % "2.1.1"
 
 }
