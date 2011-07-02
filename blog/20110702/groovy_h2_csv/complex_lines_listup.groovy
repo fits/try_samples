a9d55905ffc69b64fa7fa5b4322d3ba30f49c0f1
fits <wadays_wozx@nifty.com>
2011-07-02 22:41:00 +0900

    blog sample

---
+++ blog/20110702/groovy_h2_csv/complex_lines_listup.groovy
@@ -0,0 +1,31 @@
+@Grapes([
+    @Grab(group = 'com.h2database', module = 'h2', version = '1.3.157'),
+    @GrabConfig(systemClassLoader = true)
+])
+import groovy.sql.Sql
+
+def db = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
+
+def sql = """
+SELECT
+ *
+FROM (
+	SELECT
+		pref_name,
+		station_g_cd,
+		station_name,
+		count(*) as lines
+	FROM
+	  CSVREAD('m_station.csv') S
+	  JOIN CSVREAD('m_pref.csv') P ON S.pref_cd=P.pref_cd
+	GROUP BY station_g_cd, station_name
+	ORDER BY lines DESC, station_g_cd
+)
+WHERE ROWNUM <= 10
+"""
+
+db.eachRow(sql) {r ->
+    println "${r.station_name}âwÅi${r.pref_name}Åj: ${r.lines}"
+}
+
+
