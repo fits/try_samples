e5e5a8f91a5955cd5d822a5579f9280d522eb6c1
fits <wadays_wozx@nifty.com>
2011-07-15 00:32:12 +0900

    並列コレクション版のサンプル追加

---
+++ scala/csv/ekidata_sample/listup_station_par.scala
@@ -0,0 +1,18 @@
+import scala.io.Source
+
+case class Station(val stationName: String, val prefName: Option[String], val stationGroupCode: String)
+
+val prefMap = Source.fromFile("m_pref.csv").getLines().drop(1).toList.par.map {l =>
+	val items = l.split(",")
+	items(0) -> items(1)
+}.toMap
+
+val lines = Source.fromFile("m_station.csv").getLines()
+
+val list = lines.drop(1).toList.par.map(_.split(",")).groupBy {s =>
+	Station(s(9), prefMap.get(s(10)), s(5))
+}.toList.sortBy(_._2.length * -1) take 10
+
+list.foreach {s =>
+	printf("%s駅 (%s) : %d\n", s._1.stationName, s._1.prefName.get, s._2.length)
+}
