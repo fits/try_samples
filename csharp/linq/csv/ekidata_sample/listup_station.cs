0c7696931e61f0e572db98fd9b1e2360c4277a14
fits <wadays_wozx@nifty.com>
2011-07-17 01:22:00 +0900

    駅名と路線数を出力する処理を実装

--- csharp/linq/csv/ekidata_sample/listup_station.cs
+++ csharp/linq/csv/ekidata_sample/listup_station.cs
@@ -8,21 +8,23 @@ class ListUpStation
 {
 	public static void Main(string[] args)
 	{
-		var lines = new List<string>(File.ReadAllLines("m_station.csv", Encoding.Default));
-		lines.RemoveAt(0);
+		var lines = File.ReadAllLines("m_station.csv", Encoding.Default);
 
-		var q = 
-			from l in lines
+		var q = (
+			from l in lines.Skip(1)
 			let s = l.Split(',')
 			group s by new { 
 				StationName = s[9],
 				PrefName = s[10], 
-				stationGroupCode = s[5]
-			};
+				StationGroupCode = s[5]
+			} into stGroup
+			orderby stGroup.Count() descending
+			select stGroup
+		).Take(10);
 
 		foreach(var l in q)
 		{
-			Console.WriteLine(l);
+			Console.WriteLine("{0}‰w ({1}) : {2}", l.Key.StationName, l.Key.PrefName, l.Count());
 		}
 	}
 }
