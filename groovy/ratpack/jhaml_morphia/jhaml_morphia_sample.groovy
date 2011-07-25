669cb6ce73ad9a448c2551ebd1f02a83d2e49bf9
fits <wadays_wozx@nifty.com>
2011-07-26 01:25:12 +0900

    Userの登録・一覧表示処理を実装

--- groovy/ratpack/jhaml_morphia/jhaml_morphia_sample.groovy
+++ groovy/ratpack/jhaml_morphia/jhaml_morphia_sample.groovy
@@ -1,15 +1,40 @@
 
-import com.cadrlife.jhaml.*
+import groovy.text.SimpleTemplateEngine
+
+import com.google.code.morphia.Morphia
+import com.mongodb.Mongo
+import com.cadrlife.jhaml.JHaml
+import com.google.code.morphia.annotations.*
+import org.bson.types.ObjectId
+
+@Entity(value = "users", noClassnameStored = true)
+class User {
+	@Id ObjectId id
+	String name
+}
 
 def renderHaml = {template, params->
-	new JHaml().parse(new File("templates/${template}").text)
+	def hamlText = new JHaml().parse(new File("templates/${template}").text)
+	new SimpleTemplateEngine().createTemplate(hamlText).make(params).toString()
 }
+
+def db = new Morphia().createDatastore(new Mongo("localhost"), "book_review")
 
 get("/") {
-	
-	
-	
-	
-	renderHaml "index.haml", [:]
+	def users = db.find(User.class).order("name")
+
+	renderHaml "index.haml", ["users": users]
 }
 
+
+get("/users") {
+	def users = db.find(User.class).order("name")
+
+	renderHaml "user.haml", ["users": users]
+}
+
+post("/users") {
+	db.save(new User(name: params.name))
+
+	response.sendRedirect("users")
+}
