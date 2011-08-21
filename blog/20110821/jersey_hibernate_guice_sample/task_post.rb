41dcd13dbb947b8f40bb9d8c1f80c0afe6f389ba
fits <wadays_wozx@nifty.com>
2011-08-21 12:30:40 +0900

    簡略化

--- blog/20110821/jersey_hibernate_guice_sample/task_post.rb
+++ blog/20110821/jersey_hibernate_guice_sample/task_post.rb
@@ -1,8 +1,6 @@
 require "net/http"
 require "uri"
 
-res = Net::HTTP.post_form(URI.parse("http://localhost:8080/sample/task"), {
-	"title" => ARGV[0]
-})
+res = Net::HTTP.post_form(URI.parse("http://localhost:8080/sample/task"), "title" => ARGV[0])
 
 puts res.body
