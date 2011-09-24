0388a6b8c587be0568965b448dc47cbff945d0b1
fits <wadays_wozx@nifty.com>
2011-09-24 18:08:53 +0900

    例外処理の追加

--- scala/parallel_collection/download_web/download_web.scala
+++ scala/parallel_collection/download_web/download_web.scala
@@ -3,12 +3,21 @@ import scala.io.Source
 import java.io.File
 import java.net.URL
 import java.nio.file.{Paths, Files}
+import java.nio.file.StandardCopyOption._
 
 val dir = args(0)
 
 Source.stdin.getLines.toList.par.foreach {u =>
 	val url = new URL(u)
 	val f = new File(url.getFile()).getName()
+	val filePath = Paths.get(dir, f)
 
-	Files.copy(url.openStream(), Paths.get(dir, f))
+	try {
+		Files.copy(url.openStream(), filePath, REPLACE_EXISTING)
+
+		printf("completed: %s => %s\n", url, filePath)
+
+	} catch {
+		case e: Exception => printf("failed: %s, %s\n", url, e)
+	}
 }
