65fa8d7b61913c58bf98729a7867dec5d8a597a5
fits <wadays_wozx@nifty.com>
2011-10-02 01:28:21 +0900

    修正

--- JRuby/https/post/basic_post.rb
+++ JRuby/https/post/basic_post.rb
@@ -6,9 +6,11 @@ user = ARGV[1]
 pass = ARGV[2]
 postData = ARGV[3]
 
-res = Net::HTTP.start(url.host, url.port) do |https|
-	https.use_ssl = true
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+https.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
+res = https.start do
 	req = Net::HTTP::Post.new(url.path)
 	req.basic_auth user, pass
 
@@ -17,4 +19,4 @@ res = Net::HTTP.start(url.host, url.port) do |https|
 	https.request(req)
 end
 
-puts res
+puts res.body
