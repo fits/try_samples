39fc1250a9ef1a900193c37c43c84639e0dbb4a4
fits <wadays_wozx@nifty.com>
2011-10-09 02:54:46 +0900

    URLへの接続処理を非同期に実施するように変更

--- java/7.0/nio2/download_web/AsyncDownloadFile.java
+++ java/7.0/nio2/download_web/AsyncDownloadFile.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.net.URI;
 
 import java.nio.ByteBuffer;
@@ -8,7 +9,10 @@ import java.nio.channels.AsynchronousFileChannel;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
 
 public class AsyncDownloadFile {
 	public static void main(String[] args) throws Exception {
@@ -17,28 +21,47 @@ public class AsyncDownloadFile {
 			return;
 		}
 
-		String dir = args[0];
-		URI uri = new URI(args[1]);
+		final String dir = args[0];
+		final URI uri = new URI(args[1]);
+		final String fileName = new File(uri.getPath()).getName();
 
-		String fileName = new File(uri.getPath()).getName();
+		ExecutorService es = Executors.newCachedThreadPool();
 
-		ArrayList<Future<Integer>> futures = new ArrayList<>();
-
-		try (BufferedInputStream bis = new BufferedInputStream(uri.toURL().openStream())) {
-			byte[] buf = new byte[10240];
-			int len = 0;
-			int pos = 0;
-
-			AsynchronousFileChannel fc = AsynchronousFileChannel.open(Paths.get(dir, fileName), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-
-			while ((len = bis.read(buf, 0, buf.length)) > -1) {
-				futures.add(fc.write(ByteBuffer.wrap(buf, 0, len), pos));
-				pos += len;
+		final Future<InputStream> con = es.submit(new Callable<InputStream>() {
+			public InputStream call() throws Exception {
+				return uri.toURL().openStream();
 			}
+		});
+
+		Future<List<Future<Integer>>> res = es.submit(new Callable<List<Future<Integer>>>() {
+			public List<Future<Integer>> call() throws Exception {
+				ArrayList<Future<Integer>> result = new ArrayList<>();
+
+				try (BufferedInputStream bis = new BufferedInputStream(con.get())) {
+					byte[] buf = new byte[10240];
+					int pos = 0;
+					int len = 0;
+
+					AsynchronousFileChannel fc = AsynchronousFileChannel.open(Paths.get(dir, fileName), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+
+					while ((len = bis.read(buf, 0, buf.length)) > -1) {
+						result.add(fc.write(ByteBuffer.wrap(buf, 0, len), pos));
+						pos += len;
+					}
+				}
+				return result;
+			}
+		});
+
+		try {
+			for (Future<Integer> f : res.get()) {
+				f.get();
+			}
+			System.out.println("downloaded: " + uri);
+		} catch (Exception ex) {
+			System.out.println("failed: " + uri);
 		}
 
-		for (Future<Integer> f : futures) {
-			f.get();
-		}
+		es.shutdown();
 	}
 }
