7599ce9d0cc471c7aa24c9cc049a910d1d6b7a77
fits <wadays_wozx@nifty.com>
2011-10-08 09:28:40 +0900

    非同期処理を使った複数Webコンテンツダウンロード

---
+++ fsharp/async/async_download_web/async_download_web.fs
@@ -0,0 +1,32 @@
+open System
+open System.IO
+open System.Net
+
+[<EntryPoint>]
+let main(args: string[]) = 
+    let downloadFile (dir: string) (url: string) = 
+        async {
+            try
+                let req = WebRequest.Create(url)
+                let! res = req.AsyncGetResponse()
+
+                let fileName = Path.Combine(dir, Path.GetFileName(url))
+
+                use stream = res.GetResponseStream()
+                use fs = new FileStream(fileName, FileMode.Create)
+
+                let! buf = stream.AsyncRead(int res.ContentLength)
+                do! fs.AsyncWrite(buf, 0, buf.Length)
+
+                stdout.WriteLine("success: {0}", url)
+            with
+            | _ as e -> stdout.WriteLine("failed: {0}, {1}", url, e.Message)
+        }
+
+    stdin.ReadToEnd().Split([| Environment.NewLine |], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (downloadFile args.[0])
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> ignore
+
+    0
