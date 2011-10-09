66df59c581dcaa42ed68cc9793663ae6e33b693b
fits <wadays_wozx@nifty.com>
2011-10-09 22:28:12 +0900

    リファクタリング

--- groovy/gpars/download_web/async_download_web.groovy
+++ groovy/gpars/download_web/async_download_web.groovy
@@ -1,4 +1,4 @@
-import groovyx.gpars.GParsExecutorsPool
+import groovyx.gpars.*
 import java.nio.file.Paths
 import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousFileChannel
@@ -12,21 +12,18 @@ if (args.length < 1) {
 def dir = args[0]
 
 GParsExecutorsPool.withPool {
-	def res = []
-
-	System.in.readLines() each {
+	System.in.readLines() collect {
 		def url = new URL(it)
 		def file = new File(dir, new File(url.file).name)
 
 		def stream = { url.newInputStream() }.callAsync()
 		def readStream = { stream.get().bytes }.callAsync()
-		res << {
+
+		return {
 			file.bytes = readStream.get()
 			[url, file]
 		}.callAsync()
-	}
-
-	res.each {
+	} each {
 		try {
 			def ret = it.get()
 			println "downloaded: ${ret[0]} => ${ret[1]}"
