54a43ee5c17f899713482f265f05714fd50c5fd9
fits <wadays_wozx@nifty.com>
2011-10-12 01:11:17 +0900

    ファイルダウンロードの暫定的な実装

--- scala/parallel_collection/download_web/async_download_web.scala
+++ scala/parallel_collection/download_web/async_download_web.scala
@@ -3,35 +3,63 @@ import scala.actors.Actor
 import scala.actors.Actor._
 
 import java.io.InputStream
+import java.io.File
 import java.net.URL
+import java.nio.file.{Paths, Files, Path}
+import java.nio.file.StandardCopyOption._
 
-class URLActor(k: (InputStream => Unit)) extends Actor {
+case class URLOpen(val url: URL, val k: (InputStream => Unit))
+case class CreateFile(val filePath: Path, val stream: InputStream, val k: (Path => Unit))
+
+class URLActor extends Actor {
 	def act() {
 		loop {
 			react {
-				case url: String => {
-					println("receive : " + url)
-					k(new URL(url).openStream())
-					//exit を実施しないと終了しない
-					exit()
+				case uo: URLOpen => {
+					println("receive : " + uo.url)
+					uo.k(uo.url.openStream())
+				}
+				case rs: CreateFile => {
+					println("file create")
+					Files.copy(rs.stream, rs.filePath, REPLACE_EXISTING)
+					rs.k(rs.filePath)
 				}
 			}
 		}
 	}
+
+	def stop() {
+		exit()
+	}
 }
 
-val url = args(0)
+val url = new URL(args(0))
+val dir = args(1)
 
 reset {
-	val stream = shift {k: (InputStream => Unit) =>
-		val actor = new URLActor(k)
-		actor.start
-		actor ! url
+	val actor = new URLActor()
+	actor.start
+	println("actor.start")
 
-		println("actor ! " + url)
+	val stream = shift {k: (InputStream => Unit) =>
+		actor ! URLOpen(url, k)
+		println("actor ! URLOpen")
 	}
 
-	
-
 	println("stream = " + stream)
+
+	val file = shift {k: (Path => Unit) =>
+		val f = new File(url.getFile()).getName()
+		val filePath = Paths.get(dir, f)
+
+		actor ! CreateFile(filePath, stream, k)
+		println("actor ! CreateFile")
+	}
+
+	println("downloaded: " + file)
+
+	actor.stop()
+	println("actor.stop")
 }
+
+println("*** out reset")
