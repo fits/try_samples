c336a317f57de01f1dd0c4f561fba072b277b588
fits <wadays_wozx@nifty.com>
2011-10-15 17:50:57 +0900

    Webコンテンツの非同期ダウンロード処理のサンプル

---
+++ blog/20111016/async_download_web.groovy
@@ -0,0 +1,23 @@
+import groovyx.gpars.*
+
+def dir = args[0]
+
+GParsExecutorsPool.withPool {
+	System.in.readLines() collect {
+		def url = new URL(it)
+		def file = new File(dir, new File(url.file).name)
+
+		def stream = { url.newInputStream() }.callAsync()
+		def readStream = { stream.get().bytes }.callAsync()
+		def future = { file.bytes = readStream.get() }.callAsync()
+
+		[url: url, file: file, future: future]
+	} each {
+		try {
+			it.future.get()
+			println "downloaded: ${it.url} => ${it.file}"
+		} catch(e) {
+			println "failed: ${it.url}, $e"
+		}
+	}
+}
---
+++ blog/20111016/async_download_web.scala
@@ -0,0 +1,76 @@
+import scala.actors.Actor
+import scala.actors.Actor._
+import scala.io.Source
+import scala.util.continuations._
+
+import java.io.{InputStream, File}
+import java.net.URL
+import java.nio.file.{Paths, Files, Path}
+import java.nio.file.StandardCopyOption._
+
+case class URLOpen(val url: URL, val k: (InputStream => Unit))
+case class URLDownload(val url: URL, val stream: InputStream, 
+				val destDir: String, val k: (Path => Unit))
+
+class URLActor extends Actor {
+	def act() {
+		loop {
+			react {
+				case uo: URLOpen => {
+					try {
+						uo.k(uo.url.openStream())
+					}
+					catch {
+						case e: Exception => failStop(e, uo.url)
+					}
+				}
+				case rs: URLDownload => {
+					val f = new File(rs.url.getFile()).getName()
+					val filePath = Paths.get(rs.destDir, f)
+
+					try {
+						Files.copy(rs.stream, filePath, REPLACE_EXISTING)
+						rs.k(filePath)
+					}
+					catch {
+						case e: Exception => failStop(e, rs.url)
+					}
+				}
+			}
+		}
+	}
+
+	def stop() {
+		exit
+	}
+
+	def failStop(e: Exception, url: URL) {
+		printf("failed: %s, %s\n", url, e)
+		exit
+	}
+}
+
+val dir = args(0)
+
+//Source.stdin.getLines.toList.par.foreach {u =>
+Source.stdin.getLines.toList.foreach {u =>
+	val url = new URL(u)
+
+	reset {
+		val actor = new URLActor()
+		actor.start
+
+		//URL接続処理
+		val stream = shift {k: (InputStream => Unit) =>
+			actor ! URLOpen(url, k)
+		}
+
+		//ダウンロード処理
+		val file = shift {k: (Path => Unit) =>
+			actor ! URLDownload(url, stream, dir, k)
+		}
+
+		printf("downloaded: %s => %s\n", url, file)
+		actor.stop
+	}
+}
