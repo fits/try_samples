b4f02a68a0e3295b6eb514b943afed761ca68e8f
fits <wadays_wozx@nifty.com>
2011-10-15 19:39:45 +0900

    非同期処理の処理内容を変更

--- blog/20111016/async_download_web.groovy
+++ blog/20111016/async_download_web.groovy
@@ -3,18 +3,20 @@ import groovyx.gpars.*
 def dir = args[0]
 
 GParsExecutorsPool.withPool {
+	//URL接続処理
+	def openUrl = { it.newInputStream() }.async()
+	//ダウンロード処理
+	def downloadUrl = { f, ou -> f.bytes = ou.get().bytes }.async()
+
 	System.in.readLines() collect {
 		def url = new URL(it)
 		def file = new File(dir, new File(url.file).name)
 
-		def stream = { url.newInputStream() }.callAsync()
-		def readStream = { stream.get().bytes }.callAsync()
-		def future = { file.bytes = readStream.get() }.callAsync()
+		[url: url, file: file, result: downloadUrl(file, openUrl(url))]
 
-		[url: url, file: file, future: future]
-	} each {
+	} eachParallel {
 		try {
-			it.future.get()
+			it.result.get()
 			println "downloaded: ${it.url} => ${it.file}"
 		} catch(e) {
 			println "failed: ${it.url}, $e"
--- groovy/gpars/download_web/async_download_web.groovy
+++ groovy/gpars/download_web/async_download_web.groovy
@@ -1,8 +1,4 @@
 import groovyx.gpars.*
-import java.nio.file.Paths
-import java.nio.ByteBuffer
-import java.nio.channels.AsynchronousFileChannel
-import static java.nio.file.StandardOpenOption.*
 
 if (args.length < 1) {
 	println "${new File(System.getProperty('script.name')).name} <output dir> [<parallel num>]"
@@ -12,18 +8,18 @@ if (args.length < 1) {
 def dir = args[0]
 
 GParsExecutorsPool.withPool {
+	def openUrl = { it.newInputStream() }.async()
+	def downloadUrl = { f, ou -> f.bytes = ou.get().bytes }.async()
+
 	System.in.readLines() collect {
 		def url = new URL(it)
 		def file = new File(dir, new File(url.file).name)
 
-		def stream = { url.newInputStream() }.callAsync()
-		def readStream = { stream.get().bytes }.callAsync()
-		def future = { file.bytes = readStream.get() }.callAsync()
+		[url: url, file: file, result: downloadUrl(file, openUrl(url))]
 
-		[url: url, file: file, future: future]
-	} each {
+	} eachParallel {
 		try {
-			it.future.get()
+			it.result.get()
 			println "downloaded: ${it.url} => ${it.file}"
 		} catch(e) {
 			println "failed: ${it.url}, $e"
