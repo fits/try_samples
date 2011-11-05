91269cc4ea289ddd83f8cd9775f07dea841c5395
fits <wadays_wozx@nifty.com>
2011-11-05 10:11:10 +0900

    複数ファイルのダウンロード処理を実装

--- groovy/camel/download_web/download_web.groovy
+++ groovy/camel/download_web/download_web.groovy
@@ -2,26 +2,37 @@
 @GrabResolver(name = "Apache Camel SNAPSHOTS", root = "https://repository.apache.org/content/repositories/snapshots/")
 @Grab("org.apache.camel:camel-core:2.9.0-SNAPSHOT")
 @Grab("org.apache.camel:camel-http:2.9.0-SNAPSHOT")
+@Grab("org.apache.camel:camel-stream:2.9.0-SNAPSHOT")
 import org.apache.camel.Processor
+import org.apache.camel.Exchange
 import org.apache.camel.impl.DefaultCamelContext
 import org.apache.camel.builder.RouteBuilder
+
+def dir = args[0]
 
 ctx = new DefaultCamelContext()
 
 ctx.addRoutes(new RouteBuilder() {
 	void configure() {
-		from("direct:start")
-			.to("http://www.google.co.jp/")
-			.process({println it.in.body.text} as Processor)
-			.end()
+		onException(Exception.class)
+			.handled(true)
+			.process({
+				println "failed: ${it.in.body}, ${it.exception}"
+			} as Processor)
+
+		from("stream:in")
+			.split(body().tokenize())
+			.setHeader(Exchange.HTTP_URI, body())
+			.to("http://localhost/")
+			.to("file:${dir}")
+			.process({
+				println "downloaded: " + header(Exchange.HTTP_URI).evaluate(it, String.class)
+			} as Processor)
 	}
 })
 
-
-template = ctx.createProducerTemplate()
-
-println "start"
-
 ctx.start()
 
-template.sendBody("direct:start", null)
+Thread.sleep(1000)
+
+ctx.shutdown()
