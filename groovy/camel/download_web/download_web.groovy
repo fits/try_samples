6715cc2cbb617ffdfb94557ebb6eda39afbcc51f
fits <wadays_wozx@nifty.com>
2011-11-06 11:14:55 +0900

    改良

--- groovy/camel/download_web/download_web.groovy
+++ groovy/camel/download_web/download_web.groovy
@@ -3,6 +3,8 @@
 @Grab("org.apache.camel:camel-core:2.9.0-SNAPSHOT")
 @Grab("org.apache.camel:camel-http:2.9.0-SNAPSHOT")
 @Grab("org.apache.camel:camel-stream:2.9.0-SNAPSHOT")
+//@Grab("org.slf4j:slf4j-simple:1.6.3")
+@Grab("org.slf4j:slf4j-jdk14:1.6.3")
 import org.apache.camel.Processor
 import org.apache.camel.Exchange
 import org.apache.camel.impl.DefaultCamelContext
@@ -14,19 +16,23 @@ ctx = new DefaultCamelContext()
 
 ctx.addRoutes(new RouteBuilder() {
 	void configure() {
+		//例外発生時の処理
 		onException(Exception.class)
 			.handled(true)
 			.process({
-				println "failed: ${it.in.body}, ${it.exception}"
+				println "failed: ${it.in.body}"
 			} as Processor)
 
 		from("stream:in")
 			.split(body().tokenize())
-			.setHeader(Exchange.HTTP_URI, body())
-			.to("http://localhost/")
 			.process({
-				it.in.setHeader(Exchange.FILE_NAME, it.in.getHeader(Exchange.HTTP_URI).split('/').last())
+				def url = it.in.body
+				//接続先URLの設定
+				it.in.setHeader(Exchange.HTTP_URI, url)
+				//出力ファイル名の設定
+				it.in.setHeader(Exchange.FILE_NAME, url.split('/').last())
 			} as Processor)
+			.to("http://localhost/")
 			.to("file:${dir}")
 			.process({
 				println "downloaded: ${it.in.getHeader(Exchange.HTTP_URI)} => ${it.in.getHeader(Exchange.FILE_NAME)}"
@@ -35,7 +41,5 @@ ctx.addRoutes(new RouteBuilder() {
 })
 
 ctx.start()
-
-Thread.sleep(1000)
 
 ctx.stop()
