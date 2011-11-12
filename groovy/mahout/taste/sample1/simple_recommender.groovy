7ee0aa3daca4c4dfcaa9855d56138bc810b8f9e5
fits <wadays_wozx@nifty.com>
2011-11-12 16:59:08 +0900

    Recommender を変更

--- groovy/mahout/taste/sample1/simple_recommender.groovy
+++ groovy/mahout/taste/sample1/simple_recommender.groovy
@@ -4,11 +4,17 @@
 import org.apache.mahout.cf.taste.impl.recommender.*
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
 
+if (args.length < 2) {
+	println "${new File(System.getProperty('script.name')).name} <csv file> <target userID>"
+	return
+}
+
 def data = new FileDataModel(new File(args[0]))
 
-def recommender = new ItemUserAverageRecommender(data)
+def recommender = new ItemAverageRecommender(data)
+//def recommender = new ItemUserAverageRecommender(data)
 
-recommender.recommend(1, 5).each {
+recommender.recommend(Long.parseLong(args[1]), 5).each {
 	println "result : ${it.itemID}, ${it.value}"
 }
 
