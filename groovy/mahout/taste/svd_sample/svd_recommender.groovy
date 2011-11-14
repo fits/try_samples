80d0565afb5899d8b55c5be50f3dbd096631a0eb
fits <wadays_wozx@nifty.com>
2011-11-15 00:56:16 +0900

    SVD のサンプル

---
+++ groovy/mahout/taste/svd_sample/svd_recommender.groovy
@@ -0,0 +1,21 @@
+@Grab("org.apache.mahout:mahout-core:0.5")
+@Grab("org.slf4j:slf4j-jdk14:1.6.3")
+//@Grab("org.slf4j:slf4j-nop:1.6.3")
+import org.apache.mahout.cf.taste.impl.recommender.svd.*
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
+
+if (args.length < 2) {
+	println "${new File(System.getProperty('script.name')).name} <csv file> <target userID>"
+	return
+}
+
+def data = new FileDataModel(new File(args[0]))
+
+def factorizer = new ALSWRFactorizer(data, 3, 0.05, 5)
+
+def recommender = new SVDRecommender(data, factorizer)
+
+recommender.recommend(Long.parseLong(args[1]), 5).each {
+	println "result : ${it.itemID}, ${it.value}"
+}
+
