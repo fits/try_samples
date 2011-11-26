77ce43e6784ce2369b12185e39848beba6f2c76e
fits <wadays_wozx@nifty.com>
2011-11-26 16:42:54 +0900

    グループを追加するサンプル

---
+++ groovy/zookeeper/sample/create_sample.groovy
@@ -0,0 +1,31 @@
+@Grapes([
+	@Grab("org.apache.zookeeper:zookeeper:3.4.0"),
+	@GrabExclude("com.sun.jmx#jmxri"),
+	@GrabExclude("com.sun.jdmk#jmxtools"),
+	@GrabExclude("javax.jms#jms")
+])
+import java.util.concurrent.CountDownLatch
+import org.apache.zookeeper.ZooKeeper
+import org.apache.zookeeper.Watcher
+import static org.apache.zookeeper.Watcher.Event.KeeperState.*
+import static org.apache.zookeeper.ZooDefs.Ids.*
+import static org.apache.zookeeper.CreateMode.*
+
+def signal = new CountDownLatch(1)
+
+def zk = new ZooKeeper("localhost", 5000, {event ->
+	println "process : ${event}"
+
+	if (event.state == SyncConnected) {
+		signal.countDown()
+	}
+} as Watcher)
+
+//ê⁄ë±ë“Çø
+signal.await()
+
+def createdPath = zk.create("/sample", "123".bytes, OPEN_ACL_UNSAFE, PERSISTENT )
+
+println "path : ${createdPath}"
+
+zk.close()
