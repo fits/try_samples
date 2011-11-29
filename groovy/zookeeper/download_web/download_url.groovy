cc2d9e58e9c48a9bd37f6f0ac11bef99e50182a6
fits <wadays_wozx@nifty.com>
2011-11-29 09:07:32 +0900

    カウントダウン用のクラス追加

--- groovy/zookeeper/download_web/download_url.groovy
+++ groovy/zookeeper/download_web/download_url.groovy
@@ -12,6 +12,28 @@ import static org.apache.zookeeper.Watcher.Event.KeeperState.*
 import static org.apache.zookeeper.ZooDefs.Ids.*
 import static org.apache.zookeeper.CreateMode.*
 
+class Counter {
+	private int initialCount
+	private int count
+
+	Counter(int count) {
+		this.initialCount = count
+		this.count = count
+	}
+
+	void countDown() {
+		count--
+	}
+
+	void reset() {
+		count = initialCount
+	}
+
+	boolean isValid() {
+		count > 0
+	}
+}
+
 def downloadUrl(URL url) {
 	try {
 		def f = "${args[0]}/${url.file.split('/').last()}"
@@ -39,31 +61,29 @@ def zk = new ZooKeeper("localhost", 5000, {event ->
 //Ú‘±‘Ò‚¿
 signal.await()
 
-def counter = 0
+def counter = new Counter(10)
 def root = "/download"
 
-while(counter < 10) {
+while(counter.isValid()) {
 	def list = zk.getChildren(root, false)
 
 	if (list) {
-		for (i in 0..<list.size()) {
-			def path = "${root}/${list.get(i)}"
+		def path = "${root}/${list.first()}"
 
-			try {
-				def data = zk.getData(path, false, null)
-				zk.delete(path, -1)
+		try {
+			def data = zk.getData(path, false, null)
+			zk.delete(path, -1)
 
-				downloadUrl(new URL(new String(data, "UTF-8")))
-				break
+			downloadUrl(new URL(new String(data, "UTF-8")))
 
-			} catch (KeeperException.NoNodeException e) {
-				println "no node : ${path}"
-			}
+		} catch (KeeperException.NoNodeException e) {
+			println "no node : ${path}"
 		}
-		counter = 0
+
+		counter.reset()
 	}
 	else {
-		counter++
+		counter.countDown()
 		Thread.sleep(1000)
 	}
 }
