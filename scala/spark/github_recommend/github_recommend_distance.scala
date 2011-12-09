1548aaacffc3f306dc32ddb2ceeebd8dc339cdad
fits <wadays_wozx@nifty.com>
2011-12-10 00:31:02 +0900

    コメント追加

--- scala/spark/github_recommend/github_recommend_distance.scala
+++ scala/spark/github_recommend/github_recommend_distance.scala
@@ -12,6 +12,7 @@ object GitHubRecommendDistance {
 		val file = spark.textFile(args(1))
 		val targetUser = args(2)
 
+		//アイテム単位の集計
 		val itemsRes = file.map { l =>
 			val items = l.split(",")
 			(items(3), (items(1), 1.0))
@@ -20,17 +21,21 @@ object GitHubRecommendDistance {
 			val target = users.find { case (user, _) => user == targetUser }
 
 			target match {
+				//ターゲットユーザーを含む場合はポイントの差を設定
 				case Some((_, targetPoint)) => users.map { case (user, point) =>
 					(user, abs(point - targetPoint))
 				}
+				//ターゲットユーザーを含まない場合は None を設定
 				case None => users.map { case (user, point) => (user, None) }
 			}
 		}
 
+		//ユーザー単位の集計
 		val usersRes = itemsRes.flatMap { case (item, users) =>
 			users.map { case (user, point) => (user, (item, point)) }
 
 		}.groupByKey().mapValues { items =>
+			//ポイントの合計値を算出
 			val total = items.foldLeft(0.0) { (subTotal, itemTuple) =>
 				itemTuple._2 match {
 					case p: Double => subTotal + 1.0 / (1.0 + p)
@@ -41,10 +46,14 @@ object GitHubRecommendDistance {
 			(total, items)
 		}
 
+		//ターゲットユーザー以外で
+		//ポイントの合計値が 7 より大きいユーザーを抽出
 		val pickupRes = usersRes.filter { case (user, (total, _)) =>
 			total > 7 && user != targetUser
 		}
 
+		//抽出されたユーザーの中でターゲットユーザーが関連していない
+		//アイテムをカウント
 		val res = pickupRes.flatMap { case (user, (total, items)) =>
 			items.filter { case (_, point) =>
 				point == None
@@ -53,8 +62,9 @@ object GitHubRecommendDistance {
 			}
 		}.reduceByKey(_ + _)
 
+		//sortBy のソート順を設定
 		implicit val order = Ordering.Int.reverse
-
+		//カウント数の多い上位 5件のアイテムを取り出して出力
 		res.collect.sortBy(_._2).take(5).foreach { case (item, point) =>
 			printf("%s : %d\n", item, point)
 		}
