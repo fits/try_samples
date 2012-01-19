302436e966878dc396282d588770b4d10fcbdaf2
fits <wadays_wozx@nifty.com>
2012-01-20 00:16:29 +0900

    XML出力版のサンプル追加

---
+++ groovy/resteasy-jaxrs/standalone/xml_sample.groovy
@@ -0,0 +1,48 @@
+@GrabResolver(name = 'jboss', root = 'http://repository.jboss.org/nexus/content/groups/public/')
+@Grab('org.jboss.resteasy:resteasy-jaxrs:2.3.1.GA')
+@Grab('org.jboss.resteasy:resteasy-jaxb-provider:2.3.1.GA')
+@Grab('org.jboss.resteasy:tjws:2.3.1.GA')
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.xml.bind.annotation.*
+
+import org.jboss.resteasy.plugins.server.tjws.TJWSEmbeddedJaxrsServer
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+class Customer {
+	@XmlElement
+	String id
+	@XmlElement
+	String name
+}
+
+@Path("list")
+public class SimpleResource {
+
+    @GET
+    @Produces(["application/xml"])
+    public List<Customer> getCustomerList() {
+		[
+			new Customer(id: "id:1", name: "テストデータ"),
+			new Customer(id: "id:2", name: "チェック")
+		]
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(["application/xml"])
+    public Customer getCustomer(@PathParam("id") String id) {
+		new Customer(id: id, name: "test")
+    }
+
+}
+
+def server = new TJWSEmbeddedJaxrsServer()
+
+server.setPort(8081)
+server.deployment.actualResourceClasses.add(SimpleResource.class)
+
+server.start()
