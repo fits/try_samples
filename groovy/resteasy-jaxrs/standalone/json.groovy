7148eca0f939f265d394e1a2d2a1fb9251237d6c
fits <wadays_wozx@nifty.com>
2012-01-19 23:53:39 +0900

    RESTEasy 2.3.1 に対応し、JSONの処理を変更

--- groovy/resteasy-jaxrs/standalone/json.groovy
+++ groovy/resteasy-jaxrs/standalone/json.groovy
@@ -1,17 +1,22 @@
-
+@GrabResolver(name = 'jboss', root = 'http://repository.jboss.org/nexus/content/groups/public/')
+@Grab('org.jboss.resteasy:resteasy-jaxrs:2.3.1.GA')
+@Grab('org.jboss.resteasy:resteasy-jettison-provider:2.3.1.GA')
+@Grab('org.jboss.resteasy:tjws:2.3.1.GA')
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
-
-import net.sf.json.groovy.JsonGroovyBuilder
-import net.sf.json.JSONSerializer
+import javax.xml.bind.annotation.*
 
 import org.jboss.resteasy.plugins.server.tjws.TJWSEmbeddedJaxrsServer
 
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
 class Customer {
-	def id
-	def name
+	@XmlElement
+	String id
+	@XmlElement
+	String name
 }
 
 @Path("list")
@@ -19,26 +24,18 @@ public class SimpleResource {
 
     @GET
     @Produces(["application/json"])
-    public String getCustomerList() {
-		def builder = new JsonGroovyBuilder()
-
-		return builder.customers {
-			customer {
-				id = "id:1"
-				name = "テストデータ"
-			}
-			customer {
-				id = "id:2"
-				name = "チェック"
-			}
-		}
+    public List<Customer> getCustomerList() {
+		[
+			new Customer(id: "id:1", name: "テストデータ"),
+			new Customer(id: "id:2", name: "チェック")
+		]
     }
 
     @GET
     @Path("{id}")
     @Produces(["application/json"])
-    public String getCustomer(@PathParam("id") String id) {
-		return JSONSerializer.toJSON(new Customer(id: id, name: "test"))
+    public Customer getCustomer(@PathParam("id") String id) {
+		new Customer(id: id, name: "test")
     }
 
 }
@@ -46,6 +43,6 @@ public class SimpleResource {
 def server = new TJWSEmbeddedJaxrsServer()
 
 server.setPort(8081)
-server.registry.addPerRequestResource(SimpleResource.class)
+server.deployment.actualResourceClasses.add(SimpleResource.class)
 
 server.start()
