f5d2c175d0bd8b6607d7881093202ed23718a252
fits <wadays_wozx@nifty.com>
2012-01-23 09:06:57 +0900

    入力値の制限を追加

--- java/resteasy/xml_sample/src/main/java/fits/sample/SampleResource.java
+++ java/resteasy/xml_sample/src/main/java/fits/sample/SampleResource.java
@@ -2,14 +2,23 @@ package fits.sample;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import javax.validation.constraints.*;
+
+import org.jboss.resteasy.spi.validation.ValidateRequest;
 
 @Path("sample")
+@ValidateRequest
 public class SampleResource {
 
 	@POST
 	@Path("data")
 	@Produces("application/xml")
-	public Data data(@FormParam("name") String name) {
+	public Data data(
+		@FormParam("name")
+		@NotNull
+		@Size(max=10)
+		String name) {
+
 		Data result = new Data();
 		result.name = name;
 		return result;
