14bda03d46e17dd14f86d50fa176fbf97163acad
fits <wadays_wozx@nifty.com>
2012-01-28 16:24:56 +0900

    @Aroundから@Beforeに変更

--- java/hibernate_validator/method_validate_sample_aop/src/main/java/fits/sample/MethodValidateAspect.java
+++ java/hibernate_validator/method_validate_sample_aop/src/main/java/fits/sample/MethodValidateAspect.java
@@ -12,22 +12,22 @@ import org.hibernate.validator.method.*;
 @Aspect
 public class MethodValidateAspect {
 
-	@Around("execution(void DataTester.test(..))")
-	public void checkMethod(ProceedingJoinPoint pjp) throws Throwable {
-		MethodSignature msig = (MethodSignature)pjp.getSignature();
+	@Before("call(* DataTester.*(..))")
+	public void checkMethod(JoinPoint jp) {
+		System.out.println("--- checkMethod : " + jp);
 
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		Validator validator = factory.getValidator();
 
 		MethodValidator mvalidator = validator.unwrap(MethodValidator.class);
 
-		Set<MethodConstraintViolation<Object>> violations = mvalidator.validateAllParameters(pjp.getThis(), msig.getMethod(), pjp.getArgs());
+		MethodSignature msig = (MethodSignature)jp.getSignature();
+
+		Set<MethodConstraintViolation<Object>> violations = mvalidator.validateAllParameters(jp.getTarget(), msig.getMethod(), jp.getArgs());
 
 		for (MethodConstraintViolation<Object> vi : violations) {
 			System.out.println("*** invalid : " + vi.getMessage());
 		}
-
-		pjp.proceed();
 	}
 
 }
