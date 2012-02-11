bd829960e2e884089d10ab21d18c3e6150eead7c
fits <wadays_wozx@nifty.com>
2012-02-12 00:33:40 +0900

    タスク名を変更

--- scala/sbt/sbt_0_11/plugins/groovy_sample_plugin/GroovySamplePlugin.scala
+++ scala/sbt/sbt_0_11/plugins/groovy_sample_plugin/GroovySamplePlugin.scala
@@ -8,11 +8,10 @@ import org.codehaus.groovy.tools.Compiler
 
 object GroovySamplePlugin extends Plugin {
 
-	val Config = config("groovy")
 	val groovySource = SettingKey[File]("groovy-source")
 	val outputDirectory = SettingKey[File]("groovy-output")
-	val sources = TaskKey[Seq[File]]("sources")
-	val compile = TaskKey[Unit]("compile", "Run Groovy compiler")
+	val sources = TaskKey[Seq[File]]("groovy-sources")
+	val compile = TaskKey[Unit]("groovy-compile", "Run Groovy compiler")
 
 	val groovySettings = Seq(
 		groovySource <<= sourceDirectory(_ / "main" / "groovy"),
