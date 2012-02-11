e03deb677e63090c792de768b40efb715e59efca
fits <wadays_wozx@nifty.com>
2012-02-11 11:18:23 +0900

    修正

--- scala/sbt/sbt_0_11/plugins/sample_plugin/SamplePlugin.scala
+++ scala/sbt/sbt_0_11/plugins/sample_plugin/SamplePlugin.scala
@@ -5,11 +5,11 @@ import sbt._
 object SamplePlugin extends Plugin {
 
 	val sampleName = SettingKey[String]("sample-name")
-	val sampleTask = TaskKey[Unit]("sampe-task")
+	val sampleTask = TaskKey[Unit]("sample-task")
 
 	val sampleSettings = Seq(
 		sampleName := "sample",
-		sampleTask <<= sampleName map { str => println(str) }
+		sampleTask <<= sampleName map { str => println("*** echo : " + str) }
 	)
 
 }
