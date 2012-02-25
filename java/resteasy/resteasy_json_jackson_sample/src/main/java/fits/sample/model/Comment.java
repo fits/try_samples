7f62ec0c9e791d4ef0ab88c4854c962135599c8e
fits <wadays_wozx@nifty.com>
2012-02-25 15:08:31 +0900

    クラス定義に含まないプロパティがJSONデータに存在した場合への対処

--- java/resteasy/resteasy_json_jackson_sample/src/main/java/fits/sample/model/Comment.java
+++ java/resteasy/resteasy_json_jackson_sample/src/main/java/fits/sample/model/Comment.java
@@ -1,8 +1,11 @@
 package fits.sample.model;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @XmlRootElement
+//クラス定義に存在しない JSON データのプロパティを無視するように設定
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Comment {
 	public String content;
 
