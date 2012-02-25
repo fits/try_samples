aff8796d9f6d8118e448f0da56a62e4fc3b172d2
fits <wadays_wozx@nifty.com>
2012-02-25 12:40:30 +0900

    関連データを含んだJSONをサーバーにPOSTする処理を実装

--- java/resteasy/resteasy_json_jackson_sample/src/main/webapp/app.js
+++ java/resteasy/resteasy_json_jackson_sample/src/main/webapp/app.js
@@ -15,5 +15,28 @@ Ext.application({
 	],
 
 	launch: function() {
+		Ext.data.writer.Json.override({
+			// getRecordData をオーバーライド
+			getRecordData: function() {
+				//オーバーライド元のメソッド呼び出し
+				var data = this.callOverridden(arguments);
+
+				var record = arguments[0];
+
+				if (record.associations.length > 0) {
+					Ext.Array.each(record.associations.keys, function(key) {
+						data[key] = [];
+
+						//関連毎の Store 取得
+						var assocStore = record[key]();
+
+						Ext.Array.each(assocStore.data.items, function(assocItem) {
+							data[key].push(assocItem.data);
+						});
+					});
+				}
+				return data;
+			}
+		});
 	}
 });
