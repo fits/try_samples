e2deeb54d49e195f6f445955ad8b40b14d118c80
fits <wadays_wozx@nifty.com>
2012-03-30 11:53:48 +0900

    Gen.choose を使った値の最小値と最大値の指定を追加

--- scala/scalaCheck/sample1_sbt/SampleSpecification.scala
+++ scala/scalaCheck/sample1_sbt/SampleSpecification.scala
@@ -2,10 +2,14 @@ package fits.sample
 
 import org.scalacheck._
 import org.scalacheck.Prop._
+import org.scalacheck.Gen._
 
-
-object SampleSpecification extends Properties("String") {
+object SampleSpecification extends Properties("Sample") {
 	property("startsWith") = forAll {(a: String, b: String) => 
 		(a + b).startsWith(a)
+	}
+
+	property("num choose") = forAll(choose(0, 10), choose(5, 9)) {(a: Int, b: Int) =>
+		a + b < 20
 	}
 }
