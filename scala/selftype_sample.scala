f4b452e08a660a8b6f06759a18e25a3d40d73db8
fits <wadays_wozx@nifty.com>
2012-04-04 00:25:25 +0900

    型定義を用いた自分型を追加

--- scala/selftype_sample.scala
+++ scala/selftype_sample.scala
@@ -4,7 +4,7 @@ class A { self =>
 
 	object B {
 		object C {
-			//自分型アノテーション self を使って A の test メソッドを呼び出す
+			//自分型 self を使って A の test メソッドを呼び出す
 			def test(msg: String) = self.test("C_test:" + msg)
 		}
 	}
@@ -13,3 +13,27 @@ class A { self =>
 val a = new A
 a.B.C.test("abc")
 
+println("---------------------")
+
+trait A1 {
+	def a1 = println("a1")
+}
+
+trait A2 {
+	def a2 = println("a2")
+}
+
+/**
+ * As の自分型 が A1 と A2 のトレイトであることを指定
+ */
+trait As { self: A1 with A2 =>
+	def a = {
+		a1
+		a2
+	}
+}
+
+// As をミックスインするには A1 と A2 もミックスインしなければならない
+object Sample extends As with A1 with A2
+
+Sample.a
