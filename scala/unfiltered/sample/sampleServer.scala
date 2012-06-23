d0f951cf847bb7b2f859d979e594761039bfe55c
fits <wadays_wozx@nifty.com>
2012-06-23 21:13:09 +0900

    html を返す処理追加

--- scala/unfiltered/sample/sampleServer.scala
+++ scala/unfiltered/sample/sampleServer.scala
@@ -7,6 +7,15 @@ object SampleServer {
 
 	def main(args: Array[String]) {
 		val sample = unfiltered.filter.Planify {
+			case GET(Path("/test.html")) => Html(
+				<html>
+				<body>
+					<h1>test</h1>
+				</body>
+				</html>
+			)
+
+			case GET(Path(Seg("users" :: id :: Nil))) => ResponseString("id = " + id)
 			case _ => ResponseString("sample")
 		}
 
