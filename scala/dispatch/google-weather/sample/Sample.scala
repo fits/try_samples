ab1820235c61833034d8d84f7600114f21ffc251
fits <wadays_wozx@nifty.com>
2012-06-29 00:45:01 +0900

    実行時引数のチェックを追加

--- scala/dispatch/google-weather/sample/Sample.scala
+++ scala/dispatch/google-weather/sample/Sample.scala
@@ -6,13 +6,16 @@ import XhtmlParsing._
 object Sample {
 
 	def main(args: Array[String]) {
+		if (args.length < 1) {
+			return
+		}
+
 		val req = url("http://www.google.com/ig/api?weather=" + args(0) + "&hl=ja")
 
 		println(req as_str)
 
 		Http(req </> { nodes =>
-			val weather = nodes \\ "current_conditions" \ "condition" \ "@data"
-			println(weather)
+			nodes \\ "current_conditions" \ "condition" \ "@data" foreach println
 		})
 
 	}
