22cdb6f19478653f38a4a1830011ad5e899008e6
fits <wadays_wozx@nifty.com>
2012-06-28 23:03:41 +0900

    compose追加

--- scala/scalaz/applicative/sample1/Sample.scala
+++ scala/scalaz/applicative/sample1/Sample.scala
@@ -8,5 +8,12 @@ object Sample {
 	def main(args: Array[String]) {
 		val r = (List(1, 2, 3, 4) |@| List(10, 20)) {_ + _}
 		println(r)
+
+		val f = (x: Int) => x * 2
+		val g = (y: Int) => y + 4
+
+		val r2 = g compose f apply 5
+		println(r2)
+
 	}
 }
