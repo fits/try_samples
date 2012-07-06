b8534968ea085bda4a45f435907b4484db0e0363
fits <wadays_wozx@nifty.com>
2012-07-07 00:47:26 +0900

    モジュール化した Vert.x RouteMatcher のサンプル

---
+++ groovy/vert.x/module/sample_web_module/sample_server.groovy
@@ -0,0 +1,25 @@
+
+import org.vertx.groovy.core.http.RouteMatcher
+
+Object[] run() {
+	println "** run ${vertx}, ${container}"
+
+	def rm = new RouteMatcher()
+
+	rm.get '/sample', { req ->
+		req.response.end 'test data'
+	}
+
+	rm.get '/sample/:id', { req ->
+		req.response.end "data : ${req.params['id']}"
+	}
+
+	server = vertx.createHttpServer()
+	server.requestHandler(rm.asClosure()).listen 8080
+}
+
+def vertxStop() {
+	server.close()
+	println "stop"
+}
+
