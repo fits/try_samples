0cd95df4c571d7aa93789c697f7c8f5bbb1159fb
fits <wadays_wozx@nifty.com>
2012-09-09 20:45:36 +0900

    Scalaz による関数合成サンプル

---
+++ scala/scalaz/kleisli/sample/Sample.scala
@@ -0,0 +1,32 @@
+package fits.sample
+
+import scalaz._
+import Scalaz._
+
+object Sample extends App {
+
+	val plus = (x: Int) => some(x + 3)
+	val times = (x: Int) => some(x * 2)
+
+	// Some(14)
+	println(some(4) >>= plus >>= times)
+	// Some(14)
+	println(some(4) >>= List(Kleisli(plus), Kleisli(times)).reduceLeft {(b, a) =>
+		b >=> a
+	})
+	// Some(14)
+	println(some(4) >>= List(Kleisli(plus), Kleisli(times)).reduceRight {(a, b) =>
+		b <=< a
+	})
+
+	// Some(11)
+	println(some(4) >>= List(Kleisli(plus), Kleisli(times)).reduceLeft {(b, a) =>
+		b <=< a
+	})
+	// Some(11)
+	println(some(4) >>= List(Kleisli(plus), Kleisli(times)).reduceRight {(a, b) =>
+		b >=> a
+	})
+
+
+}
