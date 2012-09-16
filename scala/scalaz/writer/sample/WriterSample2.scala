38e748d0afa251e078d99d568a6eea228a439129
fits <wadays_wozx@nifty.com>
2012-09-16 09:37:48 +0900

    for式版の Writer モナドサンプル追加

---
+++ scala/scalaz/writer/sample/WriterSample2.scala
@@ -0,0 +1,19 @@
+package fits.sample
+
+import scalaz._
+import Scalaz._
+
+object WriterSample2 extends App {
+
+	val logNum = (n: Int) => (x: Int) => WriterT.writer (s" + $n", n + x)
+
+	val st = WriterT.writer ("2", 2)
+
+	val r = for {
+		w <- st
+		w2 <- logNum(5)(w)
+		w3 <- logNum(3)(w2)
+	} yield w3
+
+	println(s"${r.written} = ${r.value}")
+}
