5ce41ca84e517cb99b0fd95846305d90932766cc
fits <wadays_wozx@nifty.com>
2012-09-23 11:21:19 +0900

    >>= を使った処理を追加

--- scala/scalaz/reader/sample/ReaderSample.scala
+++ scala/scalaz/reader/sample/ReaderSample.scala
@@ -5,11 +5,25 @@ import Scalaz._
 
 object ReaderSample extends App {
 
-	val f = for {
-		a <- Reader( (_: Int) * 2 )
-		b <- Reader( (_: Int) + 10 )
+	/* Reader[Int, Int] とすると _ が省略可能
+	 * Reader とすると _ の型指定が必要
+	 */
+	val f1 = for {
+		a <- Reader[Int, Int]( 2 * )
+		b <- Reader[Int, Int]( 10 + )
 		c <- Reader( (_: Int) - 5 )
 	} yield a + b + c
 
-	println(f(3))
+	println(f1(3))
+
+	// for と同等処理
+	val f1b = Reader[Int, Int]( 2 * ) >>= { a =>
+		Reader[Int, Int]( 10 + ) >>= { b =>
+			Reader[Int, Int]( -5 + ) >>= { c =>
+				Reader(_ => a + b + c)
+			}
+		}
+	}
+
+	println(f1b(3))
 }
