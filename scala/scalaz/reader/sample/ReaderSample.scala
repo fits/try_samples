da507c107c503b56d7afb18edd2384006ee77c4e
fits <wadays_wozx@nifty.com>
2012-09-23 10:31:27 +0900

    関数定義を削除

--- scala/scalaz/reader/sample/ReaderSample.scala
+++ scala/scalaz/reader/sample/ReaderSample.scala
@@ -5,13 +5,11 @@ import Scalaz._
 
 object ReaderSample extends App {
 
-	val plus = (x: Int) => (y: Int) => x + y
-	val multiply = (x: Int) => (y: Int) => x * y
-
 	val f = for {
-		a <- Reader(multiply(2))
-		b <- Reader(plus(10))
-	} yield a + b
+		a <- Reader( (_: Int) * 2 )
+		b <- Reader( (_: Int) + 10 )
+		c <- Reader( (_: Int) - 5 )
+	} yield a + b + c
 
 	println(f(3))
 }
