7e13b6bea44df0e4be7dd1c9fbc8008caf8479b8
fits <wadays_wozx@nifty.com>
2012-09-29 20:37:14 +0900

    3手先の移動位置取得を実装

--- java/functional_java/move_knight/MoveKnight.java
+++ java/functional_java/move_knight/MoveKnight.java
@@ -1,5 +1,5 @@
 import fj.*;
-import fj.data.List;
+import fj.data.*;
 import static fj.P.*;
 import static fj.Show.*;
 import static fj.data.List.*;
@@ -11,22 +11,38 @@ import static fj.data.List.*;
  */
 class MoveKnight {
 	public static void main(String[] args) {
-		listShow(p2Show(intShow, intShow)).println(moveKnight(pos(6, 2)));
 
+		Show<KnightPos> ks = show( f((KnightPos p) -> Stream.fromString("(" + p._1() + ", " + p._2() + ")")) );
+
+		listShow(ks).println( in3(pos(6, 2)) );
+
+		booleanShow.println( canReachIn3(pos(6, 2), pos(6, 1)) );
+		booleanShow.println( canReachIn3(pos(6, 2), pos(7, 3)) );
 	}
 
-	static List<P2<Integer, Integer>> moveKnight(P2<Integer, Integer> p) {
-		return list(
+	// ナイトの次の移動先を列挙
+	static F<KnightPos, List<KnightPos>> moveKnight() {
+		return f( (KnightPos p) -> list(
 			pos(p._1() + 2, p._2() - 1), pos(p._1() + 2, p._2() + 1),
 			pos(p._1() - 2, p._2() - 1), pos(p._1() - 2, p._2() + 1),
 			pos(p._1() + 1, p._2() - 2), pos(p._1() + 1, p._2() + 2),
 			pos(p._1() - 1, p._2() - 2), pos(p._1() - 1, p._2() + 2)
-		).filter(f( (P2<Integer, Integer> t) ->
+		).filter(f( (KnightPos t) ->
 			1<= t._1() && t._1() <= 8 && 1 <= t._2() && t._2() <= 8
-		));
+		)) );
 	}
 
-	// fj.F のインスタンスを生成するメソッド
+	// 3手先の移動位置を列挙
+	static List<KnightPos> in3(KnightPos start) {
+		return list(start).bind(moveKnight()).bind(moveKnight()).bind(moveKnight());
+	}
+
+	// 指定位置に3手で到達できるか否かを判定
+	static boolean canReachIn3(KnightPos start, KnightPos end) {
+		return in3(start).exists( f((KnightPos p) -> p._1() == end._1() && p._2() == end._2()) );
+	}
+
+	// fj.F のインスタンスを生成
 	static <A, B> F<A, B> f(final Func<A, B> func) {
 		return new F<A, B>() {
 			@Override
@@ -36,16 +52,15 @@ class MoveKnight {
 		};
 	}
 
-	static P2<Integer, Integer> pos(int x, int y) {
-		return p(x, y);
-		//return new KnightPos(x, y);
+	static KnightPos pos(int x, int y) {
+		return new KnightPos(x, y);
 	}
 
 	// ラムダ式が適用されるインターフェース
 	interface Func<A, B> {
 		B apply(A a);
 	}
-/*
+
 	static class KnightPos extends P2<Integer, Integer> {
 		private int x;
 		private int y;
@@ -58,5 +73,4 @@ class MoveKnight {
 		@Override public Integer _1() { return x; }
 		@Override public Integer _2() { return y; }
 	}
-*/
 }
