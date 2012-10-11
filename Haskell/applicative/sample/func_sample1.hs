1f99e9d322faaec4fe106e9b27b32a20b30f90ea
fits <wadays_wozx@nifty.com>
2012-10-11 20:53:54 +0900

    <*> を繋げた場合のサンプル追加

--- Haskell/applicative/sample/func_sample1.hs
+++ Haskell/applicative/sample/func_sample1.hs
@@ -3,5 +3,14 @@ import Control.Applicative
 
 main = do
 	let f = (+) <$> (*2) <*> (+10)
+	-- 22
 	putStrLn $ show $ f 4
+
+	let f2 = (+) <$> ( (+) <$> (*2) <*> (+10) ) <*> (+5)
+	-- 31
+	putStrLn $ show $ f2 4
+
+	let f3 = (\a b c -> a + b + c) <$> (*2) <*> (+10) <*> (+5)
+	-- 31
+	putStrLn $ show $ f3 4
 
