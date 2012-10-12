14c103428bd6acc19a67d18dda1ed1ee99da06e6
fits <wadays_wozx@nifty.com>
2012-10-12 22:46:45 +0900

    |> を使った実装を追加

--- blog/20120917/writer_scalaz/WriterSample.scala
+++ blog/20120917/writer_scalaz/WriterSample.scala
@@ -8,7 +8,9 @@ object WriterSample extends App {
 
 	val logNum = (n: Int) => (x: Int) => writer (s" + $n", n + x)
 
-	val r1 = writer ("2", 2) >>= logNum(5) >>= logNum(3)
+	val r1 = ("2", 2) |> writer >>= logNum(5) >>= logNum(3)
+	// 以下でも可
+	//val r1 = writer ("2", 2) >>= logNum(5) >>= logNum(3)
 
 	println(s"${r1.written} = ${r1.value}")
 
@@ -24,7 +26,9 @@ object WriterSample extends App {
 
 	val logNumList = (n: Int) => (x: Int) => writer (List(n), n + x)
 
-	val r3 = writer (List(2), 2) >>= logNumList(5) >>= logNumList(3)
+	val r3 = (List(2), 2) |> writer >>= logNumList(5) >>= logNumList(3)
+	// 以下でも可
+	//val r3 = writer (List(2), 2) >>= logNumList(5) >>= logNumList(3)
 
 	println(s"${r3.written} = ${r3.value}")
 }
--- blog/20120917/writer_scalaz/WriterTSample.scala
+++ blog/20120917/writer_scalaz/WriterTSample.scala
@@ -17,8 +17,9 @@ object WriterTSample extends App {
 		writerT(list)
 	}
 
-	val w = writerT(("2", 2) :: Nil) >>= logManyNum(3) >>= logManyNum(2)
-	//以下でも同じ
+	val w = ("2", 2) :: Nil |> writerT >>= logManyNum(3) >>= logManyNum(2)
+	//以下でも可
+	//val w = writerT(("2", 2) :: Nil) >>= logManyNum(3) >>= logManyNum(2)
 	//val w = writerT(List(("2", 2))) >>= logManyNum(3) >>= logManyNum(2)
 
 	println(s"written = ${w.written}")
