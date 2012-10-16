3596482cc257fb99ef1b1b6f2b615b71002a8331
fits <wadays_wozx@nifty.com>
2012-10-16 20:51:18 +0900

    |@| を使った処理を追加

--- blog/20121013/reader_applicative/ApplicativeSample.scala
+++ blog/20121013/reader_applicative/ApplicativeSample.scala
@@ -13,10 +13,15 @@ object ApplicativeSample extends App {
 	val f1a = ^( multiply(2) <*> plus(10) ) { case (a, b) => a + b }
 	println(f1a(4))
 
+	val f1b = (multiply(2) |@| plus(10)) { _ + _ }
+	println(f1b(4))
 
 	val f2 = multiply(2) <*> plus(10) <*> plus(5) >>> { case ((a, b), c) => a + b + c }
 	println(f2(4))
 
 	val f2a = ^( multiply(2) <*> plus(10) <*> plus(5) ) { case ((a, b), c) => a + b + c }
 	println(f2a(4))
+
+	val f2b = (multiply(2) |@| plus(10) |@| plus(5)) { _ + _ + _ }
+	println(f2b(4))
 }
