2269fc139ca9409a0ddf8546e4bdaa9c11bf10fc
fits <wadays_wozx@nifty.com>
2012-11-02 23:31:58 +0900

    callCC の入れ子を追加

--- Haskell/monad/continuation/sample2.hs
+++ Haskell/monad/continuation/sample2.hs
@@ -2,15 +2,27 @@
 import Control.Monad.Cont
 
 sample :: Int -> Cont r Int
-sample n = callCC $ \k -> do
+sample n = callCC $ \k1 -> do
 	when (odd n) $ do
-		k n
+		k1 n
 
-	return 0
+	r <- callCC $ \k2 -> do
+		when (n < 4) $ do
+			k2 $ n * 100
+
+		when (n < 6) $ do
+			k1 $ n * 10
+
+		return 0
+
+	return $ r + 1
 
 main = do
 	runCont (sample 1) print
 	runCont (sample 2) print
 	runCont (sample 3) print
 	runCont (sample 4) print
+	runCont (sample 5) print
+	runCont (sample 6) print
+	runCont (sample 7) print
 
