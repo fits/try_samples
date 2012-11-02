e518144897bd9b3ff6f03a34aa8e4919e4b22bd7
fits <wadays_wozx@nifty.com>
2012-11-02 23:06:25 +0900

    継続モナドのサンプル追加

--- Haskell/monad/continuation/sample.hs
+++ Haskell/monad/continuation/sample.hs
@@ -5,8 +5,16 @@ plus_cont :: Int -> Cont r Int
 plus_cont x = return (3 + x)
 
 calc_cont :: Int -> Cont r Int
-calc_cont a = plus_cont a >>= (\x -> return (x * 2))
+calc_cont a = plus_cont a >>= (\x -> return (x * 2)) >>= (\y -> return (y + 1))
+
+test :: a -> Cont r Int
+test _ = return 5
 
 main = do
 	runCont (plus_cont 2) print
 	runCont (calc_cont 2) print
+
+	runCont (calc_cont 4) print
+
+	runCont (test 1) (\x -> print x)
+
