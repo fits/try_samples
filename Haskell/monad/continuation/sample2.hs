ac411ec69344a6b1d96d509d897d11f27cfb4ecc
fits <wadays_wozx@nifty.com>
2012-11-02 23:54:28 +0900

    コメント追加

--- Haskell/monad/continuation/sample2.hs
+++ Haskell/monad/continuation/sample2.hs
@@ -4,25 +4,31 @@ import Control.Monad.Cont
 sample :: Int -> Cont r Int
 sample n = callCC $ \k1 -> do
 	when (odd n) $ do
+		-- (1)
 		k1 n
 
 	r <- callCC $ \k2 -> do
 		when (n < 4) $ do
+			-- (2)
 			k2 $ n * 100
 
-		when (n < 6) $ do
+		when (n > 6) $ do
+			-- (3)
 			k1 $ n * 10
 
+		-- (4)
 		return 0
 
+	-- (5)
 	return $ r + 1
 
 main = do
-	runCont (sample 1) print
-	runCont (sample 2) print
-	runCont (sample 3) print
-	runCont (sample 4) print
-	runCont (sample 5) print
-	runCont (sample 6) print
-	runCont (sample 7) print
+	runCont (sample 1) print -- (1)
+	runCont (sample 2) print -- (2) (4)
+	runCont (sample 3) print -- (1)
+	runCont (sample 4) print -- (4) (5)
+	runCont (sample 5) print -- (1)
+	runCont (sample 6) print -- (4) (5)
+	runCont (sample 7) print -- (1)
+	runCont (sample 8) print -- (3)
 
