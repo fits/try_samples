ccb9a47f938d881c4cf181d9e55bb89a551fe71b
fits <wadays_wozx@nifty.com>
2012-11-03 13:57:21 +0900

    継続モナドのサンプル追加

---
+++ Haskell/monad/continuation/sample2.hs
@@ -0,0 +1,26 @@
+
+import Control.Monad.Cont
+
+calc1 :: Int -> Cont r Int
+calc1 x = return (x + 3)
+
+calc2 :: Int -> Cont r Int
+calc2 x = return (x * 10)
+
+calc3 :: Int -> Cont r Int
+calc3 x = return (x + 4)
+
+calcAll :: Int -> Cont r Int
+calcAll n = return n >>= calc1 >>= calc2 >>= calc3
+
+main = do
+	-- 2 + 3
+	runCont (calc1 2) print
+
+	-- ((2 + 3) * 10) + 4
+	runCont (calcAll 2) print
+	-- ã‹L‚ÍˆÈ‰º‚Æ“¯‚¶
+	-- runCont (calcAll 2) (\x -> print x)
+
+	print $ runCont (calcAll 2) (\x -> x - 9)
+
