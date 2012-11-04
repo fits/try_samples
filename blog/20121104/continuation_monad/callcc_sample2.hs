7cba75316e57129eb5e5c563d2783265f5193ad2
fits <wadays_wozx@nifty.com>
2012-11-04 10:40:38 +0900

    変数名を変更

--- blog/20121104/continuation_monad/callcc_sample2.hs
+++ blog/20121104/continuation_monad/callcc_sample2.hs
@@ -7,7 +7,7 @@ sample n = callCC $ \cc1 -> do
 		-- (1)
 		cc1 n
 
-	r <- callCC $ \cc2 -> do
+	x <- callCC $ \cc2 -> do
 		when (n < 4) $ do
 			-- (2)
 			cc2 (n * 1000)
@@ -20,7 +20,7 @@ sample n = callCC $ \cc1 -> do
 		return (n * 10)
 
 	-- (5)
-	return (r + 1)
+	return (x + 1)
 
 main = do
 	runCont (sample 1) print -- (1)
