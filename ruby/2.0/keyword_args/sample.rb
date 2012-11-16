9cbd12a5a597bb5b0596d6f9a9c0c5de3001de5b
fits <wadays_wozx@nifty.com>
2012-11-17 00:54:11 +0900

    Ruby 2.0 キーワード引数のサンプル追加

---
+++ ruby/2.0/keyword_args/sample.rb
@@ -0,0 +1,13 @@
+
+def sample(name: "sample", num: 1)
+	puts "#{name}, #{num}"
+end
+
+sample
+sample(name: "test", num: 10)
+sample(name: "test2")
+sample(num: 5)
+
+# ˆÈ‰º‚Í•s‰Â
+#sample("aaa", 3)
+
