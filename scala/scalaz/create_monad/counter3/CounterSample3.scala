4d82e0353b3fb0a789b3e00159d02f229bc16333
fits <wadays_wozx@nifty.com>
2012-11-23 14:56:29 +0900

    カウンターを1からはじめる処理を追加

--- scala/scalaz/create_monad/counter3/CounterSample3.scala
+++ scala/scalaz/create_monad/counter3/CounterSample3.scala
@@ -20,14 +20,23 @@ trait CounterInstances {
 case object Counter extends CounterInstances
 
 object CounterSample3 extends App {
-	val point = (x: String) => Counter.counterInstance(intInstance).point(x)
-
 	val countUp = (s: String) => (x: String) => Counter (x + s, 1)
 
+	val point = (x: String) => Counter.counterInstance(intInstance).point(x)
 	// (a,0)
 	point("a").count |> println
 	// (ab,1)
 	( point("a") >>= countUp("b") ).count |> println
 	// (abc,2)
 	( point("a") >>= countUp("b") >>= countUp("c") ).count |> println
+
+	println("-----")
+
+	val point2 = (x: String) => Counter (x, 1)
+	// (a,1)
+	point2("a").count |> println
+	// (ab,2)
+	( point2("a") >>= countUp("b") ).count |> println
+	// (abc,3)
+	( point2("a") >>= countUp("b") >>= countUp("c") ).count |> println
 }
