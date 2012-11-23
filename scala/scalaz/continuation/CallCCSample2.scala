bc63fffdc5e27fbdbdbc55a62da49c59c0e6885e
fits <wadays_wozx@nifty.com>
2012-11-23 23:58:36 +0900

    caseの利用とコメント追加

--- scala/scalaz/continuation/CallCCSample2.scala
+++ scala/scalaz/continuation/CallCCSample2.scala
@@ -9,31 +9,26 @@ object CallCCSample2 extends App {
 	type CtInt[R] = Continuation[R, Int]
 
 	def sample[R](n: Int): CtInt[R] = callCC { cc1: (Int => CtInt[R]) =>
-
 		if(n % 2 == 1) {
-			cc1(n)
+			cc1(n) // (1)
 		}
 		else {
 			for {
-				x <- callCC { cc2: (Int => CtInt[R]) => 
-					if (n < 4) {
-						cc2(n * 1000)
-					}
-					else if (n == 4) {
-						cc1(n * 100)
-					}
-					else {
-						continuationInstance[R].point(n * 10)
+				x <- callCC { cc2: (Int => CtInt[R]) =>
+					n match {
+						case x if (x < 4) => cc2(n * 1000) // (2)
+						case 4 => cc1(n * 100) // (3)
+						case _ => continuationInstance[R].point(n * 10) // (4)
 					}
 				}
-			} yield (x + 1)
+			} yield (x + 1) // (5)
 		}
 	}
 
-	sample(1).runCont { println }
-	sample(2).runCont { println }
-	sample(3).runCont { println }
-	sample(4).runCont { println }
-	sample(5).runCont { println }
-	sample(6).runCont { println }
+	sample(1).runCont { println } // (1)
+	sample(2).runCont { println } // (2) (5)
+	sample(3).runCont { println } // (1)
+	sample(4).runCont { println } // (3)
+	sample(5).runCont { println } // (1)
+	sample(6).runCont { println } // (4) (5)
 }
