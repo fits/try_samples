07d5dad2eff156d6cf345605211fb30ca64b6cdb
fits <wadays_wozx@nifty.com>
2012-11-25 11:54:54 +0900

    変数名を変更

--- blog/20121125/continuation_monad_scalaz/CallCCSample.scala
+++ blog/20121125/continuation_monad_scalaz/CallCCSample.scala
@@ -6,9 +6,9 @@ import Scalaz._
 object CallCCSample extends App {
 	import Cont._
 
-	def sample[R](n: Int): Cont[R, Int] = callCC { cc1: (Int => Cont[R, Int]) =>
+	def sample[R](n: Int): Cont[R, Int] = callCC { cc: (Int => Cont[R, Int]) =>
 		if(n % 2 == 1) {
-			cc1(n) // (1)
+			cc(n) // (1)
 		}
 		else {
 			contInstance[R].point(n * 10) // (2)
