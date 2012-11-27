ac0ea18f7af20119bdb1906f361403d8b69503d0
fits <wadays_wozx@nifty.com>
2012-11-28 00:51:26 +0900

    サンプル追加

--- scala/scalaz/codensity/CodensitySample.scala
+++ scala/scalaz/codensity/CodensitySample.scala
@@ -6,4 +6,11 @@ import Scalaz._
 object CodensitySample extends App {
 
 	Codensity.pureCodensity(5).apply { (x: Int) => () => println(x) }()
+
+	Codensity.pureCodensity(5).apply { (x: Int) => List(x) } |> println
+
+	Codensity.rep(List(1, 2)).apply { (x) => List(x, 2, 3)} |> println
+
+	((Codensity.pureCodensity(5).improve >>= { (x: Int) => Codensity.pureCodensity[Function0, Int](x + 3) }) apply { (x) => () => println(x) })()
+
 }
