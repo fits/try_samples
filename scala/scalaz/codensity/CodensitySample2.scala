7f4f3cfcff6fb72f8b6626a72034d3e227d8808f
fits <wadays_wozx@nifty.com>
2012-12-01 23:46:30 +0900

    サンプル追加

--- scala/scalaz/codensity/CodensitySample2.scala
+++ scala/scalaz/codensity/CodensitySample2.scala
@@ -12,10 +12,17 @@ object CodensitySample2 extends App {
 
 	def calcAll[F[+_]](x: Int) = cont[F](x).flatMap(calc1).flatMap(calc2)
 
-	calc1(2).apply { (a) => () => println(a) }()
+	calc1(2).apply { (x) => () => println(x) }()
 	calc1(2).apply { Option(_) } |> println
+	calc1(2).apply { Option(_) }.println
+	calc1(2).apply { Option(_) } foreach(println)
+
+
+	calc1(2).apply { Value(_) } |> println
+	calc1(2).apply { List(_) } |> println
 
 	calcAll(2).apply { (x) => () => println(x) }()
 	calcAll(2).apply { (x) => () => x - 9 }() |> println
+	calcAll(2).apply { (x) => Option(x - 9) } foreach(println)
 
 }
