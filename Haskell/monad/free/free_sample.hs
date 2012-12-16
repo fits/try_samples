11419ec9cd1f78fec4bd97d3c0fc910651c88257
fits <wadays_wozx@nifty.com>
2012-12-16 10:27:04 +0900

    関数追加

--- Haskell/monad/free/free_sample.hs
+++ Haskell/monad/free/free_sample.hs
@@ -1,11 +1,25 @@
 import Control.Monad.Free
 
-data Greeting a n = Hello a n | Bye
+data Greeting a next = Hello a next | Bye
 	deriving (Show)
 
 instance Functor (Greeting a) where
-	fmap f (Hello a n) = Hello a (f n)
+	fmap f (Hello a next) = Hello a (f next)
 	fmap f Bye = Bye
 
+hello :: a -> Free (Greeting a) ()
+hello x = Impure (Hello x (Pure ()))
+
+bye :: Free (Greeting a) r
+bye = Impure Bye
+
+sampleData :: Free (Greeting [Char]) ()
+sampleData = do
+	hello "one"
+	hello "two"
+	hello "three"
+	bye
+
 main = do
-	print $ Impure (Hello "sample" (Pure ()))
+	print $ hello "sample"
+	print $ sampleData
