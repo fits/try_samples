d15eafeae76e26cd2f5938ed2683d89abcedacdb
fits <wadays_wozx@nifty.com>
2012-12-29 17:36:57 +0900

    Through を使った処理追加

--- scala/play2-mini/sample/SampleApp.scala
+++ scala/play2-mini/sample/SampleApp.scala
@@ -5,9 +5,25 @@ import play.api.mvc._
 import play.api.mvc.Results._
 
 object SampleApp extends Application {
-	def route = {
-		case GET(Path("/")) => Action {
-			Ok("sample")
+	def route = Routes(
+		{
+			case GET(Path("/")) => Action {
+				Ok("sample")
+			}
+		},
+		// "/a/" 以降を / で区切ってリスト化したものが groups となる
+		Through("/a/") { groups: List[String] =>
+			Action {
+				println(groups)
+				Ok("params : " + groups)
+			}
+		},
+		// "/b/xxx" の xxx 部分が groups となる
+		Through("/b/(.*)".r) { groups: List[String] =>
+			Action {
+				println(groups)
+				Ok("params : " + groups)
+			}
 		}
-	}
+	)
 }
