6e9cb8da21e0b701c385d9f7c809e7bbfbffdc69
fits <wadays_wozx@nifty.com>
2013-01-02 11:23:21 +0900

    Play2 Mini で JSON を GET・POST する単純なサンプル

---
+++ scala/play2-mini/rest_json_sample/SampleApp.scala
@@ -0,0 +1,32 @@
+package fits.sample
+
+import com.typesafe.play.mini._
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.libs.json._
+
+object SampleApp extends Application {
+
+	def route = Routes(
+		Through("/user/([^/]*)".r) { groups: List[String] =>
+			Action {
+				val id :: Nil = groups
+
+				Ok(Json.toJson {
+					Map(
+						"id" -> id,
+						"name" -> "test"
+					)
+				})
+			}
+		},
+		{
+			case POST(Path("/user")) => Action { req =>
+				val data = req.body.asJson
+				data.foreach(println)
+
+				Ok("")
+			}
+		}
+	)
+}
