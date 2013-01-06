333d0c4eced0bc5ac2545eb113c1502c6643e0c6
fits <wadays_wozx@nifty.com>
2013-01-06 16:40:35 +0900

    JSON データをブロードキャストする処理を実装

--- scala/play2-mini/websocket_chat/SampleApp.scala
+++ scala/play2-mini/websocket_chat/SampleApp.scala
@@ -8,13 +8,20 @@ import play.api.libs.iteratee._
 
 object SampleApp extends Application {
 
+	var wsList = List[PushEnumerator[JsValue]]()
+
 	def route = Routes({
 		case GET(Path("/connect")) => WebSocket.using[JsValue] { req =>
 			val out = Enumerator.imperative[JsValue]()
 
+			wsList = wsList :+ out
+
 			val in = Iteratee.foreach[JsValue] { json =>
-				out.push(json)
-			}.mapDone { _ => println("*** closed") }
+				wsList.foreach( _.push(json) )
+			}.mapDone { _ => 
+				wsList = wsList.filterNot { _ == out }
+				println("*** closed")
+			}
 
 			(in, out)
 		}
