c24d8c064627f750cd6b58bf77beafd0a1605580
fits <wadays_wozx@nifty.com>
2013-01-12 00:59:24 +0900

    STM を使うように変更

--- scala/play2-mini/websocket_chat/SampleApp.scala
+++ scala/play2-mini/websocket_chat/SampleApp.scala
@@ -5,21 +5,26 @@ import play.api.mvc._
 import play.api.mvc.Results._
 import play.api.libs.json._
 import play.api.libs.iteratee._
+import scala.concurrent.stm._
 
 object SampleApp extends Application {
 
-	var wsList = List[PushEnumerator[JsValue]]()
+	val wsList = Ref(List[PushEnumerator[JsValue]]())
 
 	def route = Routes({
 		case GET(Path("/connect")) => WebSocket.using[JsValue] { req =>
 			val out = Enumerator.imperative[JsValue]()
 
-			wsList = wsList :+ out
+			wsList.single.transform( _ :+ out )
 
 			val in = Iteratee.foreach[JsValue] { json =>
-				wsList.foreach( _.push(json) )
+				atomic { implicit txn =>
+					wsList.get.foreach( _.push(json) )
+				}
 			}.mapDone { _ => 
-				wsList = wsList.filterNot { _ == out }
+				wsList.single.transform {
+					_.filterNot( _ == out )
+				}
 				println("*** closed")
 			}
 
