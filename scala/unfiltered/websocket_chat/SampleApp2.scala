12891493d0818986bed38e64265f4edb7f6c829e
fits <wadays_wozx@nifty.com>
2013-01-13 11:35:37 +0900

    コメント追加

--- scala/unfiltered/websocket_chat/SampleApp2.scala
+++ scala/unfiltered/websocket_chat/SampleApp2.scala
@@ -10,27 +10,31 @@ object SampleApp2 extends Application {
 
 	implicit val system = ActorSystem("sample")
 
+	// WebSocket のリスト
 	val wsList = Agent(List[WebSocket]())
 
 	val wsHandle = Planify {
 		case Path("/connect") => {
-			case Open(socket) => wsList.send( _ :+ socket )
+			case Open(socket) => wsList.send( _ :+ socket ) // リストへの追加
 
 			case Close(socket) =>
+				// リストから削除
 				wsList.send {
 					_.filterNot( _ == socket )
 				}
 				println("*** closed")
 
 			case Message(socket, Text(txt)) => 
+				// リストを取得してメッセージ送信
 				wsList().foreach( _.send(txt) )
-
 
 			case Error(socket, err) => println(s"error : ${err}")
 		}
 	}
 
 	Http.local(8080).handler(wsHandle).beforeStop {
+		// Agent の終了処理
+		// ただし、beforeStop は Ctrl + C で終了すると実行されない
 		wsList.close()
 	}.start()
 }
