ec31ebad0de594ab41587135b0b51c0c66967f1d
fits <wadays_wozx@nifty.com>
2013-01-15 00:34:00 +0900

    Play2 Mini 2.1 RC2 ベースの WebSocket サンプル追加

---
+++ scala/play2-mini/websocket_chat2/SampleApp.scala
@@ -0,0 +1,23 @@
+package fits.sample
+
+import com.typesafe.play.mini._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.iteratee._
+
+object SampleApp extends Application {
+
+	val (enumerator, channel) = Concurrent.broadcast[JsValue]
+
+	def route = Routes({
+		case GET(Path("/connect")) => WebSocket.using[JsValue] { req =>
+			val in = Iteratee.foreach[JsValue] { json =>
+				channel.push(json)
+			}.mapDone { _ => 
+				println("closed")
+			}
+
+			(in, enumerator)
+		}
+	})
+}
