69936318be0e36e56ef54aa63a287759ac31cde5
fits <wadays_wozx@nifty.com>
2013-01-26 21:35:05 +0900

    add Iteratee sample

--- scala/scalaz/iteratee/sample/IterateeSample.scala
+++ scala/scalaz/iteratee/sample/IterateeSample.scala
@@ -3,10 +3,12 @@ package fits.sample
 import scalaz._
 import Scalaz._
 import iteratee._
+import Iteratee._
 
 object IterateeSample extends App {
-	val enumerator = Iteratee.enumList[Int, Id] { (1 to 5).toList }
+	val enumerator = Iteratee.enumList[Int, Id] { (1 to 10).toList }
 
-	(Iteratee.take[Int, List](3) &= enumerator).run |> println
+	(take[Int, List](3) &= enumerator).run |> println
 
+	(collect[List[Int], List] %= group(2) &= enumerator).run |> println
 }
