2732fb5112aba277bca41fe4f872951f45da4832
fits <wadays_wozx@nifty.com>
2013-02-14 08:10:31 +0900

    Enumerator.fromCallback1 を使ったサンプル追加

---
+++ scala/play2/iteratee_file/SampleApp3.scala
@@ -0,0 +1,40 @@
+package fits.sample
+
+import play.api.libs.iteratee._
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
+
+import java.io.{BufferedReader, FileReader}
+
+object SampleApp3 extends App {
+	import scala.concurrent.ExecutionContext.Implicits.global
+
+	def fromStreamLine(input: BufferedReader) = {
+		Enumerator.fromCallback1(_ => {
+			val chunk = input.readLine() match {
+				case line: String => Some(line)
+				case _            => None
+			}
+			Future.successful(chunk)
+		}, {println("*** close");input.close})
+	}
+
+	val enumerator = fromStreamLine(new BufferedReader(new FileReader(args(0))))
+
+	val future = enumerator |>>> Iteratee.foreach { s => 
+		println(s"#${s}")
+	}
+
+	Await.ready(future, Duration.Inf)
+
+	println("----------")
+
+	val enumerator2 = fromStreamLine(new BufferedReader(new FileReader(args(0))))
+	val future2 = enumerator2 &> Enumeratee.take(3) |>>> Iteratee.foreach { s => 
+		println(s"#${s}")
+	}
+
+	Await.ready(future2, Duration.Inf)
+
+}
