b6e3af5523b75545733990b3d4977465cda42506
fits <wadays_wozx@nifty.com>
2013-02-17 12:15:16 +0900

    モナド則を満たすように修正

--- scala/scalaz/create_monad/counter/CounterSample.scala
+++ scala/scalaz/create_monad/counter/CounterSample.scala
@@ -7,11 +7,11 @@ case class Counter[A](val count: (A, Int))
 
 trait CounterInstances {
 	implicit val counterInstance = new Monad[Counter] {
-		def point[A](a: => A): Counter[A] = Counter (a, 1)
+		def point[A](a: => A): Counter[A] = Counter (a, 0)
 		def bind[A, B](fa: Counter[A])(f: (A) => Counter[B]): Counter[B] = {
 			val (a, c) = fa.count
-			val (b, _) = f(a).count
-			Counter (b, c + 1)
+			val (b, d) = f(a).count
+			Counter (b, c + d)
 		}
 	}
 }
@@ -21,11 +21,12 @@ case object Counter extends CounterInstances
 object CounterSample extends App {
 	import Counter.counterInstance.point
 
-	val countUp = (s: String) => (x: String) => point(x + s)
+	val countUp = (s: String) => (x: String) => Counter (x + s, 1)
 
 	point("a").count |> println
 
 	( point("a") >>= countUp("b") ).count |> println
+	( countUp("b")("a") ).count |> println
 
 	( point("a") >>= countUp("b") >>= countUp("c") ).count |> println
 }
--- scala/scalaz/create_monad/counter/build.sbt
+++ scala/scalaz/create_monad/counter/build.sbt
@@ -1,7 +1,7 @@
-scalaVersion := "2.10.0-RC2"
+scalaVersion := "2.10.0"
 
 scalacOptions += "-Xprint:typer"
 
-libraryDependencies += "org.scalaz" % "scalaz-core" % "7.0.0-M4" cross CrossVersion.full
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.0.0-M7"
 
 mainClass in (Compile, run) := Some("fits.sample.CounterSample")
