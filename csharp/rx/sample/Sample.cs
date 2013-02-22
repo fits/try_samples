8fdac0891a0adbfaeb49983062f67bb1744859e2
fits <wadays_wozx@nifty.com>
2013-02-23 01:57:06 +0900

    Rx の処理を追加

--- csharp/rx/sample/Sample.cs
+++ csharp/rx/sample/Sample.cs
@@ -7,10 +7,25 @@ class Sample
 {
 	static void Main(string[] args)
 	{
-		var rx = from x in Enumerable.Range(1, 10) where x % 3 == 0 select x + 10;
-		foreach (var r in rx)
+		// ’Êí‚Ì Linq ‚Ìˆ—
+		var em = from x in Enumerable.Range(1, 10) where x % 3 == 0 select x + 10;
+		foreach (var x in em)
 		{
-			Console.WriteLine(r);
+			Console.WriteLine(x);
 		}
+
+		Console.WriteLine("-----");
+
+		// Rx ‚Ìˆ—1
+		Observable.Range(1, 10).Subscribe(Console.WriteLine);
+
+		Console.WriteLine("-----");
+
+		// Rx ‚Ìˆ—2
+		(from x in Observable.Range(1, 10) where x % 3 == 0 select x + 10).Subscribe(Console.WriteLine);
+
+		Observable.Range(1, 10).Where(x => x % 3 == 0).Select(x => x + 10).Subscribe(Console.WriteLine);
+
+		
 	}
 }
