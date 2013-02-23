b7678831e81f54804c1776223bfdb86be3c409ad
fits <wadays_wozx@nifty.com>
2013-02-24 00:20:42 +0900

    async/await を使った非同期処理の追加

--- csharp/rx/sample/FileSample.cs
+++ csharp/rx/sample/FileSample.cs
@@ -15,8 +15,13 @@ class FileSample
 
 		FromFile(args[0]).Skip(1).Take(2).Select(x => "#" + x).Subscribe(Console.WriteLine);
 
+		Console.WriteLine("-----");
+
+		// 下記は Mono 3.0.3 では動作しない （.NET Framework 4.5 では動作する）
+		FromAsyncFile(args[0]).Skip(1).Take(2).Select(x => "#" + x).Subscribe(Console.WriteLine);
 	}
 
+	// 同期処理版
 	private static IObservable<string> FromFile(string fileName)
 	{
 		return Observable.Create<string>(observer => {
@@ -24,11 +29,34 @@ class FileSample
 			{
 				using(var reader = File.OpenText(fileName))
 				{
-					string s = null;
-
-					while ((s = reader.ReadLine()) != null)
+					while (!reader.EndOfStream)
 					{
-						observer.OnNext(s);
+						observer.OnNext(reader.ReadLine());
+					}
+
+					observer.OnCompleted();
+					Console.WriteLine("*** close");
+				}
+			}
+			catch (Exception error) {
+				observer.OnError(error);
+			}
+			return Disposable.Empty;
+		});
+	}
+
+	// 非同期処理版 （Mono 3.0.3 では正常に動作しない）
+	private static IObservable<string> FromAsyncFile(string fileName)
+	{
+		return Observable.Create<string>(async observer => {
+			try
+			{
+				using(var reader = File.OpenText(fileName))
+				{
+					while (!reader.EndOfStream)
+					{
+						var line = await reader.ReadLineAsync();
+						observer.OnNext(line);
 					}
 
 					observer.OnCompleted();
