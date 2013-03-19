dc140785fdfad9002b9b8a5b1375782e50ab953f
fits <wadays_wozx@nifty.com>
2013-03-20 00:59:21 +0900

    JMeter 用のテストサンプル

---
+++ java/jmeter/sample/sample_server.groovy
@@ -0,0 +1,14 @@
+import org.vertx.groovy.core.http.RouteMatcher
+
+def rm = new RouteMatcher()
+
+rm.get '/sample', { req ->
+	println req.headers['Host']
+	req.response.end 'test data'
+}
+
+rm.get '/sample/:id', { req ->
+	req.response.end "data : ${req.params['id']}"
+}
+
+vertx.createHttpServer().requestHandler(rm.asClosure()).listen 8080
