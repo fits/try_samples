eb53ac4fcb444e0a0cff6f71fd89c6b1aa70c0ea
fits <wadays_wozx@nifty.com>
2013-07-07 09:35:39 +0900

    仮想マシンの名称を変更するサンプル IronPython 版

---
+++ IronPython/VirtualBox/vbox_rename.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+#
+# VirtualBox の仮想マシンのリネームを行うサンプル
+
+import sys
+
+if len(sys.argv) < 3:
+	print "%s <machine name> <new name>" % sys.argv[0]
+	sys.exit()
+
+import os
+
+import clr
+clr.AddReference("VirtualBox")
+
+from VirtualBox import *
+
+vb = VirtualBoxClass()
+
+# 仮想マシンの取得
+mc = vb.FindMachine(sys.argv[1])
+
+print "current name : %s" % mc.Name
+
+session = SessionClass()
+# 設定変更用にロックする
+mc.LockMachine(session, LockType.LockType_Write)
+
+# 変更用の仮想マシンを取得
+upMc = session.Machine
+
+upMc.Name = sys.argv[2]
+# 変更の適用
+upMc.SaveSettings()
+
+# ロックを解除する
+session.UnlockMachine()
+
+print "new name : %s" % mc.Name
