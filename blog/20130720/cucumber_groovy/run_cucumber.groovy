9b4c58e407e43c30835c8ddfafdfb38b8c7d3b27
fits <wadays_wozx@nifty.com>
2013-07-20 17:25:34 +0900

    blog サンプル: Groovy による cucumber-jvm の実行サンプル

---
+++ blog/20130720/cucumber_groovy/run_cucumber.groovy
@@ -0,0 +1,8 @@
+@Grab('info.cukes:cucumber-groovy:1.1.3')
+import cucumber.api.cli.Main
+
+//def loader = Thread.currentThread().getContextClassLoader()
+
+//Main.run(args, loader)
+
+Main.main(args)
---
+++ blog/20130720/cucumber_groovy/scripts/common.groovy
@@ -0,0 +1,78 @@
+package sample
+
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
+
+// Gitblit の Top URL
+TOP_URL = 'https://localhost:8443/'
+
+@Grab('org.seleniumhq.selenium:selenium-java:2.33.0')
+import org.openqa.selenium.*
+
+class WebTesterDriver {
+	private driver
+
+	WebTesterDriver(driver) {
+		this.driver = driver
+	}
+
+	def button(String caption) {
+		byXpath("//button[text() = '${caption}']")
+	}
+
+	def link(String caption) {
+		byXpath("//a[contains(., '${caption}')]")
+	}
+
+	def isError() {
+		byXpath("//span[@class = 'feedbackPanelERROR']") != null
+	}
+
+	def to(String url) {
+		driver.get(url)
+	}
+
+	def byName(String name) {
+		element(By.name(name))
+	}
+
+	def byXpath(String exp) {
+		element(By.xpath(exp))
+	}
+
+	def element(by) {
+		try {
+			driver.findElement(by)
+		} catch (e) {
+			null
+		}
+	}
+}
+
+Before() {
+	drv = new org.openqa.selenium.firefox.FirefoxDriver()
+	tester = new WebTesterDriver(drv)
+}
+
+After() {
+	drv.quit()
+}
+
+Given(~'Topへアクセス') { ->
+	tester.to(TOP_URL)
+}
+
+Given(~'ログイン済み') { ->
+	tester.to(TOP_URL)
+	tester.byName('username').sendKeys('admin')
+	tester.byName('password').sendKeys('admin')
+	tester.button('ログイン').click()
+}
+
+When(~/"(.*)" ボタンをクリックする/) { caption ->
+	tester.button(caption).click()
+}
+
+Then(~'エラー表示') { ->
+	assert tester.isError()
+}
