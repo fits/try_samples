5de65854204cd8311458e032f138e9f004e77787
fits <wadays_wozx@nifty.com>
2013-08-26 21:29:39 +0900

    Scala による Vert.x 実行サンプル

---
+++ scala/vert.x/http/SampleServer.scala
@@ -0,0 +1,20 @@
+
+import org.vertx.scala.core._
+import org.vertx.scala.core.http.HttpServerRequest
+import org.vertx.scala.platform.Verticle
+
+/**
+ * Scala による Vert.x の実行
+ *
+ *   vertx run scala:SampleServer
+ */
+class SampleServer extends Verticle {
+	override def start(future: Future[Void]): Unit = {
+		start()
+
+		vertx.newHttpServer.requestHandler { req: HttpServerRequest =>
+			req.response.end("test data")
+		}.listen(8080)
+	}
+}
+
