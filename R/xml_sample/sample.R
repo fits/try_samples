968ed0fbca88aebc15bef31776dde347761481b5
fits <wadays_wozx@nifty.com>
2013-09-20 00:37:33 +0900

    R による XML をパースするサンプル

---
+++ R/xml_sample/sample.R
@@ -0,0 +1,10 @@
+library(XML)
+
+doc <- xmlInternalTreeParse("data.xml")
+items <- getNodeSet(doc, "//item")
+
+no <- sapply(items, function(x) xmlGetAttr(x, "no"))
+category <- sapply(items, function(x) xmlGetAttr(x, "category"))
+value <- sapply(items, function(x) strtoi(xmlValue(x)))
+
+d <- data.frame(no, category, value)
