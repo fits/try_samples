d703d5ec08005ca8f69e73bc2356e6146235dc60
fits <wadays_wozx@nifty.com>
2013-09-20 01:07:44 +0900

    CSV 出力処理を追加

--- R/xml_sample/sample.R
+++ R/xml_sample/sample.R
@@ -8,3 +8,5 @@ category <- sapply(items, function(x) xmlGetAttr(x, "category"))
 value <- sapply(items, function(x) strtoi(xmlValue(x)))
 
 d <- data.frame(no, category, value)
+
+write.csv(d, file = "data.csv", row.names = NA)
