266b2645bac2e9025e4bbf165672350be0ebb5c3
fits <wadays_wozx@nifty.com>
2013-09-21 13:03:09 +0900

    stringsAsFactors を無効化するように設定

--- R/xml_sample/sample.R
+++ R/xml_sample/sample.R
@@ -7,6 +7,6 @@ no <- sapply(items, function(x) xmlGetAttr(x, "no"))
 category <- sapply(items, function(x) xmlGetAttr(x, "category"))
 value <- sapply(items, function(x) strtoi(xmlValue(x)))
 
-d <- data.frame(no, category, value)
+d <- data.frame(no, category, value, stringsAsFactors = FALSE)
 
 write.csv(d, file = "data.csv", row.names = FALSE)
