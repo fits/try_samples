44d48ae96bbc9ccaced6156a44fea11da858ebac
fits <wadays_wozx@nifty.com>
2013-09-22 23:12:52 +0900

    ヘッダー行の出力を追加

--- blog/20130922/sample_xmlslurper.groovy
+++ blog/20130922/sample_xmlslurper.groovy
@@ -1,6 +1,8 @@
 
 def doc = new XmlSlurper().parse(new File(args[0]))
 
+println '"tab","cat01","cat02","cat03","time","value"'
+
 doc.STATISTICAL_DATA.DATA_INF.VALUE.each {
 	println "${it.@tab},${it.@cat01},${it.@cat02},${it.@cat03},${it.@time},${it.text()}"
 }
