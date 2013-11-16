aab830525d1b5ee2e9c173bb477b9c8bf475d167
fits <wadays_wozx@nifty.com>
2013-11-16 13:17:53 +0900

    Scala 2.10 で Spark 0.9 （ソースからビルド） を使ったサンプル

---
+++ scala/spark/money_count_spark0.9_scala210/src/main/scala/money_count.scala
@@ -0,0 +1,19 @@
+package fits.sample
+
+import org.apache.spark._
+import SparkContext._
+
+object MoneyCount {
+	def main(args: Array[String]) {
+
+		val spark = new SparkContext("local", "MoneyCount")
+
+		val file = spark.textFile(args(0))
+
+		val res = file.map {
+			(_, 1)
+		}.reduceByKey(_ + _)
+
+		res.foreach { t => println(t._1 + " = " + t._2) }
+	}
+}
