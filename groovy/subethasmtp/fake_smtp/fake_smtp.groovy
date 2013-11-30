3c4ab877733e977c070306db2d6e15a9f6cd95f1
fits <wadays_wozx@nifty.com>
2013-11-30 11:09:12 +0900

    MimeMultipart の内容を出力する処理を追加

--- groovy/subethasmtp/fake_smtp/fake_smtp.groovy
+++ groovy/subethasmtp/fake_smtp/fake_smtp.groovy
@@ -1,9 +1,11 @@
 @Grab('org.subethamail:subethasmtp:3.1.7')
+@Grab('org.slf4j:slf4j-nop:1.7.5')
 import org.subethamail.smtp.server.SMTPServer
 import org.subethamail.smtp.helper.SimpleMessageListener
 import org.subethamail.smtp.helper.SimpleMessageListenerAdapter
 import javax.mail.Session
 import javax.mail.internet.MimeMessage
+import javax.mail.internet.MimeMultipart
 
 def server = new SMTPServer(new SimpleMessageListenerAdapter([
 	accept: { from, recipient ->
@@ -13,7 +15,18 @@ def server = new SMTPServer(new SimpleMessageListenerAdapter([
 		println "from: ${from}, to: ${recipient}"
 
 		def msg = new MimeMessage(Session.getInstance(new Properties()), data)
-		println msg.content
+
+		if (msg.content instanceof MimeMultipart) {
+			(0..<msg.content.count).each {
+				def body = msg.content.getBodyPart(it)
+				def enc = body.contentType.split('charset=').last()
+
+				println body.inputStream.getText(enc)
+			}
+		}
+		else {
+			println msg.content
+		}
 	}
 ] as SimpleMessageListener))
 
