486489061bfd21dfcbafb15965030086693295b0
fits <wadays_wozx@nifty.com>
2013-12-01 09:38:34 +0900

    blog sample: Groovy による Dempsy 単独実行スクリプト （KeySource の指定有無）

---
+++ blog/20131201/groovy_dempsy2/money_count_vertx.groovy
@@ -0,0 +1,110 @@
+@Grab('net.dempsy:lib-dempsyimpl:0.7.9')
+@Grab('org.slf4j:slf4j-log4j12:1.7.5')
+@Grab('io.vertx:vertx-core:2.1M1')
+@Grab('io.vertx:lang-groovy:2.0.0-final')
+import com.nokia.dempsy.annotations.*
+import com.nokia.dempsy.*
+import com.nokia.dempsy.config.*
+import com.nokia.dempsy.output.*
+
+import com.nokia.dempsy.cluster.invm.LocalClusterSessionFactory
+import com.nokia.dempsy.router.AlwaysInCurrentCluster
+import com.nokia.dempsy.router.DecentralizedRoutingStrategy
+import com.nokia.dempsy.serialization.kryo.KryoSerializer
+import com.nokia.dempsy.monitoring.coda.StatsCollectorFactoryCoda
+import com.nokia.dempsy.messagetransport.blockingqueue.BlockingQueueTransport
+
+import org.vertx.groovy.core.Vertx
+import org.vertx.groovy.core.http.RouteMatcher
+
+import groovy.transform.*
+
+@TupleConstructor
+class Money {
+	String moneyText
+
+	@MessageKey
+	String getMoneyText() {
+		moneyText
+	}
+}
+
+@MessageProcessor
+@AutoClone
+class MoneyCount {
+	private long count = 0
+	private String key
+
+	@Activation
+	void setMoneyKey(String key) {
+		println "MoneyCount.activation : ${key}"
+		this.key = key
+	}
+
+	@MessageHandler
+	void countMoney(Money money) {
+		count++
+	}
+
+	@Output
+	void printResults() {
+		println "key: ${key}, count: ${count}"
+	}
+}
+
+class Constants {
+	final static def MONEYS = [
+		'1', '5', '10', '50', '100', '500', '1000', '2000', '5000', '10000'
+	]
+}
+
+class MoneyAdaptor implements Adaptor {
+	Dispatcher dispatcher
+
+	void start() {
+		println 'MoneyAdaptor.start ...'
+
+		def vertx = Vertx.newVertx()
+
+		def rm = new RouteMatcher()
+		rm.get '/:money', { req ->
+			def money = req.params['money']
+
+			if (Constants.MONEYS.contains(money)) {
+				dispatcher.dispatch(new Money(req.params['money']))
+			}
+			req.response.end()
+		}
+
+		vertx.createHttpServer().requestHandler(rm.asClosure()).listen 8080
+	}
+
+	void stop() {
+		println 'MoneyAdaptor.stop'
+	}
+}
+
+def mp = new ClusterDefinition('mp', new MoneyCount())
+mp.outputExecuter = new com.nokia.dempsy.output.RelativeOutputSchedule(10, java.util.concurrent.TimeUnit.SECONDS)
+
+def app = new ApplicationDefinition('money-count').add(
+	new ClusterDefinition('adaptor', new MoneyAdaptor()),
+	mp
+)
+
+def dempsy = new Dempsy()
+
+dempsy.applicationDefinitions = [app]
+dempsy.clusterSessionFactory = new LocalClusterSessionFactory()
+dempsy.clusterCheck = new AlwaysInCurrentCluster()
+dempsy.defaultRoutingStrategy = new DecentralizedRoutingStrategy(1, 1)
+dempsy.defaultSerializer = new KryoSerializer()
+dempsy.defaultStatsCollectorFactory = new StatsCollectorFactoryCoda()
+dempsy.defaultTransport = new BlockingQueueTransport()
+
+dempsy.start()
+
+Runtime.runtime.addShutdownHook { ->
+	println 'shutdown ...'
+	dempsy.stop()
+}
