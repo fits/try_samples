30e62d4eb3f53bc0d6a95bef3d19a34c2da1e60a
fits <wadays_wozx@nifty.com>
2013-12-02 17:47:40 +0900

    glm の結果から予測線を描画する処理を追加

--- R/glm/poisson_sample/poissonGlm.R
+++ R/glm/poisson_sample/poissonGlm.R
@@ -7,4 +7,11 @@ plot(d$x, d$y, col = c("red", "blue")[d$f])
 d.all <- glm(y ~ ., data = d, family = poisson)
 
 library(MASS)
-stepAIC(d.all)
+d.res <- stepAIC(d.all)
+
+summary(d.res)
+
+xx <- seq(min(d$x), max(d$x), length = 1000)
+#lines(xx, exp(d.res$coefficients["(Intercept)"] + d.res$coefficients["x"] * xx), col="green")
+lines(xx, predict(d.res, newdata = data.frame(x = xx), type = "response"), col = "green")
+
