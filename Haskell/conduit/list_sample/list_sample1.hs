7683e20beb4e2eda5d237204249bb5d329010a78
fits <wadays_wozx@nifty.com>
2014-01-01 11:00:33 +0900

    サンプル追加

--- Haskell/conduit/list_sample/list_sample1.hs
+++ Haskell/conduit/list_sample/list_sample1.hs
@@ -5,4 +5,8 @@ import qualified Data.Conduit.List as CL
 main = do
 	x <- runResourceT $ CL.sourceList [1..10 :: Int] $$ CL.filter even =$ CL.consume
 
-	putStrLn $ unlines $ map show x
+	putStrLn $ show x
+
+	x2 <- runResourceT $ CL.sourceList [1..10 :: Int] $$ CL.filter even =$ CL.take 3
+
+	putStrLn $ show x2
