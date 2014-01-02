0d731c23c24df2bd3b664eb994b312b40114eac6
fits <wadays_wozx@nifty.com>
2014-01-02 09:57:31 +0900

    不要な処理を削除

--- Haskell/conduit/list_sample/list_sample1.hs
+++ Haskell/conduit/list_sample/list_sample1.hs
@@ -3,10 +3,10 @@ import Data.Conduit
 import qualified Data.Conduit.List as CL
 
 main = do
-	x <- runResourceT $ CL.sourceList [1..10 :: Int] $$ CL.filter even =$ CL.consume
+	x <- CL.sourceList [1..10 :: Int] $$ CL.filter even =$ CL.consume
 
 	putStrLn $ show x
 
-	x2 <- runResourceT $ CL.sourceList [1..10 :: Int] $$ CL.filter even =$ CL.take 3
+	x2 <- CL.sourceList [1..10 :: Int] $$ CL.filter even =$ CL.take 3
 
 	putStrLn $ show x2
