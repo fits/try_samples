b4eaae0c6b8afef3a9bd930020f2a682c7d3556b
fits <wadays_wozx@nifty.com>
2014-01-03 10:29:58 +0900

    ファイルを処理する Conduit サンプル

---
+++ Haskell/conduit/file_sample/readline_file.hs
@@ -0,0 +1,17 @@
+
+import System.Environment
+import Data.Conduit
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
+import qualified Data.ByteString.Char8 as B
+
+main = do
+	args <- getArgs
+
+	x <- runResourceT $ CB.sourceFile (args !! 0) $$ CB.lines =$ do
+		_ <- CL.drop 1
+		CL.take 3
+
+	mapM (B.putStrLn) x
+
+	return ()
