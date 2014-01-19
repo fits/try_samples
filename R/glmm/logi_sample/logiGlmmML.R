9e081bcece3c6131116e1052c94792fed7979acc
fits <wadays_wozx@nifty.com>
2014-01-19 18:53:27 +0900

    葉数 x=4 の場合の GLMM による予測線の描画処理を追加

--- R/glmm/logi_sample/logiGlmmML.R
+++ R/glmm/logi_sample/logiGlmmML.R
@@ -16,5 +16,19 @@ xx <- seq(min(d$x), max(d$x), length = 1000)
 beta <- d.res$coefficients
 lines(xx, max(d$N) * 1.0 / (1.0 + exp(-1 * (beta[1] + beta[2] * xx))), col="green")
 
-d.hs <- hist(d[d$x == 4,]$y, plot = FALSE)
-plot(d.hs$counts)
+# x=4 のyの分布
+plot(0:8, table(d[d$x == 4,]$y))
+
+d.fbinom <- function(r, x)
+  dbinom(x, 8, 1.0 / (1.0 + exp(-1 * (beta[1] + beta[2] * 4 + r)))) * dnorm(r, 0, d.res$sigma)
+
+d.dbinom <- function(xl)
+  sapply(xl, function(x) integrate(
+      f = d.fbinom,
+      lower = d.res$sigma * -10,
+      upper = d.res$sigma * 10,
+      x = x
+    )$value
+  )
+
+lines(0:8, d.dbinom(0:8) * length(d[d$x == 4,]$y), col="red", type="b")
