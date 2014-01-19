735ca8ee5e06e6a572ee7352cd653451284daa25
fits <wadays_wozx@nifty.com>
2014-01-19 11:20:54 +0900

    二つのグラフを並べて描画するように変更

--- R/glmm/logi_sample/logiGlmmML.R
+++ R/glmm/logi_sample/logiGlmmML.R
@@ -2,11 +2,19 @@ d <- read.csv('data7.csv')
 
 summary(d)
 
-d.hs <- hist(d[d$x == 4,]$y, plot = FALSE)
-plot(d.hs$counts)
-
 library(glmmML)
 
 d.res <- glmmML(cbind(y, N - y) ~ x, data = d, family = binomial, cluster = id)
 
 summary(d.res)
+
+par(mfrow = c(1, 2))
+
+plot(d$x, d$y)
+
+xx <- seq(min(d$x), max(d$x), length = 1000)
+beta <- d.res$coefficients
+lines(xx, max(d$N) * 1.0 / (1.0 + exp(-1 * (beta[1] + beta[2] * xx))), col="green")
+
+d.hs <- hist(d[d$x == 4,]$y, plot = FALSE)
+plot(d.hs$counts)
