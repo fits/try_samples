c70f418600a03881ddd21e382af15e296773e515
fits <wadays_wozx@nifty.com>
2014-01-21 10:06:47 +0900

    不要なため random の指定を削除

--- R/glmm/logi_sample/logiMcmcglmm.R
+++ R/glmm/logi_sample/logiMcmcglmm.R
@@ -2,6 +2,10 @@ d <- read.csv('data7.csv')
 
 library(MCMCglmm)
 
-d.res <- MCMCglmm(cbind(y, N - y) ~ x, random = ~id, data = d, family = "multinomial2", verbose = FALSE)
+d.res <- MCMCglmm(cbind(y, N - y) ~ x, data = d, family = "multinomial2", verbose = FALSE)
 
 summary(d.res)
+
+summary(sqrt(d.res$VCV))
+
+sd <- sqrt(posterior.mode(d.res$VCV))
