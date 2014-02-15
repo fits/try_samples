0b7dbbf00871f9a86ca7399bae1d3514d6a0f924
fits <wadays_wozx@nifty.com>
2014-02-15 21:19:12 +0900

    collect() の処理を追加

--- rust/map_filter_fold_sample/map_sample.rs
+++ rust/map_filter_fold_sample/map_sample.rs
@@ -3,12 +3,17 @@ extern mod std;
 fn main() {
 	let list = [1, 2, 3];
 	let mut res = list.iter().map(|&x| x * 10);
+	let res2: ~[int] = list.iter().map(|&x| x * 10).collect();
 
 	for &x in list.iter() {
 		println(format!("{}", x));
 	}
 
 	for x in res {
+		println(format!("{}", x));
+	}
+
+	for &x in res2.iter() {
 		println(format!("{}", x));
 	}
 }
