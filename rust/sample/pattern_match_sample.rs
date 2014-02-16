e9ee36a291c8794c4c35007ee8773b5a8c18dc9a
fits <wadays_wozx@nifty.com>
2014-02-17 00:32:44 +0900

    パターンマッチのサンプル追加

--- rust/sample/pattern_match_sample.rs
+++ rust/sample/pattern_match_sample.rs
@@ -4,9 +4,16 @@ fn main() {
 	printNum(3);
 	printNum(7);
 
+	printNum2(0);
+	printNum2(3);
+	printNum2(7);
+
 	println!("{}", updateNum(0));
 	println!("{}", updateNum(3));
 	println!("{}", updateNum(7));
+
+	println!("{}", updateNum2(3));
+	println!("{}", updateNum2(7));
 }
 
 fn printNum (x: int) {
@@ -17,10 +24,25 @@ fn printNum (x: int) {
 	}
 }
 
+fn printNum2 (x: int) {
+	match x {
+		0 => { println!("zero") }
+		1..5 => { println!("one to five") }
+		_ => { println!("else") }
+	}
+}
+
 fn updateNum (x: int) -> int {
 	match x {
 		0 => x + 1,
 		1..5 => x * 2,
 		_ => x
+	}
+}
+
+fn updateNum2 (x: int) -> int {
+	match x {
+		a @ 1..5 => a * 2,
+		_ => 0
 	}
 }
