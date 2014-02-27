70cc6658a284e5cb761708bb3a7e015f7df6d43b
fits <wadays_wozx@nifty.com>
2014-02-28 00:28:58 +0900

    描画する線を追加

--- R/glm/poisson_sample/poissonMcmc.R
+++ R/glm/poisson_sample/poissonMcmc.R
@@ -9,4 +9,6 @@ md.res <- MCMCpoisson(y ~ x, data = md)
 summary(md.res)
 
 xx <- seq(min(md$x), max(md$x), length = 10000)
+lines(xx, exp(quantile(md.res[,1], 0.25) + quantile(md.res[,2], 0.25) * xx), col="gray")
 lines(xx, exp(mean(md.res[,1]) + mean(md.res[,2]) * xx), col="green")
+lines(xx, exp(quantile(md.res[,1], 0.75) + quantile(md.res[,2], 0.75) * xx), col="gray")
