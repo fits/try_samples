0324787c4b1cd38859646d347b8512fee923a05d
fits <wadays_wozx@nifty.com>
2014-05-05 23:55:28 +0900

    ASM を使ってメソッドのコードを出力するサンプル

---
+++ groovy/asm/class_reader/print_method_code.groovy
@@ -0,0 +1,23 @@
+@Grab('org.ow2.asm:asm:5.0.2')
+@Grab('org.ow2.asm:asm-tree:5.0.2')
+@Grab('org.ow2.asm:asm-util:5.0.2')
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.util.Textifier
+import org.objectweb.asm.util.TraceMethodVisitor
+
+def reader = new ClassReader(new File(args[0]).newInputStream())
+def node = new ClassNode()
+
+reader.accept node, ClassReader.SKIP_DEBUG
+
+node.methods.each {
+	println "----- ${it.name} -----"
+
+	def t = new Textifier()
+	def mv = new TraceMethodVisitor(t)
+
+	it.accept(mv)
+
+	println t.text
+}
