647b557de65a5c130909195b2f2237efcc7ca381
fits <wadays_wozx@nifty.com>
2014-05-18 12:02:53 +0900

    blog sample: Gradle で JAR ファイルへ AspectJ を適用

---
+++ blog/20140518/gradle_aspectj/src/main/java/fits/sample/StrutsAspect.java
@@ -0,0 +1,36 @@
+package fits.sample;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.*;
+
+@Aspect
+public class StrutsAspect {
+	private final static Pattern PATTERN = Pattern.compile("(^|\\W)[cC]lass\\W");
+
+	@Around(
+		"call(void org.apache..BeanUtils.populate(Object, Map)) &&" + 
+		"within(org.apache.struts..*) && " +
+		"!within(org.apache.struts.mock.*) && " +
+		"args(bean, properties)"
+	)
+	public void aroundPopulate(ProceedingJoinPoint pjp, Object bean, Map properties) throws Throwable {
+		if (properties != null) {
+			checkProperties(properties);
+
+			pjp.proceed();
+		}
+	}
+
+	private void checkProperties(Map properties) {
+		for (Object key : properties.keySet()) {
+			String property = (String)key;
+
+			if (PATTERN.matcher(property).find()) {
+				throw new IllegalArgumentException(key + " is invalid");
+			}
+		}
+	}
+}
