9d955fd64c9fb27d9823ade28bc0c995012ae95f
fits <wadays_wozx@nifty.com>
2014-06-06 01:37:51 +0900

    問題のある計算方法を削除

--- data/tree/gini/gini_impurity.fs
+++ data/tree/gini/gini_impurity.fs
@@ -12,14 +12,8 @@ let combinationCount cs = [
 
 let gini2 xs = xs |> Seq.countBy id |> combinationCount |> Seq.sumBy (fun (x, y) -> (float x / size xs) * (float y / size xs))
 
-let combination cs = Seq.last cs :: Seq.toList cs |> Seq.pairwise
-
-let gini2a xs = xs |> Seq.countBy id |> combination |> Seq.sumBy (fun ((_, x), (_, y)) -> (float x / size xs) * (float y / size xs) * 2.0)
-
 let list = ["A"; "B"; "B"; "C"; "B"; "A";]
 
 printfn "%A" list
 printfn "%A" (gini1 list)
 printfn "%A" (gini2 list)
-printfn "%A" (gini2a list)
-
