cc05da9106c7a1298f7ad0aff3ea9a9a113e2801
fits <wadays_wozx@nifty.com>
2014-06-18 01:39:12 +0900

    別の算出方法を追加

--- data/tree/gini/gini_impurity.py
+++ data/tree/gini/gini_impurity.py
@@ -1,5 +1,5 @@
 
-from itertools import groupby
+from itertools import *
 
 def size(xs):
 	return float(len(xs))
@@ -7,6 +7,16 @@ def size(xs):
 def gini1(xs):
 	return 1 - sum(map(lambda (k, g): (size(list(g)) / size(xs)) ** 2, groupby(sorted(xs))))
 
+def countby(xs):
+	return map(lambda (k, v): (k, size(list(v))), groupby(sorted(xs)))
+
+def gini2(xs):
+	return sum(map(
+		lambda ((xk, xv), (yk, yv)): xv / size(xs) * yv / size(xs) * 2, 
+		combinations(countby(xs), 2)
+	))
+
 vlist = ["A", "B", "B", "C", "B", "A"]
 
 print gini1(vlist)
+print gini2(vlist)
