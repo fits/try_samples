ae49e84915f6e131b864baec812960ae08b511e8
fits <wadays_wozx@nifty.com>
2014-06-21 14:06:20 +0900

    別の算出方法を追加

--- R/gini/gini_impurity.R
+++ R/gini/gini_impurity.R
@@ -3,6 +3,11 @@ gini1 <- function(xs) {
   1 - Reduce("+", lapply(table(xs), function(x) (x / length(xs)) ^ 2))
 }
 
+gini2 <- function(xs) {
+  sum(apply(combn(table(xs), 2), 2, function(x) (x[1] / length(xs)) * (x[2] / length(xs)) * 2))
+}
+
 list <- c("A", "B", "B", "C", "B", "A")
 
 gini1(list)
+gini2(list)
