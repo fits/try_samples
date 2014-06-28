b6781718864147665c36573e69f31aaa5d1113ff
fits <wadays_wozx@nifty.com>
2014-06-28 19:21:40 +0900

    ブラジルW杯グループリーグの結果データを使った決定木のサンプル

---
+++ data/world_cup/2014/rpart1.R
@@ -0,0 +1,23 @@
+library(rpart)
+library(partykit)
+
+excRows = c(
+  "Matches.Played",
+  "Goals.for",
+  "Total.Goals.scored",
+  "Goals.Conceded",
+  "Penalty.goal",
+  "Own.goals.For",
+  "Open.Play.Goals",
+  "Set.Piece.Goals",
+  "Clearances.completion.rate",
+  "Passes.Completed.1",
+  "Crosses.Completed.1"
+)
+
+wd.all <- read.delim("data/teams_result.txt", row.names = 1)
+wd.data <- wd.all[, !(colnames(wd.all) %in% excRows)]
+
+wd.rpart <- rpart(League.Result ~ ., data = wd.data)
+
+plot(as.party(wd.rpart))
---
+++ data/world_cup/2014/tree0.R
@@ -0,0 +1,16 @@
+library(tree)
+
+excRows = c(
+  "Matches.Played",
+  "Clearances.completion.rate",
+  "Passes.Completed.1",
+  "Crosses.Completed.1"
+)
+
+wd.all <- read.delim("data/teams_result.txt", row.names = 1)
+wd.data <- wd.all[, !(colnames(wd.all) %in% excRows)]
+
+wd.tree <- tree(League.Result ~ ., data = wd.data)
+
+plot(wd.tree)
+text(wd.tree)
