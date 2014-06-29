68a95ab938b20ced8c4e90c1792eef9e674144b9
fits <wadays_wozx@nifty.com>
2014-06-29 13:26:54 +0900

    PC1～4 で主成分負荷量が0.3より大きいものを抽出する処理を追加

--- data/world_cup/2014/prcomp0.R
+++ data/world_cup/2014/prcomp0.R
@@ -13,3 +13,7 @@ wd.prcomp <- prcomp(wd.data, scale = T)
 
 summary(wd.prcomp)
 biplot(wd.prcomp)
+
+a <- wd.prcomp$rotation
+# PC1-PC4で絶対値が0.3より大きい変数を抽出
+a[abs(a[,1]) > 0.3 | abs(a[,2]) > 0.3 | abs(a[,3]) > 0.3 | abs(a[,4]) > 0.3, 1:4]
