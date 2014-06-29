bf3cebb8dab56a2ebe90b60e38d5cd18bf56884a
fits <wadays_wozx@nifty.com>
2014-06-29 17:44:33 +0900

    blog sample: Groovy による Apache Spark の利用サンプル

---
+++ blog/20140629/spark_groovy/money_count.groovy
@@ -0,0 +1,8 @@
+@Grab('org.apache.spark:spark-core_2.10:1.0.0')
+import org.apache.spark.api.java.JavaSparkContext
+
+def spark = new JavaSparkContext('local', 'MoneyCount')
+
+spark.textFile(args[0]).countByValue().each { k, v ->
+	println "$k = $v"
+}
