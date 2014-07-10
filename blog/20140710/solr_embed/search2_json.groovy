2224f592d40456ecec1152529b1d8c869c670d03
fits <wadays_wozx@nifty.com>
2014-07-10 22:59:55 +0900

    blog sample: Solr の組み込み実行

---
+++ blog/20140710/solr_embed/search2_json.groovy
@@ -0,0 +1,30 @@
+@Grab('org.apache.solr:solr-core:4.9.0')
+@Grab('org.slf4j:slf4j-nop:1.7.7')
+import org.apache.solr.core.CoreContainer
+import org.apache.solr.request.LocalSolrQueryRequest
+import org.apache.solr.response.SolrQueryResponse
+import org.apache.solr.response.JSONResponseWriter
+
+def cores = new CoreContainer(args[0])
+cores.load()
+
+def core = cores.getCore(args[1])
+def handler = core.getRequestHandler('/query')
+
+def req = new LocalSolrQueryRequest(core, [
+	q: ['*:*'] as String[]
+])
+
+def res = new SolrQueryResponse()
+
+core.execute(handler, req, res)
+
+def writer = new StringWriter()
+
+def rw = new JSONResponseWriter()
+rw.write(writer, req, res)
+
+println writer.toString()
+
+core.close()
+cores.shutdown()
