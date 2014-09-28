880c2e9f648e4b40e7a0749f3464570f4d293748
fits <wadays_wozx@nifty.com>
2014-09-28 19:02:53 +0900

    sodium の Java サンプル

---
+++ java/sodium/sample/Sample.java
@@ -0,0 +1,29 @@
+import sodium.*;
+import java.util.function.Consumer;
+
+class Sample {
+	public static void main(String... args) {
+		Consumer<String> prtl = msg -> System.out.println(msg);
+
+		BehaviorSink<Integer> a1 = new BehaviorSink<>(0);
+		BehaviorSink<Integer> b1 = new BehaviorSink<>(0);
+
+		Behavior<Integer> c1 = a1.lift( (a, b) -> a + b, b1 );
+
+		Listener li = c1.value().listen( a -> prtl.accept("c1 = " + a) );
+
+		prtl.accept("--- a1.send 10");
+		a1.send(10);
+
+		prtl.accept("--- b1.send 2");
+		b1.send(2);
+
+		prtl.accept("--- a1.send 30");
+		a1.send(30);
+
+		prtl.accept("--- b1.send 4");
+		b1.send(4);
+
+		li.unlisten();
+	}
+}
