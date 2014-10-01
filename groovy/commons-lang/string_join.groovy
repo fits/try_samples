eab71945f13da0d5bb42dcb178c6f168c57101f1
fits <wadays_wozx@nifty.com>
2014-10-02 00:37:23 +0900

    commons-lang のバージョンを変更

--- groovy/commons-lang/string_join.groovy
+++ groovy/commons-lang/string_join.groovy
@@ -1,4 +1,4 @@
-@Grab('commons-lang:commons-lang:2.6')
-import org.apache.commons.lang.StringUtils
+@Grab('org.apache.commons:commons-lang3:3.3.2')
+import org.apache.commons.lang3.StringUtils
 
 println StringUtils.join(['A', null, '1', 'B'], '-')
