4afcac755420f8d6d2e83cec0c9a51132db3535e
fits <wadays_wozx@nifty.com>
2014-10-02 00:41:25 +0900

    StringUtils.capitalize のサンプル

---
+++ groovy/commons-lang/string_capitalize.groovy
@@ -0,0 +1,6 @@
+@Grab('org.apache.commons:commons-lang3:3.3.2')
+import org.apache.commons.lang3.StringUtils
+
+println StringUtils.capitalize('methodName')
+
+println StringUtils.capitalize('methodname')
