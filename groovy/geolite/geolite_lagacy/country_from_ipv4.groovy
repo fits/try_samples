02545bf428965d2b09e5780c50dfb57f134911dd
fits <wadays_wozx@nifty.com>
2014-10-04 00:45:50 +0900

    不要な as long を削除

--- groovy/geolite/geolite_lagacy/country_from_ipv4.groovy
+++ groovy/geolite/geolite_lagacy/country_from_ipv4.groovy
@@ -6,7 +6,7 @@ if (args.length < 2) {
 
 def toNumForIP = { Integer.toUnsignedLong(it.hashCode()) }
 // 以下でも可
-// def toNumForIP = { (it.hashCode() & 0xffffffff) as long }
+//def toNumForIP = { it.hashCode() & 0xffffffff }
 
 def ip = toNumForIP( InetAddress.getByName(args[1]) )
 
