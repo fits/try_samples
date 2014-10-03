35a34a03b9a3886f33799b09bec6f797f757b4bd
fits <wadays_wozx@nifty.com>
2014-10-04 00:57:20 +0900

    バージョン変更

--- groovy/spark/money_count/money_count.groovy
+++ groovy/spark/money_count/money_count.groovy
@@ -1,4 +1,4 @@
-@Grab('org.apache.spark:spark-core_2.10:1.0.0')
+@Grab('org.apache.spark:spark-core_2.10:1.1.0')
 import org.apache.spark.api.java.JavaSparkContext
 
 def spark = new JavaSparkContext('local', 'MoneyCount')
