ab3ea83027695236145d404f02f94cb91d6c7d12
fits <wadays_wozx@nifty.com>
2014-11-10 00:41:46 +0900

    値とコメントを追加

--- groovy/json/jsonBuilder/sample1.groovy
+++ groovy/json/jsonBuilder/sample1.groovy
@@ -7,17 +7,20 @@ builder {
 	name 'sample'
 	value 10
 	on true
+	items 'ch1', 'ch2'
 }
 
+// {"name":"sample","value":10,"on":true,"items":["ch1","ch2"]}
 println builder.toString()
 
 def builder2 = new JsonBuilder()
 
 def d2 = [
-	[name: 'a1', value: 11, on: true],
+	[name: 'a1', value: 11, on: true, items: ['ch1', 'ch2']],
 	[name: 'b2', value: 22, on: false]
 ]
 
 builder2.call(d2)
 
+// [{"name":"a1","value":11,"on":true,"items":["ch1","ch2"]},{"name":"b2","value":22,"on":false}]
 println builder2.toString()
