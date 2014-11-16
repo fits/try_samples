0fdb90c952142a33c94176c06c49ad77b8591bde
fits <wadays_wozx@nifty.com>
2014-11-16 23:25:06 +0900

    RxJava の BehaviorSubject・PublishSubject の挙動を確認するサンプル

---
+++ groovy/rxjava/subject_sample/publish_subject_sample.groovy
@@ -0,0 +1,18 @@
+@Grab('io.reactivex:rxjava:1.0.0-rc.10')
+import rx.subjects.*
+
+def obs1 = { println "obs1: ${it}" }
+def obs2 = { println "obs2: ${it}" }
+
+def subject = PublishSubject.create()
+
+subject.subscribe obs1
+
+subject.onNext 'A'
+subject.onNext 'B'
+
+subject.subscribe obs2
+
+subject.onNext 'C'
+
+subject.onCompleted()
