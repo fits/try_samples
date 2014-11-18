a996b94baae6f02e75b631ac2ee7327450482a4f
fits <wadays_wozx@nifty.com>
2014-11-19 08:32:32 +0900

    EventSink を使った単純なサンプル

---
+++ java/sodium/sample/EventSinkSample.java
@@ -0,0 +1,21 @@
+import sodium.*;
+import java.util.function.Consumer;
+
+class EventSinkSample {
+	public static void main(String... args) {
+
+		EventSink<String> es = new EventSink<>();
+
+		Listener li = es.listen(System.out::println);
+
+		System.out.println("-----");
+
+		es.send("A1");
+
+		System.out.println("-----");
+
+		es.send("B2");
+
+		li.unlisten();
+	}
+}
