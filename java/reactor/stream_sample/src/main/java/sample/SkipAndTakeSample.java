bf2da2fdef6c96ba39f6eec11d3e8b2b697eb809
fits <wadays_wozx@nifty.com>
2014-12-31 17:21:57 +0900

    skip 処理のサンプル

---
+++ java/reactor/stream_sample/src/main/java/sample/SkipAndTakeSample.java
@@ -0,0 +1,54 @@
+package sample;
+
+import reactor.Environment;
+import reactor.rx.Promise;
+import reactor.rx.Stream;
+import reactor.rx.Streams;
+import reactor.rx.stream.Broadcaster;
+import reactor.rx.action.support.TapAndControls;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SkipAndTakeSample {
+	public static void main(String... args) throws Exception {
+		Environment env = new Environment();
+
+		skipAndTakeSampleA(env);
+
+		System.out.println("-----");
+
+		skipAndTakeSampleB(env);
+
+		env.shutdown();
+	}
+
+	// sampleA-3
+	private static void skipAndTakeSampleA(Environment env) throws Exception {
+		Broadcaster<String> stream = Streams.<String>broadcast(env);
+
+		Promise<String> promise = skip(stream, 2).take(3).observe(System.out::println).next();
+
+		Streams.range(1, 6).consume( i -> stream.onNext("sampleA-" + i) );
+
+		promise.await();
+	}
+
+	// sampleB-3
+	// sampleB-4
+	// sampleB-5
+	private static void skipAndTakeSampleB(Environment env) throws Exception {
+		Broadcaster<String> stream = Streams.<String>broadcast(env);
+
+		TapAndControls<String> tap = skip(stream, 2).take(3).observe(System.out::println).tap();
+
+		Streams.range(1, 6).consume( i -> stream.onNext("sampleB-" + i) );
+
+		tap.get();
+	}
+
+	private static <T> Stream<T> skip(Stream<T> st, int num) {
+		AtomicInteger counter = new AtomicInteger();
+
+		return st.filter(s -> counter.incrementAndGet() > num);
+	}
+}
