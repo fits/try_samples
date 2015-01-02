7842d3a1b8f4a1c55360996f4c334699e4ee9ccc
fits <wadays_wozx@nifty.com>
2015-01-02 19:51:57 +0900

    drop と take のサンプル

---
+++ java/akka-stream/sample/src/main/java/DropAndTakeSampleApp.java
@@ -0,0 +1,34 @@
+
+import akka.actor.ActorSystem;
+import akka.dispatch.OnComplete;
+import akka.stream.FlowMaterializer;
+import akka.stream.javadsl.Source;
+
+import scala.runtime.BoxedUnit;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+public class DropAndTakeSampleApp {
+	public static void main(String... args) {
+		final ActorSystem system = ActorSystem.create("sample");
+		final FlowMaterializer materializer = FlowMaterializer.create(system);
+
+		final OnComplete<BoxedUnit> complete = new OnComplete<BoxedUnit>() {
+			@Override
+			public void onComplete(Throwable failure, BoxedUnit success) {
+				system.shutdown();
+			}
+		};
+
+		List<String> data = IntStream.range(1, 6).mapToObj(i -> "sample" + i).collect(Collectors.toList());
+
+		Source.from(data)
+			.drop(3)
+			.take(2)
+			.map(s -> "#" + s)
+			.foreach(System.out::println, materializer)
+			.onComplete(complete, system.dispatcher());
+	}
+}
