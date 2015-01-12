fd80251d9eb5bcd61aa33c14f4758ac5db8b6374
fits <wadays_wozx@nifty.com>
2015-01-12 23:37:24 +0900

    クラス名を変更

--- blog/20150112/akka_streams/src/main/java/sample/PublisherSample1.java
+++ blog/20150112/akka_streams/src/main/java/sample/PublisherSample1.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class ProducerSample1 {
+public class PublisherSample1 {
 	public static void main(String... args) {
 		final Config config = ConfigFactory.load()
 			.withValue("akka.loglevel", ConfigValueFactory.fromAnyRef("error"));
