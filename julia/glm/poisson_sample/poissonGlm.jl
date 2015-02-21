9246194595e4c2c4979e06f82901e9f892b5e87e
fits <wadays_wozx@nifty.com>
2015-02-22 00:57:06 +0900

    グラフの描画処理を追加

--- julia/glm/poisson_sample/poissonGlm.jl
+++ julia/glm/poisson_sample/poissonGlm.jl
@@ -1,8 +1,18 @@
 
-using DataFrames, GLM
+using DataFrames, GLM, Gadfly
 
 d = readtable("data3a.csv")
 
 res = glm(y~x, d, Poisson(), LogLink())
 
 println(res)
+
+xx = [minimum(d[:x]):0.1:maximum(d[:x])]
+yy = exp(coef(res)[1] + coef(res)[2] * xx)
+
+p = plot(
+	layer(d, x = "x", y = "y", color = "f", Geom.point),
+	layer(DataFrame(x = xx, y = yy), x = "x", y = "y", Geom.line)
+)
+
+draw(PNG("poissonGlm.png", 6inch, 6inch), p)
