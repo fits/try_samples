5f994d2bdf394d0bc1a7a3822510193da366051a
fits <wadays_wozx@nifty.com>
2015-04-26 01:36:51 +0900

    ConnectorAddressLink を使って JMX のサービスURLを取得する版

---
+++ groovy/attach_api/attach-load_jmx2.groovy
@@ -0,0 +1,18 @@
+import com.sun.tools.attach.VirtualMachine
+import sun.management.ConnectorAddressLink
+
+def vm = VirtualMachine.attach(args[0])
+
+def getConnectorAddress = {
+	ConnectorAddressLink.importFrom(args[0] as int)
+}
+
+if (getConnectorAddress() == null) {
+	def javaHome = vm.systemProperties.getProperty('java.home')
+
+	vm.loadAgent("${javaHome}/lib/management-agent.jar")
+
+	println '*** load agent'
+}
+
+println getConnectorAddress()
