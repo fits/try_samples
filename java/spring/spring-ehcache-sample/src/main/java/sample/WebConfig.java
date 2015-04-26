d9e365941f145fb8d14bc6155de3daecf0acb0d7
fits <wadays_wozx@nifty.com>
2015-04-26 23:15:52 +0900

    JMX でキャッシュ状況をモニタリングするための設定を追加

--- java/spring/spring-ehcache-sample/src/main/java/sample/WebConfig.java
+++ java/spring/spring-ehcache-sample/src/main/java/sample/WebConfig.java
@@ -1,5 +1,6 @@
 package sample;
 
+import net.sf.ehcache.management.ManagementService;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
@@ -7,6 +8,7 @@ import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jmx.support.MBeanServerFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -15,13 +17,34 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class WebConfig {
     @Bean
     public CacheManager cacheManager() {
-        EhCacheCacheManager res = new EhCacheCacheManager();
+        EhCacheCacheManager manager = new EhCacheCacheManager();
 
+        manager.setCacheManager(ehcache().getObject());
+
+        return manager;
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehcache() {
         EhCacheManagerFactoryBean ehcache = new EhCacheManagerFactoryBean();
         ehcache.setConfigLocation(new ClassPathResource("ehcache.xml"));
 
-        res.setCacheManager(ehcache.getObject());
+        return ehcache;
+    }
 
-        return res;
+    @Bean
+    public MBeanServerFactoryBean mbeanServer() {
+        MBeanServerFactoryBean factory = new MBeanServerFactoryBean();
+        factory.setLocateExistingServerIfPossible(true);
+
+        return factory;
+    }
+
+    @Bean
+    public ManagementService managementService() {
+        ManagementService service = new ManagementService(ehcache().getObject(), mbeanServer().getObject(), true, true, true, true);
+        service.init();
+
+        return service;
     }
 }
