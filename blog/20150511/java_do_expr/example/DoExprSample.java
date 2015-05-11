f1e3abca394c9d329b30a2959a9e2b620ae7732d
fits <wadays_wozx@nifty.com>
2015-05-11 21:10:41 +0900

    blog sample: Java のアノテーションプロセッサで Haskell の do 記法を簡易的に実現

---
+++ blog/20150511/java_do_expr/example/DoExprSample.java
@@ -0,0 +1,46 @@
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.Optional;
+
+public class DoExprSample {
+	public static void main(String... args) {
+		Optional<Integer> o1 = Optional.of(2);
+		Optional<Integer> o2 = Optional.of(3);
+
+		Opt<Integer> opt = new Opt<>();
+
+		// replace by annotation processor
+		Supplier<Optional<Integer>> res = ($do<Optional, Integer> opt) -> {
+			let a = o1;
+			let b = o2;
+//			let b = Optional.empty();
+			let c = Optional.of(4);
+			return a + b + c * 2;
+		};
+
+		// Optional[13]
+		System.out.println(res.get());
+
+		Opt<String> opt2 = new Opt<>();
+
+		Supplier<Optional<String>> res2 = ($do<Optional, String> opt2) -> {
+			let a = Optional.of("a");
+			let b = Optional.of("b");
+			return a + b;
+		};
+
+		// Optional["ab"]
+		System.out.println(res2.get());
+	}
+
+	static class Opt<T> {
+		public Optional<T> bind(Optional<T> x, Function<T, Optional<T>> f) {
+			return x.flatMap(f);
+		}
+
+		public Optional<T> unit(T v) {
+			return Optional.ofNullable(v);
+		}
+	}
+}
