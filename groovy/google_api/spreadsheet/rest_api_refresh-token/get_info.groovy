694b1c7511353016af980d7de62c635dbbe9272b
fits <wadays_wozx@nifty.com>
2015-06-29 21:31:19 +0900

    REST API を使ってリフレッシュトークンからアクセストークンを取得し、任意のフィード URL を GET するサンプル

---
+++ groovy/google_api/spreadsheet/rest_api_refresh-token/get_info.groovy
@@ -0,0 +1,36 @@
+@Grab("org.apache.httpcomponents:httpclient:4.5")
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.message.BasicNameValuePair
+
+import groovy.json.JsonSlurper
+
+def json = new JsonSlurper()
+
+def conf = json.parse(new File(args[0])).installed
+def token = json.parse(new File(args[1]))
+def feedUrl = args[2]
+
+def param = { name, value -> new BasicNameValuePair(name, value) }
+
+def client = HttpClientBuilder.create().build()
+
+def post = new HttpPost('https://www.googleapis.com/oauth2/v3/token')
+
+post.entity = new UrlEncodedFormEntity([
+	param('client_id', conf.client_id),
+	param('client_secret', conf.client_secret),
+	param('grant_type', 'refresh_token'),
+	param('refresh_token', token.refresh_token)
+])
+
+def res = client.execute(post)
+
+def newToken = json.parse(res.entity.content)
+
+def get = new HttpGet(feedUrl)
+get.addHeader('Authorization', "${newToken.token_type} ${newToken.access_token}")
+
+client.execute(get).entity.writeTo(System.out)
