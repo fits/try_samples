bf3f92bb07216f44ff4725115968b3932646bf92
fits <wadays_wozx@nifty.com>
2015-07-18 19:53:18 +0900

    @OneToMany 関係のデータを登録する処理を追加

--- java/querydsl/sample-jpa/src/main/java/sample/SampleApp.java
+++ java/querydsl/sample-jpa/src/main/java/sample/SampleApp.java
@@ -3,6 +3,7 @@ package sample;
 import com.querydsl.jpa.impl.JPAQuery;
 import lombok.val;
 import sample.model.Product;
+import sample.model.ProductVariation;
 import sample.model.QProduct;
 
 import javax.persistence.Persistence;
@@ -17,15 +18,14 @@ public class SampleApp {
 		val tx = em.getTransaction();
 		tx.begin();
 
-		em.persist(createProduct("sample1", "100"));
-		em.persist(createProduct("sample2", "200"));
-		em.persist(createProduct("abc3", "300"));
+		em.persist(product("sample1", "100", variation("White", "L"), variation("Black", "M")));
+		em.persist(product("sample2", "200"));
+		em.persist(product("abc3", "300"));
 
 		tx.commit();
 
-		QProduct p = QProduct.product;
-
-		JPAQuery<Product> query = new JPAQuery<>(em);
+		val p = QProduct.product;
+		val query = new JPAQuery<Product>(em);
 
 		List<Product> res = query.from(p).where(p.name.startsWith("sample")).fetch();
 
@@ -34,12 +34,26 @@ public class SampleApp {
 		em.close();
 	}
 
-	private static Product createProduct(String name, String price) {
+	private static Product product(String name, String price, ProductVariation... variations) {
 		val res = new Product();
+
 		res.setName(name);
 		res.setPrice(new BigDecimal(price));
+
+		for (val v : variations) {
+			res.getVariationList().add(v);
+			v.setProduct(res);
+		}
 
 		return res;
 	}
 
+	private static ProductVariation variation(String color, String size) {
+		val res = new ProductVariation();
+
+		res.setColor(color);
+		res.setSize(size);
+
+		return res;
+	}
 }
--- java/querydsl/sample-jpa/src/main/java/sample/model/Product.java
+++ java/querydsl/sample-jpa/src/main/java/sample/model/Product.java
@@ -1,16 +1,25 @@
 package sample.model;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 public class Product {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private BigDecimal price;
+
+	@Setter(AccessLevel.NONE)
+	@OneToMany(mappedBy = "product", cascade= CascadeType.ALL)
+	private List<ProductVariation> variationList = new ArrayList<>();
 }
