da84f9af72ef7c9807593957b2c871d024e81f98
fits <wadays_wozx@nifty.com>
2015-08-24 23:09:50 +0900

    別バージョンのサンプル追加

---
+++ java/functional_java/db/db_sample/src/main/java/sample/App5.java
@@ -0,0 +1,79 @@
+package sample;
+
+import fj.control.db.DB;
+import fj.control.db.DbState;
+import fj.data.Option;
+import fj.function.Try1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App5 {
+    public static void main(String... args) throws Exception {
+        DbState dbs = DbState.reader(args[0]);
+
+        DB<?> q = query("select count(*) from product", App5::scalarValue);
+
+        System.out.println(dbs.run(q));
+
+        DbState dbw = DbState.writer(args[0]);
+
+        final String insertVariationSql = "insert into product_variation (product_id, color, size) values (?, ?, ?)";
+
+        DB<?> q2 = command("insert into product (name, price) values (?, ?)", "bbb", 4500)
+                .bind(v -> query("select last_insert_id()", App5::scalarValue))
+                .bind(id ->
+                        command(insertVariationSql, id.some(), "Orange", "F")
+                                .bind(t -> command(insertVariationSql, id.some(), "Pink", "LL")));
+
+        System.out.println(dbw.run(q2));
+    }
+
+    private static Option<Integer> scalarValue(ResultSet rs) {
+        try {
+            return rs.next() ? Option.some(rs.getInt(1)) : Option.none();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static DB<Integer> command(String sql, Object... params) {
+        return DB.db(con -> {
+            try (PreparedStatement ps = createStatement(con, sql, params)) {
+                return ps.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static <T> DB<T> query(String sql, ResultHandler<T> handler, Object... params) {
+        return DB.db(con -> {
+            try (
+                PreparedStatement ps = createStatement(con, sql, params);
+                ResultSet rs = ps.executeQuery()
+            ) {
+                return handler.f(rs);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static PreparedStatement createStatement(Connection con, String sql, Object... params)
+            throws SQLException {
+
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        for (int i = 0; i < params.length; i++) {
+            ps.setObject(i + 1, params[i]);
+        }
+
+        return ps;
+    }
+
+    private interface ResultHandler<T> extends Try1<ResultSet, T, SQLException> {
+    }
+}
