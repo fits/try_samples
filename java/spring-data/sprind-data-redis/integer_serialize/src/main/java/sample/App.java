e53aad3dc6f1e0935a1800aa7077583db922a40c
fits <wadays_wozx@nifty.com>
2015-08-25 23:28:07 +0900

    空白をタブへ変換

--- java/spring-data/sprind-data-redis/integer_serialize/src/main/java/sample/App.java
+++ java/spring-data/sprind-data-redis/integer_serialize/src/main/java/sample/App.java
@@ -10,24 +10,24 @@ import sample.repository.SampleRepository;
 @ComponentScan
 @EnableAutoConfiguration
 public class App implements CommandLineRunner {
-    @Autowired
-    private SampleRepository sampleRepository;
+	@Autowired
+	private SampleRepository sampleRepository;
 
-    @Override
-    public void run(String... args) {
-        String key = "a1";
+	@Override
+	public void run(String... args) {
+		String key = "a1";
 
-        sampleRepository.update(key, v -> 10);
-        System.out.println(sampleRepository.get(key));
+		sampleRepository.update(key, v -> 10);
+		System.out.println(sampleRepository.get(key));
 
-        sampleRepository.updateWithCas1(key, v -> v + 5);
-        System.out.println(sampleRepository.get(key));
+		sampleRepository.updateWithCas1(key, v -> v + 5);
+		System.out.println(sampleRepository.get(key));
 
-        sampleRepository.updateWithCas2(key, v -> v + 8);
-        System.out.println(sampleRepository.get(key));
-    }
+		sampleRepository.updateWithCas2(key, v -> v + 8);
+		System.out.println(sampleRepository.get(key));
+	}
 
-    public static void main(String... args) {
-        SpringApplication.run(App.class, args);
-    }
+	public static void main(String... args) {
+		SpringApplication.run(App.class, args);
+	}
 }
