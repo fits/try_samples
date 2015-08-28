2e850dffd68c810d65c09b4e3904abc72177260a
fits <wadays_wozx@nifty.com>
2015-08-28 18:17:56 +0900

    データの insert 時に自動採番のキーを取得して、明細データも insert する処理を追加

--- scala/slick/slick3_sample/src/main/scala/SampleApp.scala
+++ scala/slick/slick3_sample/src/main/scala/SampleApp.scala
@@ -3,18 +3,42 @@ package sample
 import slick.driver.MySQLDriver.api._
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 class Products(tag: Tag) extends Table[(Int, String, Double)](tag, "product") {
-	def id = column[Int]("id", O.PrimaryKey)
+	def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
 	def name = column[String]("name")
 	def price = column[Double]("price")
 
 	def * = (id, name, price)
 }
 
+object Products {
+	val products = TableQuery[Products]
+}
+
+class ProductVariations(tag: Tag) extends Table[(Int, Int, String, String)](tag, "product_variation") {
+	def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+	def productId = column[Int]("product_id")
+	def color = column[String]("color")
+	def size = column[String]("size")
+
+	def * = (id, productId, color, size)
+
+	def product = foreignKey("", productId, Products.products)(_.id)
+}
+
+object ProductVariations {
+	val productVariations = TableQuery[ProductVariations]
+}
+
+
 object SampleApp extends App {
 	val db = Database.forConfig("sample")
 
-	val products = TableQuery[Products]
+	val products = Products.products
+	val variations = ProductVariations.productVariations
 
 	try {
 		val q = for {
@@ -24,19 +48,23 @@ object SampleApp extends App {
 		db.run(q.result).map(_.foreach(println))
 
 		val insert = DBIO.seq {
-			products.map(p => (p.name, p.price)) += ("slick01", 200.0)
-		}
+			val idRes = products.map(p => (p.name, p.price)).returning(products.map(_.id)) += ("slick" + System.currentTimeMillis(), 200.0)
+
+			println(products.map(p => (p.name, p.price)).returning(products.map(_.id)).insertStatement)
+
+			idRes.flatMap {id =>
+				variations.map(v => (v.productId, v.color, v.size)) ++= Seq(
+					(id, "Cyan", "L"),
+					(id, "Black", "M")
+				)
+			}
+		}.transactionally
 
 		val res = db.run(insert)
 
-		res onSuccess {
-			case msg => println(msg)
-		}
+		Await.result(res, Duration.Inf)
 
-		res onFailure {
-			case msg => println(msg)
-		}
-
+		res foreach println
 
 	} finally db.close
 }
