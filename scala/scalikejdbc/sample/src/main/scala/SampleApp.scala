26860d1b2ddd5e1bf07a1340b331be3a66b313d2
fits <wadays_wozx@nifty.com>
2015-08-29 13:16:50 +0900

    ScalikeJDBC の簡単なサンプル

---
+++ scala/scalikejdbc/sample/src/main/scala/SampleApp.scala
@@ -0,0 +1,19 @@
+package sample
+
+import scalikejdbc._
+import scalikejdbc.config._
+
+object SampleApp extends App {
+	DBs.setupAll()
+
+	try {
+		val price = 3000.0
+
+		val res = DB readOnly { implicit session =>
+			sql"select name from product where price > ${price}".map(rs => rs.string("name")).toList().apply()
+		}
+
+		println(res)
+
+	} finally DBs.closeAll()
+}
