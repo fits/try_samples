f2fd297fe97a9568fde8cc4f44f09651c034413a
fits <wadays_wozx@nifty.com>
2015-10-01 23:48:22 +0900

    Go言語による MySQL 接続サンプル

---
+++ go/mysql/sample/src/main/main3.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root@/sample")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	rows, err := db.Query("select id, name, price, release_date from product where price > ?", 100)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	var (
+		id int
+		name string
+		price int
+		date mysql.NullTime
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name, &price, &date)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("id=%d, name=%s, price=%d, date=%s\n", id, name, price, date)	}
+}
+
