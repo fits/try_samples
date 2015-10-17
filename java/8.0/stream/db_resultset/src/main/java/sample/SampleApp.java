7b3037c4f3066d7995f575cd82b81d287e2949b3
fits <wadays_wozx@nifty.com>
2015-10-17 19:45:33 +0900

    count の結果を確認する処理を追加

--- java/8.0/stream/db_resultset/src/main/java/sample/SampleApp.java
+++ java/8.0/stream/db_resultset/src/main/java/sample/SampleApp.java
@@ -5,17 +5,42 @@ import java.sql.*;
 public class SampleApp {
 	public static void main(String... args) throws Exception {
 
-		String sql = "select * from product";
+		try (Connection con = DriverManager.getConnection(args[0])) {
+			sample1(con);
+
+			System.out.println("---");
+
+			sample2(con);
+		}
+	}
+
+	private static void sample1(Connection con) throws SQLException {
+		String sql = "select * from product p join product_variation v on v.product_id=p.id";
 
 		try (
-			Connection con = DriverManager.getConnection(args[0]);
 			PreparedStatement ps = con.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery()
 		) {
-			new ResultSetSpliterator<String>(
-					rs,
-					r -> r.getString("id") + " : " + r.getString("name")
+			new ResultSetSpliterator<>(
+				rs,
+				r -> r.getString("name") + ", " + r.getString("color") + "," + r.getString("size")
 			).stream().forEach(System.out::println);
+		}
+	}
+
+	private static void sample2(Connection con) throws SQLException {
+		String sql = "select * from product p join product_variation v on v.product_id=p.id";
+
+		try (
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery()
+		) {
+			long count = new ResultSetSpliterator<>(
+				rs,
+				r -> r.getString("id")
+			).stream().count();
+
+			System.out.println("count : " + count);
 		}
 	}
 }
