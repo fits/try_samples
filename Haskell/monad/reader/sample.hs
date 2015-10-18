258f05e458f840bd86df13ea4273ebc6cf0720d9
fits <wadays_wozx@nifty.com>
2015-10-18 12:04:32 +0900

    import を変更

--- Haskell/monad/reader/sample.hs
+++ Haskell/monad/reader/sample.hs
@@ -1,20 +1,20 @@
 
-import Control.Monad.Instances
+import Control.Monad.Reader
 
 f :: Int -> Int
 f = do
-	a <- (*2)
-	b <- (+10)
-	return (a + b)
+    a <- (*2)
+    b <- (+10)
+    return (a + b)
 
 f2 :: Int -> Int
 f2 = do
-	a <- (*2)
-	b <- (+10)
-	c <- (+5)
-	return (a + b + c)
+    a <- (*2)
+    b <- (+10)
+    c <- (+5)
+    return (a + b + c)
 
 main = do
-	putStrLn $ show $ f 4
-	putStrLn $ show $ f2 4
+    putStrLn $ show $ f 4
+    putStrLn $ show $ f2 4
 
