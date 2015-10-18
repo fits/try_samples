451de8ea345f75c7b0ee4e7bdc92954a71955d06
fits <wadays_wozx@nifty.com>
2015-10-18 13:33:19 +0900

    Writer モナドのサンプル

---
+++ Haskell/monad/writer/sample.hs
@@ -0,0 +1,8 @@
+import Control.Monad.Writer
+
+logNum :: Int -> Int -> Writer [Int] Int
+logNum n x = writer (x + n, [x])
+
+main = do
+    print $ runWriter (return 1 :: Writer [Int] Int)
+    print $ runWriter $ (return 1 :: Writer [Int] Int) >>= logNum 2 >>= logNum 3
