b19916c0dbfd53ee041bc0f73710e5d89b17701e
fits <wadays_wozx@nifty.com>
2015-11-03 12:17:06 +0900

    chainer のサンプル

---
+++ python/chainer/iris_classification/iris_classification.py
@@ -0,0 +1,71 @@
+
+import sys
+import csv
+import chainer
+import chainer.functions as F
+import chainer.optimizers
+import numpy as np
+
+num = 5
+
+iris_type = {"setosa": 0, "versicolor": 1, "virginica": 2}
+
+def dataset_tuple(items):
+	return ( [ np.float32(v) for v in items[0:4] ], np.int32(iris_type[items[4]]) )
+
+lines = list(csv.reader(open(sys.argv[1], 'r')))
+
+dataset = [ dataset_tuple(v) for v in lines[1:] ]
+
+model = chainer.FunctionSet(
+	l1 = F.Linear(4, num),
+	l2 = F.Linear(num, 3),
+)
+
+optimizer = chainer.optimizers.Adam()
+#optimizer = chainer.optimizers.SGD()
+optimizer.setup(model.parameters)
+
+def forward(x):
+	u2 = model.l1(x)
+	#z2 = F.sigmoid(u2)
+	z2 = F.relu(u2)
+	u3 = model.l2(z2)
+	return F.sigmoid(u3)
+
+def loss(h, t):
+	return F.softmax_cross_entropy(h, t)
+
+def to_variable(a):
+	return chainer.Variable(np.asarray(a))
+
+def learn(dataset, batchsize = 10, times = 1):
+	datasize = len(dataset)
+
+	for n in range(times):
+		perm = np.random.permutation(datasize)
+
+		ds = np.array(dataset)
+
+		for i in range(0, datasize, batchsize):
+			tds = ds[perm[i : i + batchsize]]
+
+			x = to_variable([ d[0] for d in tds ])
+			t = to_variable([ d[1] for d in tds ])
+
+			optimizer.zero_grads()
+
+			h = forward(x)
+			e = loss(h, t)
+
+			a = F.accuracy(h, t)
+
+			print(e.data)
+			print(a.data)
+
+			e.backward()
+			optimizer.update()
+
+learn(dataset, 30, 5)
+
+print(model.parameters)
