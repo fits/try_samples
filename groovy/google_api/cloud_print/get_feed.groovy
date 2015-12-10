04465756a26a294093cc9bf137dc33a949506137
fits <wadays_wozx@nifty.com>
2015-12-10 23:59:28 +0900

    Google Cloud Print を REST で使用するサンプル （リフレッシュトークン使用）

---
+++ groovy/google_api/cloud_print/get_feed.groovy
@@ -0,0 +1,25 @@
+@Grab('com.mashape.unirest:unirest-java:1.4.7')
+import com.mashape.unirest.http.Unirest
+
+import groovy.json.JsonSlurper
+
+def json = new JsonSlurper()
+
+def conf = json.parse(new File(args[0])).installed
+def token = json.parse(new File(args[1]))
+def feedUrl = args[2]
+
+def res = Unirest.post('https://www.googleapis.com/oauth2/v3/token')
+	.field('client_id', conf.client_id)
+	.field('client_secret', conf.client_secret)
+	.field('grant_type', 'refresh_token')
+	.field('refresh_token', token.refresh_token)
+	.asJson()
+
+def newToken = res.body.object
+
+def res2 = Unirest.get(feedUrl)
+	.header('Authorization', "${newToken.token_type} ${newToken.access_token}")
+	.asJson()
+
+println res2.body
