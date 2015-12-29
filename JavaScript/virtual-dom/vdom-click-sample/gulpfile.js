627a990472eb1ce6c05fdc1d2589a96ffd2eb99a
fits <wadays_wozx@nifty.com>
2015-12-29 16:25:17 +0900

    gulp を使った Virtual-DOM + DOM-Delegator のサンプル

---
+++ JavaScript/virtual-dom/vdom-click-sample/gulpfile.js
@@ -0,0 +1,24 @@
+
+var fs = require('fs');
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+
+var flatten = require('gulp-flatten');
+
+gulp.task('js-dist', () => {
+	gulp.src('bower_components/*/dist/*.js')
+		.pipe(flatten())
+		.pipe(gulp.dest('js'));
+});
+
+gulp.task('browserify', () => {
+
+	browserify({
+		require: 'dom-delegator',
+		standalone: 'DOMDelegator'
+	}).bundle().pipe(fs.createWriteStream('js/dom-delegator.js'));
+
+});
+
+gulp.task('default', ['js-dist', 'browserify']);
