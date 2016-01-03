0345bf6251b525d82a0d463139879ce6ec2a377c
fits <wadays_wozx@nifty.com>
2016-01-03 22:48:20 +0900

    コメントを削除

--- Haskell/arrow/sample/arrow_sample1.hs
+++ Haskell/arrow/sample/arrow_sample1.hs
@@ -2,20 +2,19 @@
 import Control.Arrow
 
 main = do
-	let plus = arr $ (+) 3
-	-- 22
-	print $ plus 4
+    let plus = arr $ (+) 3
+    print $ plus 4
 
-	let times = arr $ (*) 2
-	print $ times 3
+    let times = arr $ (*) 2
+    print $ times 3
 
-	-- (4 + 3) * 2 = 14
-	let f = plus >>> times
-	print $ f 4
+    -- (4 + 3) * 2 = 14
+    let f = plus >>> times
+    print $ f 4
 
-	-- (3 + 3, 5 * 2) = (6, 10)
-	print $ plus *** times $ (3, 5)
-	print $ first plus >>> second times $ (3, 5)
+    -- (3 + 3, 5 * 2) = (6, 10)
+    print $ plus *** times $ (3, 5)
+    print $ first plus >>> second times $ (3, 5)
 
-	-- (4 + 3, 4 * 2) = (7, 8)
-	print $ plus &&& times $ 4
+    -- (4 + 3, 4 * 2) = (7, 8)
+    print $ plus &&& times $ 4
