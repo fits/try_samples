601a57fdbed8d49085534381e486cb7b1c14a1d1
fits <wadays_wozx@nifty.com>
2016-01-15 00:19:32 +0900

    html の内容を変更

--- JavaScript/node.js/hyperscript/html2hscript_sample/sample.js
+++ JavaScript/node.js/hyperscript/html2hscript_sample/sample.js
@@ -2,6 +2,6 @@
 var Promise = require('bluebird');
 var parser = Promise.promisify(require('html2hscript'));
 
-var html = '<div name="sample1" id="a">sample<br></br><ul var="items" v-click="items_click"><li><a href="item1.html">item1</a></li><li><a href="item2.html">item2</a></li></ul></div>';
+var html = '<div name="sample1" id="a">sample<br /><ul var="items" v-click="items_click"><li><a href="item1.html">item1</a></li><li><a href="item2.html">item2</a></li></ul></div>';
 
 parser(html).then( hs => console.log(hs) );
