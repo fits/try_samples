ab70d5cfc8b9066ffd6d1da0e4bc6f63a5a42650
fits <wadays_wozx@nifty.com>
2016-01-15 00:06:37 +0900

    分割代入のサンプル

---
+++ JavaScript/node.js/ECMAScript6/destructuring_sample.js
@@ -0,0 +1,10 @@
+// node --harmony_destructuring <file>
+
+var [a1, a2] = [10, 20];
+
+console.log(a2);
+
+var {name, value} = {name: 'sample1', note: '', value: 5};
+
+console.log(name);
+console.log(value);
