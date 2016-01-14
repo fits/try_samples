ede46482ab0dddbaf74ce46e65372be86182e7fa
fits <wadays_wozx@nifty.com>
2016-01-15 00:15:34 +0900

    html2hscript のサンプル

---
+++ JavaScript/node.js/hyperscript/html2hscript_sample/sample.js
@@ -0,0 +1,7 @@
+
+var Promise = require('bluebird');
+var parser = Promise.promisify(require('html2hscript'));
+
+var html = '<div name="sample1" id="a">sample<br></br><ul var="items" v-click="items_click"><li><a href="item1.html">item1</a></li><li><a href="item2.html">item2</a></li></ul></div>';
+
+parser(html).then( hs => console.log(hs) );
