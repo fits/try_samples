eb6d5e525e8486b6e5c3fe7091c4ed9d7af6bb08
fits <wadays_wozx@nifty.com>
2016-06-27 20:05:10 +0900

    blog sample: node-ffi で OpenCL を使う

---
+++ blog/20160627/node-ffi_opencl/get_device_id.js
@@ -0,0 +1,41 @@
+
+'use strict';
+
+const ffi = require('ffi');
+const ref = require('ref');
+
+const CL_DEVICE_TYPE_DEFAULT = 1;
+
+const uintPtr = ref.refType(ref.types.uint32);
+const sizeTPtr = ref.refType('size_t');
+
+const openCl = ffi.Library('OpenCL', {
+	'clGetPlatformIDs': ['int', ['uint', sizeTPtr, uintPtr]],
+	'clGetDeviceIDs': ['int', ['size_t', 'ulong', 'uint', sizeTPtr, uintPtr]]
+});
+
+const checkError = (errCode, title = '') => {
+	if (errCode != 0) {
+		throw new Error(`${title} Error: ${errCode}`);
+	}
+};
+
+const platformIdsPtr = ref.alloc(sizeTPtr);
+
+let res = openCl.clGetPlatformIDs(1, platformIdsPtr, null);
+
+checkError(res, 'clGetPlatformIDs');
+
+const platformId = sizeTPtr.get(platformIdsPtr);
+
+console.log(`platformId: ${platformId}`);
+
+const deviceIdsPtr = ref.alloc(sizeTPtr);
+
+res = openCl.clGetDeviceIDs(platformId, CL_DEVICE_TYPE_DEFAULT, 1, deviceIdsPtr, null);
+
+checkError(res, 'clGetDeviceIDs');
+
+const deviceId = sizeTPtr.get(deviceIdsPtr);
+
+console.log(`deviceId: ${deviceId}`);
