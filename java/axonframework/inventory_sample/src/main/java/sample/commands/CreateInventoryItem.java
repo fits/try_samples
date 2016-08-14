f3e8b4bb2636d4f2a62ba3c17fe4a2c6cb826749
fits <wadays_wozx@nifty.com>
2016-08-14 13:08:08 +0900

    @TargetAggregateIdentifierを付与 （ただし、付けなくても動作は変わらない模様）

--- java/axonframework/inventory_sample/src/main/java/sample/commands/CreateInventoryItem.java
+++ java/axonframework/inventory_sample/src/main/java/sample/commands/CreateInventoryItem.java
@@ -1,9 +1,12 @@
 package sample.commands;
 
+import org.axonframework.commandhandling.TargetAggregateIdentifier;
+
 import lombok.Value;
 
 @Value
 public class CreateInventoryItem {
+    @TargetAggregateIdentifier
     private String id;
     private String name;
 }
