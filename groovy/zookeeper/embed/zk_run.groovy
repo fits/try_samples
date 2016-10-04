3d4652ec03814e843cbe5e8bef1cb6f4452b1a13
fits <wadays_wozx@nifty.com>
2016-10-05 01:10:10 +0900

    ZooKeeper 3.4.9 へ変更

--- groovy/zookeeper/embed/zk_run.groovy
+++ groovy/zookeeper/embed/zk_run.groovy
@@ -1,10 +1,7 @@
 // ZooKeeper クラスタリング実行版
-@Grapes([
-    @Grab("org.apache.zookeeper:zookeeper:3.4.5"),
-    @GrabExclude("com.sun.jmx#jmxri"),
-    @GrabExclude("com.sun.jdmk#jmxtools"),
-    @GrabExclude("javax.jms#jms")
-])
+@Grab('org.apache.zookeeper:zookeeper:3.4.9')
+@Grab('org.slf4j:slf4j-simple:1.7.21')
+@Grab('log4j:log4j:1.2.17') // "java.lang.NoClassDefFoundError: org/apache/log4j/jmx/HierarchyDynamicMBean" の対策
 import org.apache.zookeeper.server.quorum.QuorumPeerMain
 
 if (args.length < 2) {
