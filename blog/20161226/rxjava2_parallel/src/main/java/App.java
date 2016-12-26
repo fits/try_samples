7c374dea1e2573fa629504f7e33f86afc296ae55
fits <wadays_wozx@nifty.com>
2016-12-26 20:25:22 +0900

    blog sample: RxJava2で並列処理

---
+++ blog/20161226/rxjava2_parallel/src/main/java/App.java
@@ -0,0 +1,84 @@
+import io.reactivex.Single;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+
+import java.lang.invoke.MethodHandle;
+
+import static java.lang.invoke.MethodHandles.publicLookup;
+import static java.lang.invoke.MethodType.methodType;
+
+public class App {
+    public static void main(String... args) throws Throwable {
+        MethodHandle mh = publicLookup().findStatic(
+            App.class, 
+            "sample" + args[0], 
+            methodType(void.class)
+        );
+
+        mh.invoke();
+
+        System.in.read();
+    }
+
+    public static void sampleA() {
+        Single.just("(A)")
+            .repeat(3)
+            .map(n -> {
+                printThread(n + "_map");
+                return n;
+            })
+            .subscribe(n -> printThread(n + "_subscribe"));
+    }
+
+    public static void sampleB() {
+        Single.just("(B)")
+            .repeat(3)
+            .observeOn(Schedulers.computation())
+            //.observeOn(Schedulers.single())
+            .map(n -> {
+                printThread(n + "_map");
+                return n;
+            })
+            .subscribe(n -> printThread(n + "_subscribe"));
+    }
+
+    public static void sampleC() {
+        Single.just("(C)")
+            .repeat(3)
+            .flatMap(s ->
+                Flowable.just(s)
+                    .map(n -> {
+                        printThread(n + "_map");
+                        return n;
+                    })
+                    .subscribeOn(Schedulers.computation())
+                    //.subscribeOn(Schedulers.newThread())
+
+            )
+            .subscribe(n -> printThread(n + "_subscribe"));
+    }
+
+    public static void sampleD() {
+        Single.just("(D)")
+            .repeat(3)
+            .flatMap(s ->
+                Flowable.just(s)
+                    .map(n -> {
+                        printThread(n + "_map");
+                        return n;
+                    })
+                    .map(n -> {
+                        printThread(n + "_subscribe");
+                        return n;
+                    })
+                    .subscribeOn(Schedulers.computation())
+
+            )
+            .subscribe();
+    }
+
+
+    private static void printThread(String msg) {
+        System.out.println(msg + ", thread: " + Thread.currentThread());
+    }
+}
