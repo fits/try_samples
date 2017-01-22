20321c77cb391e8ab9e8dbad88c42dc4b8f394b9
fits <wadays_wozx@nifty.com>
2017-01-22 21:45:22 +0900

    rampUsers を使った処理へ変更

--- scala/gatling/sample_gradle/src/main/scala/Sample.scala
+++ scala/gatling/sample_gradle/src/main/scala/Sample.scala
@@ -2,6 +2,8 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import scala.concurrent.duration._
+
 class Sample extends Simulation {
 	val httpConf = http.baseURL("http://localhost:8081/")
 
@@ -9,5 +11,6 @@ class Sample extends Simulation {
 		http("top").get("/")
 	)
 
-	setUp(scn.inject(atOnceUsers(1))).protocols(httpConf)
+	// setUp(scn.inject(atOnceUsers(1))).protocols(httpConf)
+	setUp(scn.inject(rampUsers(100) over(10 seconds))).protocols(httpConf)
 }
