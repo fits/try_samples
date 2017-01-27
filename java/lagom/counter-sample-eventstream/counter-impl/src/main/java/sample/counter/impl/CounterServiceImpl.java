7683d817adf12dee7361d5a71c8406e564a2c4d6
fits <wadays_wozx@nifty.com>
2017-01-28 01:51:31 +0900

    Offset の値をファイルへ読み書きする処理を追加

--- java/lagom/counter-sample-eventstream/counter-impl/src/main/java/sample/counter/impl/CounterServiceImpl.java
+++ java/lagom/counter-sample-eventstream/counter-impl/src/main/java/sample/counter/impl/CounterServiceImpl.java
@@ -3,7 +3,13 @@ package sample.counter.impl;
 import akka.NotUsed;
 
 import akka.japi.Pair;
+import akka.japi.pf.Match;
+import akka.japi.pf.PFBuilder;
 import akka.stream.Materializer;
+import akka.stream.javadsl.FileIO;
+import akka.stream.javadsl.Source;
+import akka.util.ByteString;
+import akka.util.ByteString.ByteStrings;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 import com.lightbend.lagom.javadsl.persistence.Offset;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntityRef;
@@ -16,19 +22,26 @@ import sample.counter.api.Counter;
 import sample.counter.api.CounterService;
 import sample.counter.impl.CounterCommand.*;
 
+import java.nio.file.Paths;
+import java.util.UUID;
+
 public class CounterServiceImpl implements CounterService {
-    private final PersistentEntityRegistry persistentEntityRegistry;
+    private static final String OFFSET_FILE = "offset.dat";
+
+    private final PersistentEntityRegistry entityRegistry;
+    private final Materializer materializer;
 
     @Inject
-    public CounterServiceImpl(PersistentEntityRegistry persistentEntityRegistry,
+    public CounterServiceImpl(PersistentEntityRegistry entityRegistry,
                               Materializer materializer) {
 
-        this.persistentEntityRegistry = persistentEntityRegistry;
-        persistentEntityRegistry.register(CounterEntity.class);
+        this.entityRegistry = entityRegistry;
+        entityRegistry.register(CounterEntity.class);
 
-        // TODO: Offset の復元
+        this.materializer = materializer;
 
-        persistentEntityRegistry.eventStream(CounterEvent.TAG, Offset.NONE)
+        offsetSource()
+                .flatMapConcat(offset -> entityRegistry.eventStream(CounterEvent.TAG, offset))
                 .runForeach(this::subscribe, materializer);
     }
 
@@ -49,11 +62,35 @@ public class CounterServiceImpl implements CounterService {
 
     private void subscribe(Pair<CounterEvent, Offset> data) {
         System.out.println("*** subscribe: " + data);
-
-        // TODO: Offset の保存
+        saveOffset(data.second());
     }
 
     private PersistentEntityRef<CounterCommand> getEntity(String id) {
-        return persistentEntityRegistry.refFor(CounterEntity.class, id);
+        return entityRegistry.refFor(CounterEntity.class, id);
+    }
+
+    private void saveOffset(Offset offset) {
+        Source.single(offset)
+                .map(Offset::toString)
+                .map(ByteStrings::fromString)
+                .runWith(FileIO.toPath(Paths.get(OFFSET_FILE)), materializer);
+    }
+
+    private Source<Offset, ?> offsetSource() {
+        PFBuilder<Throwable, Offset> pfunc = Match.match(Exception.class, e -> Offset.NONE);
+
+        return FileIO.fromPath(Paths.get(OFFSET_FILE))
+                .map(ByteString::utf8String)
+                .map(this::toOffset)
+                .recover(pfunc.build());
+    }
+
+    private Offset toOffset(String str) {
+        if (str.contains("-")) {
+            return Offset.timeBasedUUID(UUID.fromString(str));
+        }
+        else {
+            return Offset.sequence(Long.parseLong(str));
+        }
     }
 }
