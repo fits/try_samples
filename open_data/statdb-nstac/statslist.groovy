cc1a1b547435804a6f59020d74ae6e36c42a6617
fits <wadays_wozx@nifty.com>
2017-02-11 02:49:56 +0900

    Unirest へ変更

--- open_data/statdb-nstac/statslist.groovy
+++ open_data/statdb-nstac/statslist.groovy
@@ -1,6 +1,5 @@
-@Grab("org.apache.httpcomponents:httpclient:4.3")
-import org.apache.http.client.methods.*
-import org.apache.http.impl.client.*
+@Grab('com.mashape.unirest:unirest-java:1.4.7')
+import com.mashape.unirest.http.Unirest
 
 /**
  * 次世代統計利用システム API を使って統計表情報を取得するスクリプト
@@ -10,13 +9,9 @@ def enc = 'UTF-8'
 
 def appId = args[0]
 def statsCode = args[1]
-def destFileName = (args.length > 2)? args[2]: "statslist_${statsCode}.xml"
 
-def url = "http://statdb.nstac.go.jp/api/1.0b/app/getStatsList?appId=${appId}&statsCode=${statsCode}"
+def url = "http://api.e-stat.go.jp/rest/2.0/app/getStatsList?appId=${appId}&statsCode=${statsCode}"
 
-def httpClient = new DefaultHttpClient()
-def res = httpClient.execute(new HttpGet(url))
+def res = Unirest.get(url).asXml()
 
-new File(destFileName).withWriter(enc) {
-	it.write res.entity.content.getText(enc)
-}
+println res.body
