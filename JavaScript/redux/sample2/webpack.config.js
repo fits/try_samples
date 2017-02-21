6c9bd6ee89afeb67b56138bde524cf61133036bc
fits <wadays_wozx@nifty.com>
2017-02-22 01:32:18 +0900

    redux サンプル （webpack 利用版）

---
+++ JavaScript/redux/sample2/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: './index.js',
+	output: {
+		filename: 'bundle.js',
+		path: './build'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+}
