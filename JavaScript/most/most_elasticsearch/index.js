c6ada8602a55c1800730fe73b8e763d658f0be53
fits <wadays_wozx@nifty.com>
2017-02-24 01:27:56 +0900

    Most を使った Elasticsearch の検索サンプル

---
+++ JavaScript/most/most_elasticsearch/index.js
@@ -0,0 +1,43 @@
+
+import { fromEvent, fromPromise, of } from 'most';
+import Hogan from 'hogan.js';
+
+const wordNode = document.getElementById('word');
+
+const tpl = Hogan.compile(
+	document.getElementById('tpl').innerHTML
+);
+
+const renderItems = rs => 
+	document.getElementById('result').innerHTML = tpl.render({items: rs});
+
+const searchParam = word => {
+	return JSON.stringify({
+		query: {
+			term: {
+				_all: word
+			}
+		}
+	});
+};
+
+const search = word => 
+	fromPromise(
+		fetch('http://localhost:9200/sample/_search', {
+			body: searchParam(word),
+			method: 'POST'
+		})
+	)
+	.flatMap(r => fromPromise(r.json()))
+	.map(r => r.hits.hits)
+	.recoverWith(err => of([]));
+
+const toItem = rs => rs.map(r => {
+	return { id: r._id, source: JSON.stringify(r._source) };
+});
+
+fromEvent('click', document.getElementById('search'))
+	.map(ev => wordNode.value)
+	.flatMap(search)
+	.map(toItem)
+	.observe(renderItems);
