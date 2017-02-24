cc17ce38a5b3e6bf15a0ffc6b6390167dc7d54da
fits <wadays_wozx@nifty.com>
2017-02-25 02:05:17 +0900

    Mithril を使った Elasticsearch の検索サンプル

---
+++ JavaScript/mithril/mithril_elasticsearch/index.js
@@ -0,0 +1,41 @@
+
+import m from 'mithril';
+
+let items = [];
+let word = '';
+
+const doSearch = () => m.request({
+	url: 'http://localhost:9200/sample/_search',
+	method: 'POST',
+	data: {
+		query: {
+			term: {
+				_all: word
+			}
+		}
+	}
+})
+.then(r => items = r.hits.hits);
+
+const SearchResult = {
+	view: (vnode) => 
+		items.map(it => m('div', `${it._id}, ${JSON.stringify(it._source)}`))
+};
+
+const Search = {
+	view: (vnode) => m('div', [
+		m('input[type = text]', {
+			oninput: m.withAttr('value', v => word = v),
+			onkeydown: (ev) => {
+				if (ev.which === 13) {
+					doSearch();
+				}
+			}
+		}),
+		m('button', { onclick: doSearch }, 'search'),
+		m('p', 'Result:'),
+		m(SearchResult)
+	])
+};
+
+m.mount(document.getElementById('app'), Search);
