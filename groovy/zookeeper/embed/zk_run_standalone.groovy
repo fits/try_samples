359299455f841c19caf06fb20903c6f6e582a21c
fits <wadays_wozx@nifty.com>
2017-03-05 12:57:54 +0900

    ZooKeeper のバージョンアップ

--- groovy/zookeeper/embed/zk_run_standalone.groovy
+++ groovy/zookeeper/embed/zk_run_standalone.groovy
@@ -1,7 +1,7 @@
 // ZooKeeper 単体実行版
-@Grab('org.apache.zookeeper:zookeeper:3.4.9')
-@Grab('org.slf4j:slf4j-simple:1.7.21')
-@Grab('log4j:log4j:1.2.17') // "java.lang.NoClassDefFoundError: org/apache/log4j/jmx/HierarchyDynamicMBean" の対策
+@Grab('org.apache.zookeeper:zookeeper:3.5.2-alpha')
+@Grab('org.slf4j:slf4j-simple:1.7.24')
+@Grab('org.mortbay.jetty:jetty:6.1.26')
 import org.apache.zookeeper.server.ZooKeeperServerMain
 
 ZooKeeperServerMain.main(args)
