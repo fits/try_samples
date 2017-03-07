c6aef6f0dc5045ca4011c6914db0f40acdd0feee
fits <wadays_wozx@nifty.com>
2017-03-07 23:39:24 +0900

    Flink 1.2.0 へバージョンアップ

--- java/Flink/count_sample/src/main/java/SampleApp.java
+++ java/Flink/count_sample/src/main/java/SampleApp.java
@@ -6,7 +6,7 @@ import org.apache.flink.util.Collector;
 
 public class SampleApp {
     public static void main(String... args) throws Exception {
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
 
         DataSet<String> input = env.fromElements(
             "1 10 5 50 100 1 10", 
@@ -24,7 +24,7 @@ public class SampleApp {
     private static class SampleSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
         @Override
         public void flatMap(String value, Collector<Tuple2<String, Integer>> output) throws Exception {
-            for (String s : value.split(" ")) {
+            for (String s : value.split("\\s")) {
                 output.collect(new Tuple2<>(s, 1));
             }
         }
