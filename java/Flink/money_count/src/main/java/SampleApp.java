f340ec6d3f2f7aa119111bd48c3f2d64f814978e
fits <wadays_wozx@nifty.com>
2017-03-09 00:52:24 +0900

    ファイルの内容をカウントする Flink のサンプル

---
+++ java/Flink/money_count/src/main/java/SampleApp.java
@@ -0,0 +1,15 @@
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+public class SampleApp {
+    public static void main(String... args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
+
+        env.readTextFile(args[0])
+            .map( w -> new Tuple2<>(w, 1) )
+            .groupBy(0)
+            .sum(1)
+            .print();
+    }
+}
