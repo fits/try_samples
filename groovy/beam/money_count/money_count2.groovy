be6894f5929cf998e6b67ab367fef9dc1984621d
fits <wadays_wozx@nifty.com>
2017-03-09 03:13:43 +0900

    改良版

---
+++ groovy/beam/money_count/money_count2.groovy
@@ -0,0 +1,21 @@
+@Grab('org.apache.beam:beam-runners-direct-java:0.5.0')
+@Grab('org.slf4j:slf4j-simple:1.7.24')
+import org.apache.beam.sdk.options.PipelineOptionsFactory
+import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.io.TextIO
+import org.apache.beam.sdk.transforms.*
+import org.apache.beam.sdk.values.*
+import groovy.transform.CompileStatic
+
+def srcFile = args[0]
+def destFile = args[1]
+
+def opt = PipelineOptionsFactory.create()
+def p = Pipeline.create(opt)
+
+p.apply(TextIO.Read.from(srcFile))
+	.apply(Count.perElement())
+	.apply(ToString.kv())
+	.apply(TextIO.Write.to(destFile).withNumShards(1))
+
+p.run().waitUntilFinish()
