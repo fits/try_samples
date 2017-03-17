7fe1a065f1913aa873850ea9ef8f33b1c45aa84b
fits <wadays_wozx@nifty.com>
2017-03-18 02:21:23 +0900

    Apache Curator を使って ZooKeeper をテスト実行するサンプル

---
+++ groovy/curator/zk_testing_server/sample.groovy
@@ -0,0 +1,19 @@
+@Grapes([
+	@Grab('org.apache.curator:curator-test:3.3.0'),
+	@GrabExclude('io.netty#netty:3.7.0.Final')
+])
+@Grab('org.slf4j:slf4j-nop:1.7.25')
+import org.apache.curator.test.TestingServer
+
+def zkPort = args.length > 0 ? args[0] as int : 2181
+def zkDir = args.length > 1 ? new File(args[1]) : null
+
+new TestingServer(zkPort, zkDir, false).withCloseable { server ->
+	server.start()
+
+	println 'started ...'
+
+	System.in.read()
+
+	server.stop()
+}
