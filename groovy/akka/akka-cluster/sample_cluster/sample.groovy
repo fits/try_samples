0a2ec2c62a53e9698742e213256bc2abd2c68b0e
fits <wadays_wozx@nifty.com>
2017-03-22 01:51:05 +0900

    Akka Cluster の挙動を確認するためのサンプル

---
+++ groovy/akka/akka-cluster/sample_cluster/sample.groovy
@@ -0,0 +1,45 @@
+@Grab('com.typesafe.akka:akka-cluster_2.12:2.5.0-RC1')
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.UntypedActor
+
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
+
+class SampleListener extends UntypedActor {
+	def cluster = Cluster.get(getContext().system())
+
+	void preStart() {
+		println '*** preStart'
+
+		cluster.subscribe(
+			getSelf(), 
+			ClusterEvent.initialStateAsEvents(), 
+			ClusterEvent.MemberEvent, 
+			ClusterEvent.UnreachableMember
+		)
+	}
+
+	void postStop() {
+		println '*** postStop'
+
+		cluster.leave(cluster.selfAddress())
+		cluster.unsubscribe(getSelf())
+	}
+
+	void onReceive(msg) {
+		println "*** receive: ${msg}"
+	}
+}
+
+def system = ActorSystem.create('sample')
+
+system.actorOf(Props.create(SampleListener))
+
+System.in.read()
+
+Await.ready(system.terminate(), Duration.create(5, TimeUnit.SECONDS))
