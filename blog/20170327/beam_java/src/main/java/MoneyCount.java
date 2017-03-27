b8f7aef2966c9e5b929a75c6dc898926b80c4ca6
fits <wadays_wozx@nifty.com>
2017-03-27 22:54:43 +0900

    blog sample: Java で Apache Beam を使用

---
+++ blog/20170327/beam_java/src/main/java/MoneyCount.java
@@ -0,0 +1,27 @@
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.ToString;
+
+public class MoneyCount {
+    public static void main(String... args) throws Exception {
+        PipelineOptions opt = PipelineOptionsFactory.create();
+        Pipeline p = Pipeline.create(opt);
+
+        p.apply(TextIO.Read.from(args[0]))
+                .apply(Count.perElement())
+                .apply(ToString.kvs())
+                .apply(ParDo.of(new DoFn<String, String>() {
+                    @ProcessElement
+                    public void process(ProcessContext ctx) {
+                        System.out.println(ctx.element());
+                    }
+                }));
+
+        p.run().waitUntilFinish();
+    }
+}
