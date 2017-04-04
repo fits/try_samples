c3cff48ffbc3492faa24d07b26faac41f64ddcd9
fits <wadays_wozx@nifty.com>
2017-04-05 01:57:22 +0900

    評価処理を追加

--- kotlin/deeplearning4j/rnn_sample/src/main/kotlin/SampleApp.kt
+++ kotlin/deeplearning4j/rnn_sample/src/main/kotlin/SampleApp.kt
@@ -1,6 +1,7 @@
 import org.datavec.api.records.reader.impl.csv.CSVSequenceRecordReader
 import org.datavec.api.split.NumberedFileInputSplit
 import org.deeplearning4j.datasets.datavec.SequenceRecordReaderDataSetIterator
+import org.deeplearning4j.eval.RegressionEvaluation
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration
 import org.deeplearning4j.nn.conf.Updater
 import org.deeplearning4j.nn.conf.layers.GravesLSTM
@@ -9,23 +10,24 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.nd4j.linalg.activations.Activation
+import org.nd4j.linalg.dataset.DataSet
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler
 import org.nd4j.linalg.lossfunctions.LossFunctions
 
 fun main(args: Array<String>) {
     val miniBatchSize = 20
-    val iterations = 300
+    val iterations = 500
+    val neuNum = 8
 
-    val trainReader = CSVSequenceRecordReader()
-    trainReader.initialize(NumberedFileInputSplit("data/train_%d.csv", 0, 0))
-
-    val trainData = SequenceRecordReaderDataSetIterator(trainReader, miniBatchSize, -1, 1, true).next()
+    val trainData = loadData("data/train_%d.csv", miniBatchSize)
+    val testData = loadData("data/test_%d.csv", miniBatchSize)
 
     val normalizer = NormalizerMinMaxScaler()
     normalizer.fitLabel(true)
     normalizer.fit(trainData)
 
     normalizer.preProcess(trainData)
+    normalizer.preProcess(testData)
 
     val conf = NeuralNetConfiguration.Builder()
             .iterations(iterations)
@@ -36,7 +38,7 @@ fun main(args: Array<String>) {
                     GravesLSTM.Builder()
                             .activation(Activation.TANH)
                             .nIn(1)
-                            .nOut(10)
+                            .nOut(neuNum)
                             .build(),
                     RnnOutputLayer.Builder(LossFunctions.LossFunction.MSE)
                             .activation(Activation.IDENTITY)
@@ -51,4 +53,25 @@ fun main(args: Array<String>) {
     net.setListeners(ScoreIterationListener(20))
 
     net.fit(trainData)
+
+    net.rnnTimeStep(trainData.featureMatrix)
+    val predict = net.rnnTimeStep(testData.featureMatrix)
+
+    val evaluation = RegressionEvaluation(1)
+
+    evaluation.evalTimeSeries(testData.labels, predict)
+
+    println(evaluation.stats())
+
+    normalizer.revertLabels(predict)
+
+    println(predict)
+}
+
+fun loadData(path: String, miniBatch: Int = 10): DataSet {
+    val trainReader = CSVSequenceRecordReader()
+    trainReader.initialize(NumberedFileInputSplit(path, 0, 0))
+
+    return SequenceRecordReaderDataSetIterator(trainReader, miniBatch, -1, 1, true)
+            .next()
 }
