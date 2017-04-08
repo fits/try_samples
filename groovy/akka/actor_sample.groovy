ff58d871daf82bc7ee2c22ee48026525b5e98892
fits <wadays_wozx@nifty.com>
2017-04-08 22:15:39 +0900

    AbstractActor へ変更

--- groovy/akka/actor_sample.groovy
+++ groovy/akka/actor_sample.groovy
@@ -1,12 +1,21 @@
-@Grab('com.typesafe.akka:akka-actor_2.12:2.5-M1')
-import akka.actor.*
+@Grab('com.typesafe.akka:akka-actor_2.12:2.5.0-RC2')
+import akka.actor.ActorSystem
+import akka.actor.ActorRef
+import akka.actor.AbstractActor
+import akka.actor.AbstractActor.Receive
+import akka.actor.Props
+
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
 
-class SampleActor extends UntypedActor {
-	void onReceive(msg) {
-		println "*** receive: ${msg}"
+class SampleActor extends AbstractActor {
+	Receive createReceive() {
+		receiveBuilder()
+			.match(String) { msg ->
+				println "*** receive: ${msg}"
+			}
+			.build()
 	}
 }
 
