7dd4e7bb6dcf38f5150dbf44bad7992a906a8a3f
fits <wadays_wozx@nifty.com>
2017-04-20 01:03:46 +0900

    Concourse で mvn package を実行するサンプル

---
+++ concourse/mvn-sample/sample1/src/main/java/SampleApp.java
@@ -0,0 +1,8 @@
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SampleApp {
+    public static void main(String... args) {
+        System.out.println(new ObjectMapper());
+    }
+}
