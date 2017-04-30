7b584ac08051f2ffc1c22e67849723ee5617f694
fits <wadays_wozx@nifty.com>
2017-04-30 17:01:22 +0900

    MimeType を加工する処理を追加

--- python/har_parse/har_scatter_plot.py
+++ python/har_parse/har_scatter_plot.py
@@ -8,12 +8,15 @@ import matplotlib.pyplot as plt
 
 data = json.load(codecs.open(sys.argv[1], 'r', 'utf-8'))
 
+to_type = lambda mtype: mtype.split(';')[0].split('/')[1]
+
 convert = lambda d: {
     'url': d['request']['url'],
     'method': d['request']['method'],
     'status': d['response']['status'],
     'body-size': d['response']['bodySize'],
     'mime-type': d['response']['content']['mimeType'],
+    'type': to_type(d['response']['content']['mimeType']),
     'wait': d['timings']['wait'],
     'receive': d['timings']['receive'],
     'time': d['time']
@@ -21,7 +24,7 @@ convert = lambda d: {
 
 df = pd.DataFrame(list(map(convert, data['log']['entries'])))
 
-g = sns.PairGrid(df, hue = 'mime-type', x_vars = ['time'], y_vars = ['body-size'], size = 10)
+g = sns.PairGrid(df, hue = 'type', x_vars = ['time'], y_vars = ['body-size'], size = 10)
 
 g.map(plt.scatter, linewidths = 8).add_legend()
 
