11b948f323ed3a4f47a2298ee50e6d1e46e396bd
fits <wadays_wozx@nifty.com>
2017-04-30 16:32:37 +0900

    HAR ファイルをパースして散布図を出力するサンプル

---
+++ python/har_parse/har_scatter_plot.py
@@ -0,0 +1,30 @@
+
+import sys
+import json
+import codecs
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = json.load(codecs.open(sys.argv[1], 'r', 'utf-8'))
+
+convert = lambda d: {
+    'url': d['request']['url'],
+    'method': d['request']['method'],
+    'status': d['response']['status'],
+    'body-size': d['response']['bodySize'],
+    'mime-type': d['response']['content']['mimeType'],
+    'wait': d['timings']['wait'],
+    'receive': d['timings']['receive'],
+    'time': d['time']
+}
+
+df = pd.DataFrame(list(map(convert, data['log']['entries'])))
+
+g = sns.PairGrid(df, hue = 'mime-type', x_vars = ['time'], y_vars = ['body-size'], size = 10)
+
+g.map(plt.scatter, linewidths = 8).add_legend()
+
+imgfile = "%s.png" % sys.argv[1]
+
+plt.savefig(imgfile)
