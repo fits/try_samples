ff3b592a2fb30cdde6a5f847b1247b090218c422
fits <wadays_wozx@nifty.com>
2017-05-02 16:22:04 +0900

    凡例の表示を追加

--- JavaScript/node.js/d3.js/har_parse/har_scatter_plot/index.js
+++ JavaScript/node.js/d3.js/har_parse/har_scatter_plot/index.js
@@ -6,7 +6,11 @@ const { JSDOM } = jsdom;
 
 const w = 800;
 const h = 500;
-const margin = { left: 50, top: 20, bottom: 50, right: 30 };
+const margin = { left: 50, top: 20, bottom: 50, right: 150 };
+const legendMargin = { top: 30 };
+
+const fontSize = '12pt';
+const circleRadius = 5;
 
 const xDomain = [0, 5000];
 const yDomain = [0, 50000];
@@ -45,12 +49,13 @@ process.stdin.on('end', () => {
 		};
 	});
 
-	const svg = d3.select(document.body).append('svg')
-					.attr('xmlns', 'http://www.w3.org/2000/svg')
-					.attr('width', w + margin.left + margin.right)
-					.attr('height', h + margin.top + margin.bottom)
-					.append('g')
-						.attr('transform', `translate(${margin.left}, ${margin.top})`);
+	const svg = d3.select(document.body)
+		.append('svg')
+			.attr('xmlns', 'http://www.w3.org/2000/svg')
+			.attr('width', w + margin.left + margin.right)
+			.attr('height', h + margin.top + margin.bottom)
+			.append('g')
+				.attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 	const x = d3.scaleLinear().range([0, w]).domain(xDomain);
 	const y = d3.scaleLinear().range([h, 0]).domain(yDomain);
@@ -68,19 +73,40 @@ process.stdin.on('end', () => {
 	svg.append('text')
 		.attr('x', w / 2)
 		.attr('y', h + 40)
+		.style('font-size', fontSize)
 		.text('Time (ms)');
 
-	const cr = svg.selectAll('circle')
-					.data(df)
-					.enter()
-						.append('circle');
+	const point = svg.selectAll('circle')
+		.data(df)
+		.enter()
+			.append('circle');
 
-	cr.attr('cx', d => x(d.time))
+	point.attr('cx', d => x(d.time))
 		.attr('cy', d => y(d.bodySize))
-		.attr('r', 5)
+		.attr('r', circleRadius)
 		.attr('fill', d => toColor(d.subType));
+
+	const legend = svg.selectAll('.legend')
+		.data(d3.entries(colorMap))
+		.enter()
+			.append('g')
+				.attr('class', 'legend')
+				.attr('transform', (d, i) => {
+					const left = w + margin.left;
+					const top = margin.top + i * legendMargin.top;
+
+					return `translate(${left}, ${top})`;
+				});
+
+	legend.append('circle')
+			.attr('r', circleRadius)
+			.attr('fill', d => d.value);
+
+	legend.append('text')
+			.attr('x', circleRadius * 2)
+			.attr('y', 4)
+			.style('font-size', fontSize)
+			.text(d => d.key);
 
 	console.log(document.body.innerHTML);
 });
-
-
