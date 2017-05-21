761de5736b31aa2753874d62832df1bb56f953ab
fits <wadays_wozx@nifty.com>
2017-05-21 20:06:01 +0900

    .NET Core SDK で作成した F# コンソールアプリケーションのサンプル

---
+++ fsharp/dotnetcore/sample/Program.fs
@@ -0,0 +1,15 @@
+﻿
+open System
+
+[<EntryPoint>]
+let main argv =
+    let plus x = x + 3
+    let times x = x * 2
+
+    let f = plus >> times
+    printfn "%i" (f 4)
+
+    let g = plus << times
+    printfn "%i" (g 4)
+
+    0
