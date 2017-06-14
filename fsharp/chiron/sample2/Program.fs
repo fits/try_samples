5b6d236d318dcbd934ebbad460c27e460619d7ff
fits <wadays_wozx@nifty.com>
2017-06-15 01:40:32 +0900

    レコードを使った JSON 変換サンプル

---
+++ fsharp/chiron/sample2/Program.fs
@@ -0,0 +1,19 @@
+﻿
+open System
+open Chiron
+open Chiron.Inference
+
+type Data =
+    { Id : string
+      Value : int }
+
+    static member ToJson (x: Data) =
+        Json.encode (Map.ofList [ ("id", x.Id); ("value", string x.Value) ])
+
+[<EntryPoint>]
+let main argv =
+    let d = { Id = "a1"; Value = 123 }
+    let res = Json.serialize d
+
+    printfn "json = %s" res
+    0
