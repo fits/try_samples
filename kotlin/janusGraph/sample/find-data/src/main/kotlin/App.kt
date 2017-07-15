da5dc5723c7ab0edd65854f7b5a7e056166c6d6d
fits <wadays_wozx@nifty.com>
2017-07-15 19:13:20 +0900

    JanusGraph のサンプル

---
+++ kotlin/janusGraph/sample/find-data/src/main/kotlin/App.kt
@@ -0,0 +1,25 @@
+
+import org.apache.tinkerpop.gremlin.structure.Vertex
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.`__`.*
+import org.janusgraph.core.JanusGraphFactory
+
+fun main(args: Array<String>) {
+    val conf = args[0]
+    val start = args[1]
+    val end = args[2]
+
+    JanusGraphFactory.open(conf).use { g ->
+        g.tx().use {
+            val p = g.traversal().V()
+                    .has("oid", start)
+                    .repeat(outE<Vertex>().`as`("e").inV())
+                    .until(has<Vertex>("oid", end))
+                    .where(select<Vertex, Vertex>("e").unfold<Vertex>().hasLabel("PERMIT"))
+                    .path()
+
+            p.forEach {
+                println(it.objects())
+            }
+        }
+    }
+}
