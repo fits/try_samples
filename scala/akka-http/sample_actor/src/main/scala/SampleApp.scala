d85bbf77e966aff5c4a3b99cc7c5432ef234a293
fits <wadays_wozx@nifty.com>
2017-08-26 07:14:40 +0900

    Actor を呼び出す方法を追加

--- scala/akka-http/sample_actor/src/main/scala/SampleApp.scala
+++ scala/akka-http/sample_actor/src/main/scala/SampleApp.scala
@@ -3,15 +3,19 @@ import akka.actor.{Actor, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshalling.ToResponseMarshaller
 import akka.http.scaladsl.server.Directives._
+import akka.pattern.ask
 import akka.stream.ActorMaterializer
+import akka.util.Timeout
 
+import scala.concurrent.duration._
 import scala.io.StdIn
 
 case class RequestData(value: String, proc: String => Unit)
 
 class RequestHandler extends Actor {
   override def receive: Receive = {
-    case RequestData(v, p) => p(s"ok: ${v}")
+    case RequestData(v, p) => p(s"sample1: ${v}")
+    case v: String => sender ! s"sample2: ${v}"
   }
 }
 
@@ -23,11 +27,18 @@ object SampleApp extends App {
   val actor = system.actorOf(Props[RequestHandler])
 
   val route = get {
-    path("sample") {
+    path("sample1") {
       parameter("v") { v =>
         completeWith(implicitly[ToResponseMarshaller[String]]) { f =>
           actor ! RequestData(v, f)
         }
+      }
+    } ~
+    path("sample2" / Segment) { v: String =>
+      implicit val timeout = Timeout(1 seconds)
+
+      onSuccess(actor ? v) { r =>
+        complete(s"result ${r}")
       }
     }
   }
