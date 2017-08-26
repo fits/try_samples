4a7586e2d3ea66401dd2e111438202a6f806d372
fits <wadays_wozx@nifty.com>
2017-08-26 20:28:16 +0900

    Akka HTTP の WebSocket 処理のサンプル

---
+++ scala/akka-http/sample_websocket/src/main/scala/SampleApp.scala
@@ -0,0 +1,43 @@
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Flow
+
+import scala.io.StdIn
+
+object SampleApp extends App {
+  implicit val system = ActorSystem("sample")
+  implicit val executionContext = system.dispatcher
+  implicit val materializer = ActorMaterializer()
+
+  val websocketFlow = { name: String =>
+    Flow[Message].collect {
+      case TextMessage.Strict(msg) => {
+        println(s"*** receive ${name}: ${msg}")
+        TextMessage.Strict(s"${name}: ${msg}")
+      }
+    }
+  }
+
+  val route = get {
+    pathSingleSlash {
+      getFromResource("web/index.html")
+    } ~
+    path("sample") {
+      parameter("name") { name =>
+        handleWebSocketMessages(websocketFlow(name))
+      }
+    }
+  }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println("started server ...")
+
+  StdIn.readLine()
+
+  bindingFuture.flatMap(_.unbind).onComplete(_ => system.terminate())
+}
