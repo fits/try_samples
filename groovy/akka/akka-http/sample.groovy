39608caf050090ee003cd2065b6dcb9923d0d587
fits <wadays_wozx@nifty.com>
2017-08-27 23:31:03 +0900

    Akka HTTP のサンプル

---
+++ groovy/akka/akka-http/sample.groovy
@@ -0,0 +1,38 @@
+@Grab('com.typesafe.akka:akka-http_2.12:10.0.9')
+import akka.actor.*
+import akka.http.javadsl.*
+import akka.http.javadsl.server.AllDirectives
+import akka.stream.ActorMaterializer
+
+class SampleRoute extends AllDirectives {
+	def createRoute() {
+		route(
+			path('sample') {
+				complete('sample')
+			}
+		)
+	}
+}
+
+def system = ActorSystem.create("sample")
+def mat = ActorMaterializer.create(system)
+
+def http = Http.get(system)
+
+def routeFlow = new SampleRoute().createRoute().flow(system, mat)
+
+def binding = http.bindAndHandle(
+	routeFlow, 
+	ConnectHttp.toHost('localhost', 8080),
+	mat
+)
+
+println 'start ...'
+
+System.in.read()
+
+binding.thenCompose {
+	it.unbind()
+}.thenAccept {
+	system.terminate()
+}
