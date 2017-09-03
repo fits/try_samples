faa11ced385d4b3f3a096d27569337ba52a5ce76
fits <wadays_wozx@nifty.com>
2017-09-03 17:07:04 +0900

    一定の間隔で MQTT Broker へランダムな値を publish するサンプル

---
+++ groovy/akka/akka-streams/alpakka/mqtt_random_publish_sample.groovy
@@ -0,0 +1,43 @@
+@Grab('com.typesafe.akka:akka-stream_2.12:2.5.4')
+@Grab('com.lightbend.akka:akka-stream-alpakka-mqtt_2.12:0.11')
+import akka.actor.ActorSystem
+import akka.japi.Pair
+import akka.stream.ActorMaterializer
+import akka.stream.javadsl.Source
+import akka.stream.alpakka.mqtt.MqttQoS
+import akka.stream.alpakka.mqtt.MqttConnectionSettings
+import akka.stream.alpakka.mqtt.MqttMessage
+import akka.stream.alpakka.mqtt.javadsl.MqttSink
+import akka.util.ByteString
+
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
+
+def topic = args[0]
+def clientId = args[1]
+
+def system = ActorSystem.create()
+def mat = ActorMaterializer.create(system)
+
+def settings = MqttConnectionSettings.create(
+	'tcp://localhost:1883',
+	clientId,
+	new org.eclipse.paho.client.mqttv3.persist.MemoryPersistence()
+)
+
+Source.tick(
+	Duration.create(0, TimeUnit.SECONDS), 
+	Duration.create(1, TimeUnit.SECONDS),
+	1
+).map {
+	def value = Math.random()
+
+	println "random value: ${value}"
+
+	MqttMessage.create(topic, ByteString.fromString(value.toString()))
+
+}.runWith(MqttSink.create(settings, MqttQoS.atMostOnce()), mat)
+
+System.in.read()
+
+system.terminate()
