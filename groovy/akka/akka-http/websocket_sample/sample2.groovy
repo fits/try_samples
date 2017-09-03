a1a308133072f6c758f1b4f1b21732609f92e5ab
fits <wadays_wozx@nifty.com>
2017-09-03 13:42:01 +0900

    Flow.fromSinkAndSource を使ったサンプル

---
+++ groovy/akka/akka-http/websocket_sample/sample2.groovy
@@ -0,0 +1,123 @@
+@Grab('com.typesafe.akka:akka-http_2.12:10.0.9')
+@Grab('com.typesafe.akka:akka-stream_2.12:2.5.4')
+import static akka.http.javadsl.server.Directives.*
+import akka.NotUsed
+import akka.actor.*
+import akka.actor.AbstractActor.Receive
+import akka.http.javadsl.Http
+import akka.http.javadsl.ConnectHttp
+import akka.http.javadsl.model.ws.*
+import akka.stream.ActorMaterializer
+import akka.stream.OverflowStrategy
+import akka.stream.javadsl.Source
+import akka.stream.javadsl.Sink
+import akka.stream.javadsl.Flow
+
+class Ready {}
+class Done {}
+
+class SampleActor extends AbstractActor {
+	String name
+	Optional<ActorRef> subscriber = Optional.empty()
+
+	SampleActor(String name) {
+		this.name = name
+	}
+
+	void postStop() {
+		println "*** post stop: ${name}"
+	}
+
+	Receive createReceive() {
+		receiveBuilder()
+			.match(Ready) { msg ->
+				println "**** ready - ${name}: ${msg}"
+
+				context.watch(sender())
+				subscriber = Optional.of(sender())
+			}
+			.match(Done) {
+				println "**** complete - ${name}, ${it}"
+				subscriber = Optional.empty()
+				context().stop(self())
+			}
+			.match(Terminated) {
+				println "**** terminated - ${name}, ${it}"
+				context().stop(self())
+			}
+			.match(String) { msg ->
+				println "*** received - ${name}: ${msg}"
+
+				subscriber.map {
+					it.tell("${name}: ${msg}", self())
+				}
+			}
+			.matchAny { msg ->
+				println "*** no match: ${msg}"
+			}
+			.build()
+	}
+}
+
+class Util {
+	static def websocketFlow(ActorSystem system, String name) {
+		def actor = system.actorOf(Props.create(SampleActor, name))
+
+		def sink = Flow.create().map {
+			println "*** ws : ${it}"
+			it.strictText
+		}.to( Sink.actorRef(actor, new Done()) )
+
+		def source = Source.actorRef(5, OverflowStrategy.fail()).mapMaterializedValue {
+			actor.tell(new Ready(), it)
+			NotUsed
+		}.map {
+			TextMessage.create(it)
+		}
+
+		Flow.fromSinkAndSource(sink, source)
+	}
+}
+
+
+def system = ActorSystem.create("sample")
+def mat = ActorMaterializer.create(system)
+
+def http = Http.get(system)
+
+def createRoute = {
+	route(
+		get {
+			pathSingleSlash {
+				getFromResource('index.html')
+			}
+		},
+		get {
+			path('sample') {
+				parameter('name') { name -> 
+					handleWebSocketMessages(
+						Util.websocketFlow(system, name)
+					)
+				}
+			}
+		}
+	)
+}
+
+def routeFlow = createRoute().flow(system, mat)
+
+def binding = http.bindAndHandle(
+	routeFlow, 
+	ConnectHttp.toHost('localhost', 8080),
+	mat
+)
+
+println 'start ...'
+
+System.in.read()
+
+binding.thenCompose {
+	it.unbind()
+}.thenAccept {
+	system.terminate()
+}
