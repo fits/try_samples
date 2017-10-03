d2863164947a0d108c2181c3680d05da50e2ba70
fits <wadays_wozx@nifty.com>
2017-10-04 03:24:48 +0900

    &mut を使った処理を追加

--- rust/sample/reference_sample.rs
+++ rust/sample/reference_sample.rs
@@ -1,12 +1,24 @@
 
 fn sample(v: &Vec<i32>) {
-	println!("sample.v: {:?}", v);
+	println!("sample: {:?}", v);
 }
+
+fn add(v: &mut Vec<i32>, i: i32) {
+	v.push(i);
+	println!("add: {:?}", v);
+}
+
 
 fn main() {
 	let v = vec![1, 2];
 
 	sample(&v);
 
-	println!("{:?}", v);
+	println!("v = {:?}", v);
+
+	let mut a = vec![];
+
+	add(&mut a, 3);
+
+	println!("a = {:?}", a);
 }
