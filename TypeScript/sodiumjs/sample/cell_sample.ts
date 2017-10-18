418125e31945c966d3c473cfe93dbc1d04931a19
fits <wadays_wozx@nifty.com>
2017-10-19 01:22:13 +0900

    unlisten の追加

--- TypeScript/sodiumjs/sample/cell_sample.ts
+++ TypeScript/sodiumjs/sample/cell_sample.ts
@@ -4,9 +4,13 @@ import { Cell } from 'sodiumjs'
 const d1 = new Cell<number>(123)
 const d2 = d1.map(v => v * 2)
 
-d1.listen(v => console.log(`d1 = ${v}`))
-d2.listen(v => console.log(`d2 = ${v}`))
+const l1 = d1.listen(v => console.log(`d1 = ${v}`))
+const l2 = d2.listen(v => console.log(`d2 = ${v}`))
 
 const d3 = d1.lift(d2, (a, b) => a + b)
 
 console.log(`d3 = ${d3.sample()}`)
+
+// unlisten
+l1()
+l2()
