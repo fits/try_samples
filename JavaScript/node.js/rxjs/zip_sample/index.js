770f9b79b223f8dba3d2e42d839f1d2c4ba87a78
fits <wadays_wozx@nifty.com>
2017-10-21 12:19:53 +0900

    zip 処理のサンプル

---
+++ JavaScript/node.js/rxjs/zip_sample/index.js
@@ -0,0 +1,35 @@
+
+const Rx = require('rxjs/Rx')
+
+const category = new Rx.Subject()
+const value = new Rx.Subject()
+
+category.subscribe(c => console.log(`category = ${c}`))
+value.subscribe(v => console.log(`value = ${v}`))
+
+const src = new Rx.Subject()
+
+src.filter(d => d.type == 'category')
+	.map(d => d.data)
+	.subscribe(category)
+
+src.filter(d => d.type == 'value')
+	.map(d => d.data)
+	.subscribe(value)
+
+category.zip(value)
+	.subscribe(v => console.log(`*** combine = ${v}`))
+
+src.next({type: 'category', data: 'type1'})
+
+src.next({type: 'value', data: '1'})
+
+src.next({type: 'category', data: 'type2'})
+
+src.next({type: 'value', data: '2'})
+
+src.next({type: 'invalid', data: 'test'})
+
+src.next({type: 'value', data: '3'})
+
+src.next({type: 'category', data: 'type3'})
