1e6603ef03b73d0b4a7dce7c07b19e8f52f53a4f
fits <wadays_wozx@nifty.com>
2017-11-27 22:19:59 +0900

    流行性耳下腺炎の AR モデル構築

---
+++ open_data/idwr/ar12.py
@@ -0,0 +1,23 @@
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.ar_model import AR
+
+data_file = sys.argv[1]
+dest_file = sys.argv[2]
+
+df = pd.read_csv(data_file, encoding = 'Shift_JIS')
+
+d = df.groupby(['year', 'week']).sum().iloc[:, 12].astype('float').values
+
+d_ar = AR(d)
+
+res = d_ar.fit(3)
+
+print(f"aic = {res.aic}")
+
+plt.plot(d)
+plt.plot(range(2, 250), res.predict(3, 250))
+
+plt.savefig(dest_file)
