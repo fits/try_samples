b8c7963fc1ba43eb5416e3fdace6f598b223f458
fits <wadays_wozx@nifty.com>
2017-12-03 16:00:25 +0900

    リファクタリング

--- python/statsmodels/ar_sample.py
+++ python/statsmodels/ar_sample.py
@@ -11,22 +11,22 @@ dest_file = sys.argv[3]
 
 df = pd.read_csv(data_file, encoding = 'Shift_JIS')
 
-d = df.groupby(['year', 'week'])[item_name].sum().values
-
-ar = AR(d)
+dv = df.groupby(['year', 'week'])[item_name].sum().values
 
 def ar_fit(p):
-    res = ar.fit(p)
-    return (p, res.aic, res)
+	return (p, AR(dv).fit(p))
 
 r = reduce(
-    lambda a, b: a if a[1] <= b[1] else b,
+    lambda a, b: a if a[1].aic <= b[1].aic else b,
     map(ar_fit, range(1, 3))
 )
 
-print(f"p = {r[0]}, aic = {r[1]}")
+print(f"p = {r[0]}, aic = {r[1].aic}")
 
-plt.plot(d)
-plt.plot(range(r[0], 251), r[2].predict(r[0], 250))
+plt.plot(dv)
+
+start = r[0]
+
+plt.plot(range(start, 251), r[1].predict(start, 250))
 
 plt.savefig(dest_file)
