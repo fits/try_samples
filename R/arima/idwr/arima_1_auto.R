164d4cd3fea805491f7437559a0fca35fabca254
fits <wadays_wozx@nifty.com>
2017-12-03 19:13:49 +0900

    auto.arima を使ったサンプル

---
+++ R/arima/idwr/arima_1_auto.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(forecast)
+
+df <- read.csv('idwr.csv') %>% 
+  dplyr::group_by(year, week) %>% 
+  dplyr::summarise(Data = sum(インフルエンザ))
+
+df.ts <- ts(df$Data, frequency = 52)
+
+df.arima <- auto.arima(df.ts)
+
+df.pred <- forecast(df.arima, h = 50)
+
+print(df.arima$arma)
+
+plot(df$ts, xlim = c(1, 6), ylim = c(0, max(df$Data)), type = 'l')
+
+lines(df.arima$fitted, col = 'red')
+lines(df.pred$mean, col = 'green')
+
+#dev.off()
