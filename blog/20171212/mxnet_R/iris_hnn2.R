df95ae41539b82d1e0c49ef9018d704dde3dca4d
fits <wadays_wozx@nifty.com>
2017-12-12 20:54:31 +0900

    blog sample: R の MXNet で iris を分類

---
+++ blog/20171212/mxnet_R/iris_hnn2.R
@@ -0,0 +1,38 @@
+library(mxnet)
+
+train_size = 0.7
+
+n = nrow(iris)
+perm = sample(n, size = round(n * train_size))
+
+train <- iris[perm, ]
+test <-iris[-perm, ]
+
+train.x <- data.matrix(train[1:4])
+# 0-2
+train.y <- as.numeric(train$Species) - 1
+
+test.x <- data.matrix(test[1:4])
+# 1-3
+test.y <- as.numeric(test$Species)
+
+mx.set.seed(0)
+
+model <- mx.mlp(train.x, train.y, 
+                hidden_node = c(10, 6), 
+                out_node = 3,
+                num.round = 100,
+                learning.rate = 0.1,
+                array.batch.size = 10,
+                activation = 'relu',
+                array.layout = 'rowmajor',
+                eval.metric = mx.metric.accuracy)
+
+pred <- predict(model, test.x, array.layout = 'rowmajor')
+
+# 1-3
+pred.y <- max.col(t(pred))
+
+acc <- sum(pred.y == test.y) / length(pred.y)
+
+print(acc)
