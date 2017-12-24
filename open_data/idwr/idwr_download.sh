91e776773f7466ced1eab94c6c20b84252d126ba
fits <wadays_wozx@nifty.com>
2017-12-24 16:33:02 +0900

    53週の考慮を追加

--- open_data/idwr/idwr_download.sh
+++ open_data/idwr/idwr_download.sh
@@ -5,7 +5,7 @@ ua="Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/1
 dest=$1
 year=$2
 
-for i in {1..52}; do
+for i in {1..53}; do
 	csv=$(printf "${year}-%02d-teiten.csv" $i)
 	url=$(printf "https://www.niid.go.jp/niid/images/idwr/sokuho/idwr-${year}/${year}%02d/$csv" $i)
 
