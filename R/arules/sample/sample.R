677a0721d26237b697b4368c478d3da8a2821cad
fits <wadays_wozx@nifty.com>
2017-12-28 02:43:35 +0900

    arules を使ったアソシエーション分析のサンプル

---
+++ R/arules/sample/sample.R
@@ -0,0 +1,9 @@
+library(arules)
+
+tr <- read.transactions("data/sample.basket", format = "basket", sep = ",")
+
+#inspect(tr)
+
+tr.ap <- apriori(tr, parameter = list(supp = 0.05, conf = 0.6))
+
+inspect(sort(tr.ap, by = "lift"))
