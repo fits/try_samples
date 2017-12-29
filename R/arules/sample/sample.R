496577ed31fd54557469637c9c128cdd6f204ee5
fits <wadays_wozx@nifty.com>
2017-12-29 20:29:40 +0900

    グラフへの出力を追加

--- R/arules/sample/sample.R
+++ R/arules/sample/sample.R
@@ -3,6 +3,8 @@ library(arules)
 tr <- read.transactions("data/sample.basket", format = "basket", sep = ",")
 
 #inspect(tr)
+itemFrequencyPlot(tr, type = "absolute")
+summary(tr)
 
 tr.ap <- apriori(tr, parameter = list(supp = 0.05, conf = 0.6))
 
