060a8a6a955fc0c6fd935d4048809a4918c80a09
fits <wadays_wozx@nifty.com>
2017-12-29 14:14:00 +0900

    修正

--- python/orange/orange3-associate/sample.py
+++ python/orange/orange3-associate/sample.py
@@ -11,7 +11,7 @@ for P, Q, supp, conf in association_rules(itemsets, 0.6):
     pitems = OneHot.decode(P, tbl, mapping)
     qitems = OneHot.decode(Q, tbl, mapping)
 
-    lhs = next(pitems)[1]
-    rhs = next(qitems)[1]
+    lhs = list(map(lambda v: v[1].name, pitems))
+    rhs = list(map(lambda v: v[1].name, qitems))
 
     print(f"lhs = {lhs}, rhs = {rhs}, support = {supp}, confidence = {conf}")
