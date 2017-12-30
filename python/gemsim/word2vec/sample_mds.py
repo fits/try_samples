a4550bf91f64cef5343f80c4503ed6e6247ee3eb
fits <wadays_wozx@nifty.com>
2017-12-30 12:39:56 +0900

    Word2Vec の結果を MDS でグラフ化するサンプル

---
+++ python/gemsim/word2vec/sample_mds.py
@@ -0,0 +1,26 @@
+
+import sys
+import matplotlib.pyplot as plt
+from gensim.models import word2vec
+from sklearn import manifold
+
+data_file = sys.argv[1]
+dest_img = sys.argv[2]
+
+sentences = word2vec.LineSentence(data_file)
+
+model = word2vec.Word2Vec(sentences, size = 30, sg = 1, window = 2, min_count = 2, iter = 5000)
+
+mds = manifold.MDS(n_components = 2)
+
+x = [model[k] for k in model.wv.index2word]
+
+xt = mds.fit_transform(x)
+
+print(xt)
+
+for i in range(len(model.wv.index2word)):
+    plt.scatter(xt[i][0], xt[i][1])
+    plt.annotate(model.wv.index2word[i], (xt[i][0], xt[i][1]))
+
+plt.savefig(dest_img)
