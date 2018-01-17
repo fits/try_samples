4388b17195f0108d3a164e54a6e52415d129388e
fits <wadays_wozx@nifty.com>
2018-01-18 02:07:58 +0900

    週の算出方法を修正

--- python/keras/idwr/idwr_rnn_test.py
+++ python/keras/idwr/idwr_rnn_test.py
@@ -2,6 +2,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from datetime import datetime
 from functools import reduce
 from keras.models import load_model
 
@@ -21,13 +22,13 @@ ds = df.groupby(['year', 'week'])[item_name].sum()
 
 model = load_model(model_file)
 
-max_week = max(ds.index.levels[1])
-
 def predict(a, b):
     r = model.predict(a[1])
 
     y = a[1][0, 0, 0]
     w = a[1][0, 1, 0] + 1
+
+    max_week = datetime(int(y), 12, 28).isocalendar()[1]
 
     if w > max_week:
         y += 1
@@ -44,10 +45,11 @@ pred = reduce(predict, range(predict_size), (np.array([]), fst_data))
 plt.xlim(-10, predict_max)
 
 min_year = min(ds.index.levels[0])
+years = range(min_year, min_year + int(predict_max / 52) + 1)
 
 plt.xticks(
-    range(0, predict_max, max_week), 
-    range(min_year, min_year + int(predict_max / max_week) + 1)
+    reduce(lambda a, b: a + [a[-1] + datetime(b, 12, 28).isocalendar()[1]], years[:-1], [0]), 
+    years
 )
 
 
