0364d5f8099667041d604eb15aa655daa03b0ee4
fits <wadays_wozx@nifty.com>
2018-01-24 02:26:12 +0900

    改良

--- JavaScript/node.js/ramda/groupby_sample/multi_groupby.js
+++ JavaScript/node.js/ramda/groupby_sample/multi_groupby.js
@@ -12,7 +12,7 @@ const data = [
 	{category: 'B', item: 'B01', week: '2018-01', num: 1}
 ]
 
-const groupByMulti = (fs, data) => R.reduce(
+const groupByMulti = R.curry((fs, data) => R.reduce(
 	(a, b) => R.reduce(
 		(x, y) => R.mapObjIndexed(x), 
 		R.groupBy(R.prop(fs[b])), 
@@ -20,7 +20,7 @@ const groupByMulti = (fs, data) => R.reduce(
 	)(a), 
 	data, 
 	R.range(0, fs.length)
-)
+))
 
 console.log( groupByMulti(['category'], data) )
 console.log( groupByMulti(['category', 'item'], data) )
