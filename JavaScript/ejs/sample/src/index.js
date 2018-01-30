530e5bb1f78a78090d210328e55d4a335437bee5
fits <wadays_wozx@nifty.com>
2018-01-31 00:14:04 +0900

    table の表示を実装

--- JavaScript/ejs/sample/src/index.js
+++ JavaScript/ejs/sample/src/index.js
@@ -3,6 +3,21 @@ import 'ejs/ejs.min.js'
 
 const ct = document.getElementById('tpl').innerHTML
 
-const data = {title: 'sample'}
+const data = {
+    items: {
+        'A1': {
+            'item1': 1,
+            'item2': 2,
+            'item3': 3
+        },
+        'B2': {
+            'item4': 4
+        },
+        'C3': {
+            'item5': 5,
+            'item6': 6
+        }
+    }
+}
 
 document.getElementById('content').innerHTML = ejs.render(ct, data)
