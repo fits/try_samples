ebd106dd05bbee35e776d5987eaebe1384cc2d46
fits <wadays_wozx@nifty.com>
2018-03-10 03:05:52 +0900

    コーパスを2次元配列化する処理

---
+++ python/gemsim/lda/corpus_dense.py
@@ -0,0 +1,24 @@
+
+import sys
+
+from gensim.corpora import Dictionary
+from gensim.matutils import corpus2dense
+from gensim.models import word2vec
+
+data_file = sys.argv[1]
+
+sentences = [s for s in word2vec.LineSentence(data_file)]
+
+dic = Dictionary(sentences)
+
+corpus = [dic.doc2bow(s) for s in sentences]
+
+ds = corpus2dense(corpus, len(dic))
+
+print(len(ds))
+print(ds)
+
+print('-----')
+
+print(len(ds.T))
+print(ds.T)
