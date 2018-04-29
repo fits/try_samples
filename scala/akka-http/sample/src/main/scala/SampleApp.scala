c6739cd644e835726c75274c3e5dc954ccb4d90a
fits <wadays_wozx@nifty.com>
2018-04-29 11:29:55 +0900

    ライブラリのバージョンアップ

--- scala/akka-http/sample/src/main/scala/SampleApp.scala
+++ scala/akka-http/sample/src/main/scala/SampleApp.scala
@@ -4,12 +4,13 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 
+import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
 object SampleApp extends App {
-  implicit val system = ActorSystem("sample1")
-  implicit val executionContext = system.dispatcher
-  implicit val materializer = ActorMaterializer()
+  implicit val system: ActorSystem = ActorSystem("sample1")
+  implicit val executionContext: ExecutionContext = system.dispatcher
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   val route = get {
     complete("sample")
