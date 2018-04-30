d64d29c10c5f48a3935fa77cd869d14fc6219b3c
fits <wadays_wozx@nifty.com>
2018-04-30 23:57:15 +0900

    probe の結果を IO.fromTry で IO 化するサンプル

---
+++ scala/quill/prob_sample2/src/main/scala/sample/app/SampleApp.scala
@@ -0,0 +1,20 @@
+package sample.app
+
+import io.getquill.{H2JdbcContext, UpperCase}
+
+case class Data(id: String, value: Int)
+
+object SampleApp extends App {
+  lazy val ctx = new H2JdbcContext(UpperCase, "ctx")
+
+  import ctx._
+
+  val p = for {
+    r1 <- IO.fromTry(probe("CREATE TABLE DATA(id VARCHAR(10), value int)"))
+    r2 <- runIO( quote(query[Data].insert(lift(Data("id1", 1)))) )
+    r3 <- runIO( quote(query[Data].insert(lift(Data("id2", 2)))) )
+    r4 <- runIO( quote(query[Data]) )
+  } yield (r1, r2, r3, r4)
+
+  println( performIO(p) )
+}
