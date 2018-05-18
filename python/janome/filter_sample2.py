9be199eb10df096934be415f6562dc1a4bd7fa64
fits <wadays_wozx@nifty.com>
2018-05-18 20:34:00 +0900

    名詞か形容詞を抽出するサンプル

---
+++ python/janome/filter_sample2.py
@@ -0,0 +1,12 @@
+import sys
+from janome.analyzer import Analyzer
+from janome.tokenfilter import *
+
+msg = sys.argv[1]
+
+filters = [POSKeepFilter(['名詞', '形容詞'])]
+
+analyzer = Analyzer(token_filters = filters)
+
+for t in analyzer.analyze(msg):
+    print(f'phonetic = {t.phonetic}, reading = {t.reading}, surface = {t.surface}, part_of_speech = {t.part_of_speech}, base_form = {t.base_form}, infl_form = {t.infl_form}, infl_type = {t.infl_type}, node_type = {t.node_type}')
