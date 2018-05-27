fd04a3eeed9355f7697f7fe37d6be76870a19dea
fits <wadays_wozx@nifty.com>
2018-05-27 13:50:46 +0900

    Word2Vec のベクトルを使った Encoder-Decoder 処理

---
+++ python/keras/encoder-decoder/generate_sentence/word2vec-seq_test.py
@@ -0,0 +1,60 @@
+import sys
+import numpy as np
+from gensim.models import word2vec
+from keras.models import load_model
+
+model_file_prefix = sys.argv[1]
+wv_file = sys.argv[2]
+questions = [q.split(' ') for q in sys.argv[3].split(';')]
+
+BOS = '\t'
+EOS = '\n'
+topn_similar = 10
+prob_weight = 20
+predict_max_size = 100
+
+encode_model = load_model(f'{model_file_prefix}_encode.h5')
+decode_model = load_model(f'{model_file_prefix}_decode.h5')
+
+q_maxlen = encode_model.input_shape[1]
+
+wv_model = word2vec.Word2Vec.load(wv_file)
+
+padding_wv_array = lambda d, size: np.array([np.vstack((
+    wv_model.wv[d],
+    np.zeros((size - len(d), wv_model.vector_size))
+))])
+
+def word_choice(v):
+    cd = wv_model.wv.similar_by_vector(v, topn = topn_similar)
+
+    words = [c for c, _ in cd]
+
+    if EOS in words:
+        return EOS
+
+    probs = np.exp(np.array([p for _, p in cd]) * prob_weight)
+    probs /= probs.sum()
+
+    return np.random.choice(words, p = probs)
+
+def predict(q):
+    state = encode_model.predict(padding_wv_array(q, q_maxlen))
+    ch = BOS
+
+    for _ in range(predict_max_size):
+        r, h = decode_model.predict([padding_wv_array([ch], 1), state])
+
+        ch = word_choice(r[0][0])
+
+        if ch == EOS:
+            break
+
+        yield ch
+        state = h
+
+answer = lambda q: ''.join(predict(q))
+
+for q in questions:
+    a = answer(q)
+    print(f'{q} = {a}')
---
+++ python/keras/encoder-decoder/generate_sentence/word2vec-seq_train.py
@@ -0,0 +1,76 @@
+import sys
+import numpy as np
+import pandas as pd
+from gensim.models import word2vec
+from keras.models import Model
+from keras.layers import Input, Dense, GRU, Flatten
+
+data_file = sys.argv[1]
+dest_file_prefix = sys.argv[2]
+epoch = int(sys.argv[3])
+batch = int(sys.argv[4])
+n_hidden = int(sys.argv[5])
+
+wv_size = int(sys.argv[6])
+wv_iter = int(sys.argv[7])
+
+BOS = '\t'
+EOS = '\n'
+
+df = pd.read_csv(data_file, delimiter = '\t', 
+                    names = ('keyword', 'sentence'), dtype = 'object')
+
+keywords = [k.split(' ') for k in df['keyword'].values]
+sentences = [[BOS] + s.split(' ') + [EOS] for s in df['sentence'].values]
+
+wv_model = word2vec.Word2Vec(sentences, wv_size, min_count = 1, iter = wv_iter)
+wv_model.save(f'{data_file}.wv')
+
+q_maxlen = np.max([len(q) for q in keywords])
+a_maxlen = np.max([len(a) for a in sentences])
+
+print(f'question max size: {q_maxlen}, answer max size: {a_maxlen}')
+
+padding_wv = lambda d, size: np.vstack((
+    wv_model.wv[d],
+    np.zeros((size - len(d), wv_model.vector_size))
+))
+
+x1 = np.array([padding_wv(q, q_maxlen) for q in keywords])
+x2 = np.array([padding_wv(a, a_maxlen) for a in sentences])
+y = np.array([np.vstack((d[1:], np.zeros((1, wv_model.vector_size)))) for d in x2])
+
+enc_inputs = Input(batch_shape = (None, q_maxlen, wv_model.vector_size))
+enc_outputs = Dense(n_hidden, activation = 'relu')(
+    Flatten()(
+        Dense(n_hidden, activation = 'relu')(enc_inputs)
+    )
+)
+
+enc_states = [enc_outputs]
+
+decoder = GRU(n_hidden, return_sequences = True, return_state = True)
+dec_inputs = Input(shape = (None, wv_model.vector_size))
+dec_outputs, _ = decoder(dec_inputs, initial_state = enc_states)
+
+decoder_dense = Dense(wv_model.vector_size, activation = 'softmax')
+dec_outputs = decoder_dense(dec_outputs)
+
+model = Model([enc_inputs, dec_inputs], dec_outputs)
+
+model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mae', 'acc'])
+
+model.fit([x1, x2], y, epochs = epoch, batch_size = batch)
+
+encode_model = Model(enc_inputs, enc_states)
+
+dec_p_input = Input(shape = (n_hidden,))
+
+dec_p_outputs, dec_p_state = decoder(dec_inputs, initial_state = [dec_p_input])
+
+dec_p_outputs = decoder_dense(dec_p_outputs)
+
+decode_model = Model([dec_inputs, dec_p_input], [dec_p_outputs, dec_p_state])
+
+encode_model.save(f'{dest_file_prefix}_encode.h5')
+decode_model.save(f'{dest_file_prefix}_decode.h5')
