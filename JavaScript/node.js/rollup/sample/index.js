d29b7c44236115c590247d1fc588353bbc90c50d
fits <wadays_wozx@nifty.com>
2018-07-17 21:04:10 +0900

    Rollup で単一ファイル化するサンプル

---
+++ JavaScript/node.js/rollup/sample/index.js
@@ -0,0 +1,16 @@
+
+import * as R from 'ramda'
+
+const data = [
+    {category: 'A', item: 'A01', week: '2018-01', num: 1},
+    {category: 'A', item: 'A02', week: '2018-02', num: 1},
+    {category: 'C', item: 'C01', week: '2018-04', num: 1},
+    {category: 'C', item: 'C01', week: '2018-04', num: 10},
+    {category: 'A', item: 'A01', week: '2018-01', num: 1},
+    {category: 'A', item: 'A03', week: '2018-03', num: 2},
+    {category: 'A', item: 'A03', week: '2018-03', num: 5},
+    {category: 'B', item: 'B01', week: '2018-01', num: 1}
+]
+
+console.log( R.groupBy(R.prop('category'), data) )
+console.log( R.groupBy(R.prop('week'), data) )
---
+++ JavaScript/node.js/rollup/sample/rollup.config.js
@@ -0,0 +1,15 @@
+
+import resolve from 'rollup-plugin-node-resolve'
+
+export default {
+    input: './index.js',
+
+    output: {
+        file: 'bundle.js',
+        format: 'umd'
+    },
+
+    plugins: [
+        resolve()
+    ]
+}
