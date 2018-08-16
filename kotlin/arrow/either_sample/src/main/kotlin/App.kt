9a4d888ef075d09783fdb587e12a48703a24c012
fits <wadays_wozx@nifty.com>
2018-08-17 02:09:34 +0900

    arrow-kt の Either を使ったサンプル

---
+++ kotlin/arrow/either_sample/src/main/kotlin/App.kt
@@ -0,0 +1,32 @@
+
+import arrow.core.*
+import arrow.instances.*
+import arrow.typeclasses.binding
+
+fun main(args: Array<String>) {
+    val d1: Either<String, Int> = Either.right(10)
+    val d2: Either<String, Int> = Either.right(5)
+    val d3: Either<String, Int> = Either.left("error")
+
+    val r1 = d1.flatMap { a ->
+        d2.map { b -> a + b}
+    }
+
+    println(r1)
+
+    val r2 = d1.flatMap { a ->
+        d3.map { b -> a + b }
+    }
+
+    println(r2)
+
+    val r3 = ForEither<Int>() extensions {
+        binding {
+            val a = Either.right(10).bind()
+            val b = Either.right(5).bind()
+            a + b
+        }
+    }
+
+    println(r3)
+}
