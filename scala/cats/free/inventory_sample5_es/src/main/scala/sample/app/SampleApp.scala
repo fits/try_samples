162d93a0abbc6bf899e9821e071ecf09b9e1e80c
fits <wadays_wozx@nifty.com>
2018-08-24 14:24:26 +0900

    Event をメモリ上で保持する処理を追加

--- scala/cats/free/inventory_sample5_es/src/main/scala/sample/app/SampleApp.scala
+++ scala/cats/free/inventory_sample5_es/src/main/scala/sample/app/SampleApp.scala
@@ -1,6 +1,6 @@
 package sample.app
 
-import sample.service.{InventoryInterpreter, InventoryService}
+import sample.service.{EventStoreInterpreter, InventoryInterpreter, InventoryService}
 
 object SampleApp extends App {
   val proc1 = for {
@@ -15,4 +15,26 @@ object SampleApp extends App {
   val r = InventoryInterpreter.interpret(proc1)
 
   println( r.run(Map.empty).value )
+
+  val er1 = EventStoreInterpreter.interpret(proc1)
+
+  println(er1.value)
+  println(EventStoreInterpreter.restore("id-1"))
+  println(EventStoreInterpreter.restore("id-2"))
+
+  val proc2 = for {
+    _ <- InventoryService.remove("id-1", 2)
+    _ <- InventoryService.remove("id-1", 3)
+  } yield ()
+
+  val er2 = EventStoreInterpreter.interpret(proc2)
+
+  println(er2.value)
+  println(EventStoreInterpreter.restore("id-1"))
+
+  val er3 = EventStoreInterpreter.interpret(InventoryService.move("id-1", "id-2", 3))
+
+  println(er3.value)
+  println(EventStoreInterpreter.restore("id-1"))
+  println(EventStoreInterpreter.restore("id-2"))
 }
--- scala/cats/free/inventory_sample5_es/src/main/scala/sample/service/InventoryService.scala
+++ scala/cats/free/inventory_sample5_es/src/main/scala/sample/service/InventoryService.scala
@@ -1,11 +1,14 @@
 package sample.service
 
-import cats.data.State
+import cats.data.{EitherT, State}
 import cats.free.Free
-import cats.~>
+import cats.{Id, ~>}
 import sample.es._
 import sample.model.InventoryItem
 import sample.model.common.{Amount, ItemId}
+
+import scala.collection.mutable
+import scala.concurrent.Future
 
 case class Created(id: ItemId) extends Event[ItemId]
 case class Added(id: ItemId, quantity: Amount) extends Event[Unit]
@@ -44,4 +47,55 @@ object InventoryInterpreter {
     case Added(id, qt) => state + (id -> state(id).copy(quantity = state(id).quantity + qt))
     case Removed(id, qt) => state + (id -> state(id).copy(quantity = state(id).quantity - qt))
   }
+}
+
+object EventStoreInterpreter {
+  type EitherErr[A] = EitherT[Id, Throwable, A]
+
+  private def left[A] = EitherT.leftT[Id, A]
+  private def right = EitherT.rightT[Id, Throwable]
+
+  private val eventStore = new {
+    private lazy val store = mutable.Map.empty[ItemId, List[Event[_]]]
+
+    def get(key: ItemId): EitherErr[List[Event[_]]] =
+      right(store.getOrElse(key, List.empty[Event[_]]))
+
+    def add(key: ItemId, event: Event[_]): EitherErr[Event[_]] =
+      get(key).map { es =>
+        store.put(key, es :+ event)
+        event
+      }
+  }
+
+  val step = new (Event ~> EitherErr) {
+    override def apply[A](fa: Event[A]): EitherErr[A] = fa match {
+      case c @ Created(id) => validateCreate(id).flatMap(_ => eventStore.add(id, c)).map(_ => id)
+      case a @ Added(id, _) => validateUpdate(id).flatMap(_ => eventStore.add(id, a)).map(_ => ())
+      case r @ Removed(id, qty) => validateQuantity(id, qty).flatMap(_ => eventStore.add(id, r)).map(_ => ())
+    }
+  }
+
+  def interpret[A](c: Command[A]): EitherErr[A] = c.foldMap(step)
+
+  def restore(id: ItemId): EitherErr[InventoryItem] =
+    eventStore.get(id).map(_.foldLeft(InventoryItem("")) { (acc, ev) =>
+      ev match {
+        case Created(id) => InventoryItem(id)
+        case Added(_, qty) => acc.copy(quantity = acc.quantity + qty)
+        case Removed(_, qty) => acc.copy(quantity = acc.quantity - qty)
+      }
+    })
+
+  private def validateCreate(id: ItemId): EitherErr[ItemId] =
+    eventStore.get(id).flatMap(es =>
+      if (es.isEmpty) right(id) else left(new Exception("not empty")))
+
+  private def validateUpdate(id: ItemId): EitherErr[ItemId] =
+    eventStore.get(id).flatMap(es =>
+      if (es.isEmpty) left(new Exception("is empty")) else right(id))
+
+  private def validateQuantity(id: ItemId, qty: Amount): EitherErr[InventoryItem] =
+    restore(id).flatMap(inv =>
+      if (inv.quantity < qty) left(new Exception(s"quantity < ${qty}")) else right(inv))
 }
