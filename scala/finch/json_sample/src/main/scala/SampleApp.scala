ad881aedbc2c918dfb60aa3e1126bea25b8b27bb
fits <wadays_wozx@nifty.com>
2018-09-18 00:30:24 +0900

    ライブラリのバージョンアップ

--- scala/finch/json_sample/src/main/scala/SampleApp.scala
+++ scala/finch/json_sample/src/main/scala/SampleApp.scala
@@ -1,5 +1,6 @@
 
 import io.finch._
+import io.finch.syntax._
 import io.finch.circe._
 import io.circe.generic.auto._
 
@@ -10,16 +11,16 @@ case class Data(id: String, value: Int)
 
 object SampleApp extends App {
 
-  val dataGet = get("samples" :: string) { id: String => Ok(Data(id, 1)) }
+  val dataGet = get("samples" :: path[String]) { id: String => Ok(Data(id, 1)) }
 
   val dataPost = post("samples" :: jsonBody[Data]) { d: Data => 
-    println(s"data = ${d}")
+    println(s"data = $d")
     Ok(d)
   }
 
-  val api = dataGet :+: dataPost
+  val api = (dataGet :+: dataPost).toServiceAs[Application.Json]
 
-  val server = Http.server.serve(":8080", api.toServiceAs[Application.Json])
+  val server = Http.server.serve(":8080", api)
 
   Await.ready(server)
 }
