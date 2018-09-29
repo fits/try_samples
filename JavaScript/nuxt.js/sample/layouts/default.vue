6f28793bb5dea7b615044ef30070907710210080
fits <wadays_wozx@nifty.com>
2018-09-29 16:56:59 +0900

    Nuxt.js のページ切り替えサンプル

---
+++ JavaScript/nuxt.js/sample/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/item">item</router-link>
+    </p>
+
+    <nuxt />
+  </div>
+</template>
