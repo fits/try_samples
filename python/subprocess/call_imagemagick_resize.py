2d265d180d4e9fb8c6ad2da3cb8595ee2186152c
fits <wadays_wozx@nifty.com>
2018-11-25 14:52:00 +0900

    ImageMagick を使ったリサイズ処理

---
+++ python/subprocess/call_imagemagick_resize.py
@@ -0,0 +1,20 @@
+
+import sys
+import os
+import glob
+import subprocess
+
+size = sys.argv[1]
+targets = sys.argv[2]
+dest_dir = sys.argv[3]
+
+for f in glob.glob(targets):
+    file, ext = os.path.splitext(os.path.basename(f))
+
+    dest_file = f"{dest_dir}/{file}_{size}{ext}"
+
+    cmd = f"convert {f} -resize {size}x {dest_file}"
+
+    ret = subprocess.call(cmd, shell = True)
+
+    print(f"done {f}: {ret}")
