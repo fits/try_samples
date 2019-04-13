7b1465eb2c605c5ccc2b2c1d8959dd56ac5f624c
fits <wadays_wozx@nifty.com>
2019-04-14 01:58:20 +0900

    Compiler API を使ってソースコードをパースするサンプル

---
+++ csharp/CompilerAPI/sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string src = @"
+                using System;
+
+                class Sample
+                {
+                    static void Main(string[] args)
+                    {
+                        Console.WriteLine(""sample"");
+                    }
+                }
+            ";
+
+            var tree = CSharpSyntaxTree.ParseText(src);
+
+            Console.WriteLine(tree);
+        }
+    }
+}
