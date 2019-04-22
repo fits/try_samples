00adc0616f7a9dab309173c76996f330be4e3666
fits <wadays_wozx@nifty.com>
2019-04-22 22:10:23 +0900

    blog sample: SonarAnalyzer.CSharp でサイクロマティック複雑度を算出

---
+++ blog/20190422/sonar_sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SonarAnalyzer.Metrics.CSharp;
+
+namespace CyclomaticComplexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(args[0]))
+            {
+                var tree = CSharpSyntaxTree.ParseText(reader.ReadToEnd());
+                var root = tree.GetCompilationUnitRoot();
+
+                var methods = root.DescendantNodes()
+                                    .OfType<MethodDeclarationSyntax>();
+
+                foreach(var m in methods)
+                {
+                    var c = CSharpCyclomaticComplexityMetric.GetComplexity(m);
+
+                    Console.WriteLine("{0},{1}", m.Identifier, c.Complexity);
+                }
+            }
+        }
+    }
+}
