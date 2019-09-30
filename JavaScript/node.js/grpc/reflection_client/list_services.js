953d13508b23cd94162d6844d1525e3656747c87
fits <wadays_wozx@nifty.com>
2019-10-01 00:17:54 +0900

    gRPC reflection を使ってサービス名の一覧を取得するサンプル

---
+++ JavaScript/node.js/grpc/reflection_client/list_services.js
@@ -0,0 +1,33 @@
+
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+const descriptor = require('protobufjs/ext/descriptor')
+
+const pd = protoLoader.loadSync('reflection.proto', {
+    keepCase: true,
+    defaults: true
+})
+
+const proto = grpc.loadPackageDefinition(pd).grpc.reflection.v1alpha
+
+const client = new proto.ServerReflection(
+    '127.0.0.1:50051', 
+    grpc.credentials.createInsecure()
+)
+
+const call = client.ServerReflectionInfo()
+
+call.on('error', err => {
+    call.end()
+    console.error(err)
+})
+
+call.on('data', res => {
+    call.end()
+    
+    res.list_services_response.service.forEach(s => {
+        console.log(s.name)
+    })
+})
+
+call.write({host: 'localhost', list_services: ''})
