63975817c438b78b66ab533fb213dfa15f5a7cce
fits <wadays_wozx@nifty.com>
2019-10-27 14:00:01 +0900

    型判定などのサンプル

---
+++ go/sum-types_sample.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Status interface {
+	IsDone() bool
+}
+
+type Ready struct {
+	note string
+}
+
+type InProgress struct {}
+
+type Completed struct {}
+
+type Canceled struct {
+	reason string
+}
+
+func (Ready) IsDone() bool {
+	return false
+}
+
+func (InProgress) IsDone() bool {
+	return false
+}
+
+func (Completed) IsDone() bool {
+	return true
+}
+
+func (Canceled) IsDone() bool {
+	return true
+}
+
+func showStatus(s Status) string {
+	switch s.(type) {
+		case Ready:
+			return "ready(" + s.(Ready).note + ")"
+		case InProgress:
+			return "inprogress"
+		case Completed:
+			return "completed"
+		case Canceled:
+			return "canceled(" + s.(Canceled).reason + ")"
+		default:
+			return "invalid status"
+	}
+}
+
+func printStatus(s Status) {
+	fmt.Println("staus: ", s, " = ", showStatus(s), ", done: ", s.IsDone())
+}
+
+func main() {
+	printStatus(Ready{"sample"})
+	printStatus(InProgress{})
+	printStatus(Completed{})
+	printStatus(Canceled{"test"})
+}
