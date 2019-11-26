031e0ac6a00c6bc59f99a8bdd71bf32b2c7c5e5b
fits <wadays_wozx@nifty.com>
2019-11-26 21:31:29 +0900

    Elasticsearch 7.4.2 に合わせて変更

--- groovy/elasticsearch/embed/es_embed_netty.groovy
+++ groovy/elasticsearch/embed/es_embed_netty.groovy
@@ -1,24 +1,25 @@
-@Grab('org.elasticsearch:elasticsearch:5.2.0')
-@Grab('org.elasticsearch.plugin:transport-netty4-client:5.2.0')
-@Grab('org.apache.logging.log4j:log4j-api:2.8')
-@Grab('org.apache.logging.log4j:log4j-core:2.8')
+@Grab('org.elasticsearch:elasticsearch:7.4.2')
+@Grab('org.elasticsearch.plugin:transport-netty4-client:7.4.2')
+@Grab('org.apache.logging.log4j:log4j-api:2.12.1')
+@Grab('org.apache.logging.log4j:log4j-core:2.12.1')
 import org.elasticsearch.env.Environment
 import org.elasticsearch.node.Node
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.transport.Netty4Plugin
 
 def setting = Settings.builder()
-	.put('path.home', '.')
-	.build()
+    .put(Environment.PATH_HOME_SETTING.getKey(), '.')
+    .build()
 
-def env = new Environment(setting)
+def env = new Environment(setting, null)
 
-def node = new Node(env, [Netty4Plugin])
+new Node(env, [Netty4Plugin], true).withCloseable { node ->
 
-println node.getPluginsService().info().getPluginInfos()
+    println node.getPluginsService().info().getPluginInfos()
 
-node.start()
+    node.start()
 
-println 'started server ...'
+    println 'started server ...'
 
-System.in.read()
+    System.in.read()
+}
