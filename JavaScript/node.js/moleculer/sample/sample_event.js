d843faeb4f7a46affda6c7ba59237d1cb68d46a6
fits <wadays_wozx@nifty.com>
2020-02-09 12:45:18 +0900

    イベントでサービスを連携するサンプル

---
+++ JavaScript/node.js/moleculer/sample/sample_event.js
@@ -0,0 +1,44 @@
+
+const { ServiceBroker } = require('moleculer')
+
+const broker = new ServiceBroker({ logger: console })
+
+broker.createService({
+    name: 'sample1',
+    actions: {
+        create(ctx) {
+            console.log(`sample1.command: ${JSON.stringify(ctx.params)}`)
+
+            ctx.emit('sample.created', {
+                revision: 1,
+                value: ctx.params.value
+            })
+        }
+    }
+})
+
+broker.createService({
+    name: 'sample2',
+    events: {
+        'sample.created'(event) {
+            console.log(`sample2: ${JSON.stringify(event)}`)
+        }
+    }
+})
+
+broker.createService({
+    name: 'sample3',
+    events: {
+        'sample.*'(event) {
+            console.log(`sample3: ${JSON.stringify(event)}`)
+        }
+    }
+})
+
+
+const run = async () => {
+    await broker.start()
+    await broker.call('sample1.create', { value: 123 })
+}
+
+run().catch(err => console.error(err))
