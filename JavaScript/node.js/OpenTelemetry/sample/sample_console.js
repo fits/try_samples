0d44021b9a6930a09691ad0ef2a4cde37924c358
fits <wadays_wozx@nifty.com>
2020-03-25 01:01:18 +0900

    OpenTelemetry を使ったトレーシングのサンプル

---
+++ JavaScript/node.js/OpenTelemetry/sample/sample_console.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const OpenTelemetry = require('@opentelemetry/api')
+const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } =
+    require('@opentelemetry/tracing')
+
+const provider = new BasicTracerProvider()
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
+provider.register()
+
+const tracer = OpenTelemetry.trace.getTracer('sample')
+
+const mainSpan = tracer.startSpan('main')
+
+const subSpan1 = tracer.startSpan('sub1', {parent: mainSpan})
+
+subSpan1.setAttribute('value1', 10)
+subSpan1.addEvent('sample-event1')
+
+subSpan1.end()
+
+const subSpan2 = tracer.startSpan('sub2', {parent: mainSpan})
+
+subSpan2.addEvent('sample-event2_1', {version: 1})
+subSpan2.addEvent('sample-event2_2', {version: 2})
+
+subSpan2.end()
+
+mainSpan.end()
