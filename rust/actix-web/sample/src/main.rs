c40ab5ce57f4c872d2018baea9af1e8c3d2d441a
fits <wadays_wozx@nifty.com>
2020-04-07 23:33:32 +0900

    actix-web のバージョンアップ

--- rust/actix-web/sample/src/main.rs
+++ rust/actix-web/sample/src/main.rs
@@ -1,14 +1,28 @@
 
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
 
-fn index(info: web::Path<(u32, String)>) -> impl Responder {
+async fn index(info: web::Path<(u32, String)>) -> impl Responder {
     format!("received: {:?}", info)
 }
 
-fn main() -> std::io::Result<()> {
+#[get("/a/b/{id}")]
+async fn sample1(info: web::Path<(String,)>) -> impl Responder {
+    format!("a: id={}", info.0)
+}
+
+#[get("/b/{id}/{value}/{command}")]
+async fn sample2(info: web::Path<(String, u32, String)>) -> impl Responder {
+    format!("b: id={}, value={}, command={}", info.0, info.1, info.2)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     let address = "127.0.0.1:8080";
 
     HttpServer::new(|| {
-        App::new().service(web::resource("/{id}/{name}").to(index))
-    }).bind(address)?.run()
+        App::new()
+            .service(web::resource("/{id}/{name}").to(index))
+            .service(sample1)
+            .service(sample2)
+    }).bind(address)?.run().await
 }
