0337a59b2a534cc15f756631e2eba7df2d977b26
fits <wadays_wozx@nifty.com>
2020-05-26 03:08:13 +0900

    Node.js 用の実行スクリプト

---
+++ rust/WebAssembly/sample-import/run_wasm.js
@@ -0,0 +1,43 @@
+
+const fs = require('fs')
+
+const wasmFile = process.argv[2]
+
+const run = async () => {
+    const module = await WebAssembly.compile(fs.readFileSync(wasmFile))
+
+    const imports = WebAssembly.Module.imports(module)
+    const exports = WebAssembly.Module.exports(module)
+
+    console.log('--- imports ---')
+    console.log(imports)
+
+    console.log('--- exports ---')
+    console.log(exports)
+
+    console.log('--- call main ---')
+
+    let instance = null
+    let counter = 50
+
+    const importObject = {
+        sample: {
+            count_up: () => counter++,
+            log: (ptr, len) => {
+                if (instance) {
+                    const memory = instance.exports.memory.buffer
+
+                    const buf = new Uint8Array(memory, ptr, len)
+                    const msg = String.fromCharCode(...buf)
+                    
+                    console.log(msg)
+                }
+            }
+        }
+    }
+
+    instance = await WebAssembly.instantiate(module, importObject)
+    instance.exports.main()
+}
+
+run().catch(err => console.error(err))
