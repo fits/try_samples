32c3143f19cd4455f537b963f448787334cd7866
fits <wadays_wozx@nifty.com>
2020-07-17 20:54:54 +0900

    Juniper を使った GraphQL のサンプル

---
+++ rust/juniper/sample/src/main.rs
@@ -0,0 +1,126 @@
+
+use juniper::{execute, FieldError, FieldResult, Variables};
+use uuid::Uuid;
+
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+
+#[derive(Default)]
+struct Store {
+    list: Arc<RwLock<HashMap<String, Item>>>,
+}
+
+#[derive(Debug, Clone, juniper::GraphQLEnum)]
+enum Category {
+    Standard,
+    Extra,
+}
+
+#[derive(Debug, Clone, juniper::GraphQLObject)]
+struct Item {
+    id: String,
+    category: Category,
+    value: i32,
+}
+
+#[derive(Debug, Clone, juniper::GraphQLInputObject)]
+struct CreateItem {
+    category: Category,
+    value: i32,
+}
+
+impl juniper::Context for Store {}
+
+fn error(msg: &str) -> FieldError {
+    FieldError::new(msg, juniper::Value::Null)
+}
+
+#[derive(Debug)]
+struct Query;
+
+#[juniper::object(Context = Store)]
+impl Query {
+    fn find(ctx: &Store, id: String) -> FieldResult<Item> {
+        let s = ctx.list
+            .read()
+            .map_err(|e| 
+                error(format!("read lock: {:?}", e).as_str())
+            )?;
+
+        s.get(&id).cloned().ok_or(error("not found"))
+    }
+}
+
+#[derive(Debug)]
+struct Mutation;
+
+#[juniper::object(Context = Store)]
+impl Mutation {
+    fn create(ctx: &Store, input: CreateItem) -> FieldResult<Item> {
+        let data = Item {
+            id: format!("data-{}", Uuid::new_v4()),
+            category: input.category,
+            value: input.value,
+        };
+
+        let mut s = ctx.list
+            .write()
+            .map_err(|e|
+                error(format!("write lock: {:?}", e).as_str())
+            )?;
+
+        if s.insert(data.id.clone(), data.clone()).is_none() {
+            Ok(data)
+        } else {
+            Err(error("duplicated id"))
+        }
+    }
+}
+
+type Schema = juniper::RootNode<'static, Query, Mutation>;
+
+fn main() {
+    let ctx = Store::default();
+    let schema = Schema::new(Query, Mutation);
+
+    let m = r#"
+        mutation {
+            create(input: { category: STANDARD, value: 12 }) {
+                id
+            }
+        }
+    "#;
+
+    let (r1, _) = execute(m, None, &schema, &Variables::new(), &ctx).unwrap();
+    println!("*** r1 = {:?}", r1);
+
+    let id = r1
+        .as_object_value()
+        .and_then(|v| v.get_field_value("create"))
+        .and_then(|v| v.as_object_value())
+        .and_then(|v| v.get_field_value("id"))
+        .and_then(|v| v.as_scalar())
+        .unwrap();
+
+    println!("*** id = {:?}", id);
+
+    let q = r#"
+        query FindItem($p: String!) {
+            find(id: $p) {
+                id
+                category
+                value
+            }
+        }
+    "#;
+
+    let mut vars = Variables::new();
+
+    vars.insert(
+        "p".to_string(), 
+        juniper::InputValue::scalar(id.clone())
+    );
+
+    let (r2, _) = execute(q, None, &schema, &vars, &ctx).unwrap();
+    println!("*** r2 = {:?}", r2);
+}
