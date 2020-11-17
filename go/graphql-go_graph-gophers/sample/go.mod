639a9b7d52cd74342508ab3d4c2435f24e307370
fits <wadays_wozx@nifty.com>
2020-11-18 02:01:29 +0900

    GraphQL のサンプル

---
+++ go/graphql-go_graph-gophers/sample/go.mod
@@ -0,0 +1,8 @@
+module sample
+
+go 1.15
+
+require (
+	github.com/google/uuid v1.1.2
+	github.com/graph-gophers/graphql-go v0.0.0-20201113091052-beb923fada29
+)
---
+++ go/graphql-go_graph-gophers/sample/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/google/uuid"
+	graphql "github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+)
+
+var store = []Item{}
+var mu sync.Mutex
+
+func pushItem(item Item) {
+	mu.Lock()
+	store = append(store, item)
+	mu.Unlock()
+}
+
+func findItem(id graphql.ID) *Item {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for _, r := range store {
+		if r.ID() == id {
+			return &r
+		}
+	}
+
+	return nil
+}
+
+type Item struct {
+	id graphql.ID
+	value int32
+}
+
+func (r *Item) ID() graphql.ID {
+	return r.id
+}
+
+func (r *Item) Value() int32 {
+	return r.value
+}
+
+type CreateItem struct {
+	Value int32
+}
+
+type resolver struct {}
+
+func (_ *resolver) Create(args struct { Input CreateItem }) (*Item, error) {
+	log.Printf("call create: %#v", args)
+
+	id, err := uuid.NewRandom()
+
+	if err != nil {
+		return nil, err
+	}
+
+	item := Item{graphql.ID(id.String()), args.Input.Value}
+
+	go pushItem(item)
+
+	return &item, nil
+}
+
+func (_ *resolver) Find(args struct { ID graphql.ID }) *Item {
+	log.Printf("call find: %#v", args)
+	return findItem(args.ID)
+}
+
+func main() {
+	s := `
+		input CreateItem {
+			value: Int!
+		}
+
+		type Item {
+			id: ID!
+			value: Int!
+		}
+
+		type Mutation {
+			create(input: CreateItem!): Item
+		}
+
+		type Query {
+			find(id: ID!): Item
+		}
+	`
+	schema := graphql.MustParseSchema(s, &resolver{})
+
+	http.Handle("/graphql", &relay.Handler{Schema: schema})
+
+	err := http.ListenAndServe(":8080", nil)
+
+	log.Fatal(err)
+}
