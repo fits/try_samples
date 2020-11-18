bcfb6e56a70d5d552140b51c4bb57e0cf1b00c7c
fits <wadays_wozx@nifty.com>
2020-11-19 08:13:58 +0900

    Mutex による排他ロック

---
+++ go/sync/mutex_sample.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Data struct {
+	value int
+}
+
+func noLock() {
+	var wg sync.WaitGroup
+
+	var ds []Data
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func() {
+			ds = append(ds, Data{i})
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("noLock length =", len(ds))
+}
+
+func useMutex() {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	var ds []Data
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func() {
+			mu.Lock()
+			ds = append(ds, Data{i})
+			mu.Unlock()
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("useMutex length =", len(ds))
+}
+
+func main() {
+	noLock()
+
+	println("-----")
+
+	useMutex()
+}
