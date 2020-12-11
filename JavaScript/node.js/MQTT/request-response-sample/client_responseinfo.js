984fdb7b88b1f50304fb1f0298464dbc72808fbf
fits <wadays_wozx@nifty.com>
2020-12-11 23:36:45 +0900

    ResponseInformation の利用

---
+++ JavaScript/node.js/MQTT/request-response-sample/client_responseinfo.js
@@ -0,0 +1,43 @@
+
+const mqtt = require('mqtt')
+
+const uri = 'mqtt://localhost'
+
+const qos = 1
+const publishTopic = process.argv[2]
+const publishMessage = process.argv[3]
+
+const client = mqtt.connect(uri, { 
+    protocolVersion: 5,
+    properties: {
+        requestResponseInformation: true
+    }
+})
+
+client.on('error', err => {
+    console.error(err)
+    client.end()
+})
+
+client.on('connect', connack => {
+    console.log(`*** connected: connack=${JSON.stringify(connack)}`)
+
+    const responseInfo = connack.properties.responseInformation
+
+    const responseTopic = responseInfo ?
+        `${publishTopic}/${responseInfo}` : 
+        `${publishTopic}/response/${client.options.clientId}`
+
+    client.subscribe(responseTopic, { qos })
+
+    client.publish(publishTopic, publishMessage, { qos, properties: { responseTopic } })
+})
+
+client.on('message', (topic, message, packet) => {
+    console.log(`*** received: topic=${topic}, message=${message}, packet=${JSON.stringify(packet)}`)
+    client.end()
+})
+
+client.on('close', () => {
+    console.log('*** closed')
+})
