78e03f2e995c36fb26c0fccd3ab9794ba71c496c
fits <wadays_wozx@nifty.com>
2021-01-19 02:43:59 +0900

    GraphQL union type

---
+++ go/graphql-go_graph-gophers/graphql_interface2/sample1_union.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+
+	graphql "github.com/graph-gophers/graphql-go"
+)
+
+const (
+	schemaString = `
+		union Event = Created | Deleted
+
+		type Created {
+			id: ID!
+			title: String!
+		}
+
+		type Deleted {
+			id: ID!
+			reason: String
+		}
+
+		type Query {
+			events: [Event!]!
+		}
+	`
+)
+
+type Event interface{}
+
+type Created struct {
+	id    string
+	title string
+}
+
+func (c *Created) ID() graphql.ID {
+	return graphql.ID(c.id)
+}
+
+func (c *Created) Title() string {
+	return c.title
+}
+
+type Deleted struct {
+	id     string
+	reason string
+}
+
+func (d *Deleted) ID() graphql.ID {
+	return graphql.ID(d.id)
+}
+
+func (d *Deleted) Reason() *string {
+	if d.reason == "" {
+		return nil
+	}
+
+	return &d.reason
+}
+
+type eventResolver struct {
+	Event
+}
+
+func (r *eventResolver) ToCreated() (*Created, bool) {
+	c, ok := r.Event.(*Created)
+	return c, ok
+}
+
+func (r *eventResolver) ToDeleted() (*Deleted, bool) {
+	d, ok := r.Event.(*Deleted)
+	return d, ok
+}
+
+type resolver struct{}
+
+func (r *resolver) Events() []*eventResolver {
+	return []*eventResolver{
+		{&Created{id: "i-1", title: "sample1"}},
+		{&Deleted{id: "i-1"}},
+		{&Created{id: "i-2", title: "sample2"}},
+		{&Created{id: "i-3", title: "sample3"}},
+		{&Deleted{id: "i-3", reason: "test"}},
+	}
+}
+
+func main() {
+	schema := graphql.MustParseSchema(schemaString, new(resolver))
+
+	q := `
+		{
+			events {
+				__typename
+				... on Created {
+					id
+					title
+				}
+				... on Deleted {
+					id
+					reason
+				}
+			}
+		}
+	`
+
+	r := schema.Exec(context.Background(), q, "", nil)
+	b, err := json.Marshal(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(b))
+}
