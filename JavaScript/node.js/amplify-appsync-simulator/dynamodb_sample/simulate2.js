c0d868f50cf91b8c0115694f3064cc40a8a4f2ab
fits <wadays_wozx@nifty.com>
2021-03-10 02:27:48 +0900

    None DataSource

---
+++ JavaScript/node.js/amplify-appsync-simulator/dynamodb_sample/simulate2.js
@@ -0,0 +1,129 @@
+
+const { AmplifyAppSyncSimulator } = require('amplify-appsync-simulator')
+const { graphql } = require('graphql')
+
+const schema = `
+    type Item {
+        id: ID!
+        value: Int!
+    }
+
+    input FindInput {
+        id: ID!
+    }
+
+    input CreateInput {
+        id: ID!
+        value: Int!
+    }
+
+    type Query {
+        find(input: FindInput!): Item
+    }
+
+    type Mutation {
+        add(input: CreateInput!): Item
+    }
+`
+
+const resolvers = [
+    {
+        kind: 'UNIT', typeName: 'Query', fieldName: 'find',
+        requestMappingTemplate: `
+            {
+                "version": "2018-05-29",
+                "operation": "GetItem",
+                "key": {
+                    "id": $util.dynamodb.toDynamoDBJson($ctx.args.input.id)
+                },
+                "consistentRead": true
+            }
+        `, 
+        responseMappingTemplate: '$utils.toJson($context.result)', 
+        dataSourceName: 'ItemTable'
+    },
+    {
+        kind: 'UNIT', typeName: 'Mutation', fieldName: 'add',
+        requestMappingTemplate: `
+            {
+                "version": "2018-05-29",
+                "operation": "PutItem",
+                "key": {
+                    "id": $util.dynamodb.toDynamoDBJson($ctx.args.input.id)
+                },
+                "attributeValues": {
+                    "value": $util.dynamodb.toDynamoDBJson($ctx.args.input.value)
+                }
+            }
+        `, 
+        responseMappingTemplate: '$utils.toJson($context.result)', 
+        dataSourceName: 'ItemTable'
+    },
+]
+
+const dataSources = [
+    { type: 'NONE', name: 'ItemTable' }
+]
+
+const config = {
+    schema: { content: schema },
+    resolvers,
+    dataSources,
+    appSync: {
+        defaultAuthenticationType: {
+            authenticationType: 'API_KEY'
+        }
+    }
+}
+
+const simulator = new AmplifyAppSyncSimulator()
+
+simulator.init(config)
+
+const items = {}
+
+simulator.getDataLoader('ItemTable').load = (req) => {
+    console.log(`*** dataSource load`)
+    console.log(req)
+
+    const id = req.key.id.S
+
+    if (req.operation == 'PutItem') {
+        items[id] = { id, value: parseInt(req.attributeValues.value.N) }
+    }
+
+    return items[id]
+}
+
+const ctx = {
+    requestAuthorizationMode: 'API_KEY',
+    appsyncErrors: []
+}
+
+const m = `
+    mutation CreateItem($id: ID!, $value: Int!) {
+        add(input: {id: $id, value: $value}) {
+            id
+            value
+        }
+    }
+`
+
+const q = `
+    query FindItem($id: ID!) {
+        find(input: {id: $id}) {
+            id
+            value
+        }
+    }
+`
+
+const run = async () => {
+    const r1 = await graphql(simulator.schema, m, null, ctx, {id: 'id-1', value: 123})
+    console.log(JSON.stringify(r1))
+
+    const r2 = await graphql(simulator.schema, q, null, ctx, {id: 'id-1'})
+    console.log(JSON.stringify(r2))
+}
+
+run().catch(err => console.error(err))
