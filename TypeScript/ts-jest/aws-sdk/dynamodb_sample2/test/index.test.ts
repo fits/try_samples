76ab85b028fdd13890336d5f8b30fe78f72d9a0f
fits <wadays_wozx@nifty.com>
2021-04-21 00:15:05 +0900

    モックによる処理を追加

--- TypeScript/ts-jest/aws-sdk/dynamodb_sample2/test/index.test.ts
+++ TypeScript/ts-jest/aws-sdk/dynamodb_sample2/test/index.test.ts
@@ -1,10 +1,14 @@
 
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb'
 import { handler, SampleInput } from '../src'
 
 jest.mock('@aws-sdk/client-dynamodb')
 
-const DynamoDBClientMock = DynamoDBClient as jest.MockedClass<typeof DynamoDBClient>
+const DynamoDBClientMock = 
+    DynamoDBClient as jest.MockedClass<typeof DynamoDBClient>
+
+const PutItemCommandMock = 
+    PutItemCommand as jest.MockedClass<typeof PutItemCommand>
 
 beforeEach(() => {
     DynamoDBClientMock.mockClear()
@@ -21,7 +25,40 @@ test('call handler', async () => {
     expect(DynamoDBClientMock.mock.instances).toHaveLength(1)
 })
 
-test('call handler with dynamodb error', async () => {
+test('call handler2', async () => {
+    const ev: SampleInput = { id: 'a1' }
+
+    DynamoDBClientMock.prototype.send.mockResolvedValueOnce(
+        { Attributes: {} } as never
+    )
+
+    const res = await handler(ev)
+
+    expect(res.statusCode).toBe(0)
+
+    expect(PutItemCommandMock).toBeCalled()
+    expect(PutItemCommandMock.mock.calls[0][0].Item).toEqual({id: {S: 'a1'}})
+
+    expect(DynamoDBClientMock).toBeCalled()
+    expect(DynamoDBClientMock.mock.instances).toHaveLength(1)
+})
+
+test('call handler with dynamodb error1', async () => {
+    const ev: SampleInput = { id: 'a1' }
+
+    DynamoDBClientMock.prototype.send.mockRejectedValueOnce(
+        new Error('test error') as never
+    )
+
+    const res = await handler(ev)
+
+    expect(res.statusCode).toBe(1)
+
+    expect(DynamoDBClientMock).toBeCalled()
+    expect(DynamoDBClientMock.mock.instances).toHaveLength(1)
+})
+
+test('call handler with dynamodb error2', async () => {
     const ev: SampleInput = { id: 'a1' }
 
     jest.spyOn(DynamoDBClientMock.prototype, 'send')
