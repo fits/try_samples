d45065bbd71460f903cce38cd6ba53144cec3bfa
fits <wadays_wozx@nifty.com>
2021-05-03 22:23:05 +0900

    vite を使った svelte サンプル

---
+++ TypeScript/vite/svelte/sample/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <Counter />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+  }
+</style>
---
+++ TypeScript/vite/svelte/sample/src/lib/Counter.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  let count: number = 0
+
+  const inc = () => count += 1
+  const dec = () => count -= 1
+</script>
+
+<div class="box">
+  <div>
+    <button on:click={ dec }>down</button>
+  </div>
+  <div class="value">
+    counter: { count }
+  </div>
+  <div>
+    <button on:click={ inc }>up</button>
+  </div>
+</div>
+
+<style>
+  .box {
+    display: flex;
+  }
+
+  .value {
+    width: 20%;
+  }
+</style>
