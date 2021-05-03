86fa1fb5fc00f280ac0b1339cb2d8eeb578fd5d6
fits <wadays_wozx@nifty.com>
2021-05-03 22:56:46 +0900

    vite による Vue.js サンプル

---
+++ TypeScript/vite/vue/sample/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Counter />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Counter from './components/Counter.vue'
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      Counter
+    }
+  })
+</script>
+
+<style>
+  #app {
+    text-align: center;
+    padding: 1em;
+  }
+</style>
---
+++ TypeScript/vite/vue/sample/src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <div>
+      <button v-on:click="dec">down</button>
+    </div>
+    <div class="value" v-on:click="count++">
+      counter: {{ count }}
+    </div>
+    <div>
+      <button v-on:click="inc">up</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { ref, defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Counter',
+    setup: () => {
+      const count = ref(0)
+
+      const inc = () => count.value++
+      const dec = () => count.value--
+
+      return {
+        count,
+        inc,
+        dec
+      }
+    }
+  })
+</script>
+
+<style scoped>
+  .box {
+    display: flex;
+  }
+
+  .value {
+    width: 20%;
+  }
+</style>
