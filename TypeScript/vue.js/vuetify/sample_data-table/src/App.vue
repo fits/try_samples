42d05a1b3399baf9a9d8c31573abd5ae8d90871b
fits <wadays_wozx@nifty.com>
2021-06-13 22:51:13 +0900

    Vuetify VDataTable sample

---
+++ TypeScript/vue.js/vuetify/sample_data-table/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <Sample />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Sample from './components/Sample.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    Sample,
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ TypeScript/vue.js/vuetify/sample_data-table/src/components/Sample.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="10">
+        <v-data-table :headers="headers" :items="items">
+        </v-data-table>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="8">
+        <v-data-table :headers="headers" :items="items" group-by="category">
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Sample',
+
+    data: () => ({
+      items: [
+        { name: 'item-1', value: 1, category: 'A' },
+        { name: 'item-2', value: 120, category: 'B' },
+        { name: 'item-3', value: 30, category: 'C' },
+        { name: 'item-4', value: 10, category: 'C' },
+        { name: 'item-5', value: 340, category: 'D' },
+        { name: 'item-6', value: 660, category: 'E' },
+        { name: 'item-7', value: 7, category: 'E' },
+      ]
+    }),
+    computed: {
+      headers: () => [
+        { text: 'Name', value: 'name' },
+        { text: 'Value', value: 'value' },
+        { text: 'Category', value: 'category' },
+      ]
+    },
+  })
+</script>
