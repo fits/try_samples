ede724b9b5b1a933d99d7ec955a1094b47195a66
fits <wadays_wozx@nifty.com>
2021-06-20 19:36:59 +0900

    async await 追加

--- TypeScript/vue.js/vue2/test-utils_sample/tests/Counter.test.ts
+++ TypeScript/vue.js/vue2/test-utils_sample/tests/Counter.test.ts
@@ -3,12 +3,12 @@ import 'jsdom-global/register'
 import { mount } from '@vue/test-utils'
 import Counter from '../src/components/Counter.vue'
 
-test('count up', () => {
+test('count up', async () => {
     const counter = mount(Counter)
 
     expect(counter.vm.$data.count).toBe(0)
 
-    counter.get('button').trigger('click')
+    await counter.get('button').trigger('click')
 
     expect(counter.vm.$data.count).toBe(1)
 })
--- TypeScript/vue.js/vue3/test-utils_sample/tests/Counter.test.ts
+++ TypeScript/vue.js/vue3/test-utils_sample/tests/Counter.test.ts
@@ -1,12 +1,12 @@
 import { mount } from '@vue/test-utils'
 import Counter from '../src/components/Counter.vue'
 
-test('count up', () => {
+test('count up', async () => {
     const counter = mount(Counter)
 
     expect(counter.vm.count).toBe(0)
 
-    counter.get('button').trigger('click')
+    await counter.get('button').trigger('click')
 
     expect(counter.vm.count).toBe(1)
 })
