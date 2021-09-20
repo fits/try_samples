3b68c58ae0562b988c88d75e88681150424b2901
fits <wadays_wozx@nifty.com>
2021-09-20 21:40:51 +0900

    2 space indent

--- deno/elasticsearch/nested_aggs/aggs1.ts
+++ deno/elasticsearch/nested_aggs/aggs1.ts
@@ -7,25 +7,25 @@ const baseUrl = 'http://localhost:9200'
 const indexUrl = `${baseUrl}/${index}`
 
 const { body } = await send(`${indexUrl}/_search`, 'POST', {
-    size: 0,
-    aggs: {
-        group_by_id: {
-            terms: { field: 'id' },
-            aggs: {
-                editions: {
-                    nested: { path: 'editions' },
-                    aggs: {
-                        price_min: {
-                            min: { field: 'editions.price' }
-                        },
-                        price_max: {
-                            max: { field: 'editions.price' }
-                        }
-                    }
-                }
+  size: 0,
+  aggs: {
+    group_by_id: {
+      terms: { field: 'id' },
+      aggs: {
+        editions: {
+          nested: { path: 'editions' },
+          aggs: {
+            price_min: {
+              min: { field: 'editions.price' }
+            },
+            price_max: {
+              max: { field: 'editions.price' }
             }
+          }
         }
+      }
     }
+  }
 })
 
 console.log(body)
