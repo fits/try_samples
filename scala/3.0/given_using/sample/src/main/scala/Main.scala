9a51a722e39a3c9a6dcfa45074564279b4d8506e
fits <wadays_wozx@nifty.com>
2021-10-19 21:01:56 +0900

    Given Using sample

---
+++ scala/3.0/given_using/sample/src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+
+trait Context[A]:
+  def show(v: A): Unit
+
+def sample(v: String)(using ctx: Context[String]) =
+  ctx.show(v + "!!")
+
+def sample(v: Int)(using ctx: Context[Int]) =
+  ctx.show(v * 10)
+
+@main def main: Unit = 
+  given Context[String] = new Context[String]:
+    def show(v: String): Unit = println(s"string context: $v")
+
+  given c: Context[Int] = new Context[Int]:
+    def show(v: Int): Unit = println(s"int context: $v")
+
+  sample("test1")
+  sample(5)
