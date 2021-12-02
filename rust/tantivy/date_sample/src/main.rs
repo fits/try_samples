3d5b548acfd4955cbe18b051806ea761e32acbeb
fits <wadays_wozx@nifty.com>
2021-12-03 02:20:29 +0900

    date sample

---
+++ rust/tantivy/date_sample/src/main.rs
@@ -0,0 +1,23 @@
+
+use tantivy::doc;
+use tantivy::schema::*;
+use chrono::prelude::*;
+
+fn main() -> tantivy::Result<()>{
+    let mut schema_builder = Schema::builder();
+
+    let name = schema_builder.add_text_field("name", STRING);
+    let date = schema_builder.add_date_field("date", INDEXED);
+
+    let schema = schema_builder.build();
+
+    let doc = doc!(
+        name => "item1", date => Utc::now()
+    );
+
+    let json = schema.to_json(&doc);
+
+    println!("{}", json);
+
+    Ok(())
+}
