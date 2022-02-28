c87c4cea956e4d8980846a37445e690351a671b5
fits <wadays_wozx@nifty.com>
2022-02-28 19:25:17 +0900

    json sample

---
+++ go/http-server/json_sample/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Document = map[string]interface{}
+
+type Server struct {
+	docs []Document
+}
+
+func (s *Server) add(doc Document) int {
+	id := len(s.docs)
+	s.docs = append(s.docs, doc)
+
+	return id
+}
+
+type AddResult struct {
+	ID int `json:"id"`
+}
+
+func main() {
+	server := Server{}
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			json.NewEncoder(rw).Encode(server.docs)
+			break
+		case http.MethodPost:
+			var doc Document
+			err := json.NewDecoder(r.Body).Decode(&doc)
+
+			if err != nil {
+				rw.WriteHeader(http.StatusBadRequest)
+				fmt.Fprintf(rw, "%v", err)
+			} else {
+				id := server.add(doc)
+
+				rw.WriteHeader(http.StatusCreated)
+				json.NewEncoder(rw).Encode(AddResult{id})
+			}
+
+			break
+		default:
+			rw.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
