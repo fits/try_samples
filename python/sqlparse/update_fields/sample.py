47ff9cefdc769069f3db23e6f0db77a04d533870
fits <wadays_wozx@nifty.com>
2022-04-15 05:53:13 +0900

    UPDATE SQL parse

---
+++ python/sqlparse/update_fields/sample.py
@@ -0,0 +1,28 @@
+import sqlparse
+from sqlparse.sql import Identifier, IdentifierList, Comparison, Token
+from sqlparse.tokens import DML, DDL
+
+import sys
+
+sql = sys.stdin.read()
+
+def fields_to_update(st, prefix=[]):
+    p = prefix
+
+    for t in st.tokens:
+        match t:
+            case Token(ttype=tt, value=v) if tt == DDL or (tt == DML and v.upper() != 'UPDATE'):
+                break
+            case IdentifierList():
+                for f in fields_to_update(t, p):
+                    yield f
+            case Identifier():
+                p = p + [t.get_real_name()]
+            case Comparison(left=Identifier() as l):
+                yield '.'.join(p + [l.get_real_name()])
+
+for s in sqlparse.parse(sql):
+    fields = list(fields_to_update(s))
+    print(fields)
+
+    print('-----')
