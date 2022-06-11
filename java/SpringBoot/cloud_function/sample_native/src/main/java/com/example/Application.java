664a6f14c77ff8842936ddc1eee8138687c04e3e
fits <wadays_wozx@nifty.com>
2022-06-12 02:53:52 +0900

    Spring Native 適用版

---
+++ java/SpringBoot/cloud_function/sample_native/src/main/java/com/example/Application.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import static org.springframework.nativex.hint.TypeAccess.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.TypeHint;
+
+import java.util.function.Function;
+
+@ResourceHint(patterns = "org/joda/time/tz/data/.*")
+@TypeHint(types = Application.Item.class, access = { DECLARED_CONSTRUCTORS, DECLARED_METHODS })
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Function<ItemInput, Item> sample() {
+        return input -> new Item("item-" + input.value(), input.value());
+    }
+
+    public record ItemInput(int value) {}
+    public record Item(String id, int value) {}
+}
