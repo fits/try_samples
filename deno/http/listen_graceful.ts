5ab9df06be5f037f709970fcb5809bc347d19feb
woz <woz@example.com>
2022-10-25 13:31:55 +0900

    shutdown sample

---
+++ deno/http/listen_graceful.ts
@@ -0,0 +1,26 @@
+
+const port = 8080
+
+const handle = async (con: Deno.Conn) => {
+    for await (const req of Deno.serveHttp(con)) {
+        console.log(req.request.url)
+        await req.respondWith(new Response('ok'))
+    }
+}
+
+const server = Deno.listen({ port })
+
+console.log(`listen: ${port}`)
+
+const terminate = () => {
+    console.log('Shutdown ...')
+
+    setTimeout(() => server.close(), 3000)
+}
+
+Deno.addSignalListener('SIGINT', terminate)
+Deno.addSignalListener('SIGTERM', terminate)
+
+for await (const con of server) {
+    handle(con)
+}
