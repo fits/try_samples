04a617ca758f7d77d7f2feff0ea627a1e1d47c20
woz <woz@example.com>
2022-11-24 14:16:47 +0900

    add resolver sample

--- scala/sangria/sample/src/main/scala/Main.scala
+++ scala/sangria/sample/src/main/scala/Main.scala
@@ -7,7 +7,7 @@ import scala.concurrent.ExecutionContext
 
 case class Item(name: String, value: Int)
 
-@main def main(): Unit =
+def sample1(): Unit =
   val ItemType = ObjectType("Item", fields[Unit, Item](
     Field("name", StringType, resolve = _.value.name),
     Field("value", IntType, resolve = _.value.value)
@@ -26,3 +26,45 @@ case class Item(name: String, value: Int)
   val result = Executor.execute(schema, query)
 
   result.foreach(r => println(r))
+end sample1
+
+def sample2(): Unit =
+  val ast =
+    graphql"""
+      type Item {
+        name: String!
+        value: Int!
+      }
+
+      type Query {
+        find: Item!
+      }
+    """
+
+  import sangria.schema.AstSchemaBuilder._
+
+  val builder = AstSchemaBuilder.resolverBased(
+    FieldResolver(
+      {
+        case (TypeName("Query"), FieldName("find")) => _ => Item("item-2", 34)
+        case (TypeName("Item"), FieldName("name")) => _.asInstanceOf[Context[_, Item]].value.name
+        case (TypeName("Item"), FieldName("value")) => _.asInstanceOf[Context[_, Item]].value.value
+      },
+      PartialFunction.empty
+    )
+  )
+
+  val schema = Schema.buildFromAst(ast, builder)
+
+  val query = graphql"{ find { name value } }"
+
+  given executionContext: ExecutionContext = ExecutionContext.global
+
+  val result = Executor.execute(schema, query)
+
+  result.foreach(r => println(r))
+end sample2
+
+@main def main(): Unit =
+  sample1()
+  sample2()
