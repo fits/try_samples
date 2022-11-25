07268ccf684eac4dd236a31a36f9ba7ebd4606a9
woz <woz@example.com>
2022-11-25 19:39:37 +0900

    sangria + http4s sample

---
+++ scala/sangria/sample_http4s/src/main/scala/App.scala
@@ -0,0 +1,65 @@
+import cats.effect.{ExitCode, IO, IOApp}
+import io.circe.generic.auto.*
+import com.comcast.ip4s.Port
+import org.http4s.{EntityDecoder, HttpRoutes, MediaType}
+import org.http4s.circe.jsonOf
+import org.http4s.headers.`Content-Type`
+import org.http4s.dsl.io.*
+import org.http4s.ember.server.EmberServerBuilder
+import sangria.schema.*
+import sangria.execution.Executor
+import sangria.parser.QueryParser
+import sangria.marshalling.circe.*
+
+import scala.util.Properties
+import scala.concurrent.ExecutionContext
+
+case class GqlQuery(query: String)
+case class Item(name: String, value: Int)
+
+object App extends IOApp.Simple:
+  val port: Port = Properties.envOrNone("SERVICE_PORT")
+    .flatMap(Port.fromString)
+    .orElse(Port.fromInt(8080))
+    .get
+
+  given EntityDecoder[IO, GqlQuery] = jsonOf
+  given ExecutionContext = ExecutionContext.global
+
+  val ItemType = ObjectType("Item", fields[Unit, Item](
+    Field("name", StringType, resolve = _.value.name),
+    Field("value", IntType, resolve = _.value.value)
+  ))
+
+  val QueryType = ObjectType("Query", fields[Unit, Unit](
+    Field("find", ItemType, resolve = _ => Item("item-1", 12))
+  ))
+
+  val schema = Schema(QueryType)
+
+  val svc = HttpRoutes.of[IO] {
+    case req @ POST -> Root =>
+      val params = req.headers.get[`Content-Type`] match
+        case Some(`Content-Type`(MediaType.application.json, _)) => req.as[GqlQuery]
+        case _ => req.as[String].map(GqlQuery(_))
+
+      params
+        .flatMap { q =>
+          val query = QueryParser.parse(q.query).get
+
+          val res = Executor.execute(schema, query).map(_.spaces2)
+
+          IO.fromFuture(IO(res))
+        }
+        .flatMap(Ok(_))
+  }
+
+  val app = svc.orNotFound
+
+  val server = EmberServerBuilder
+    .default[IO]
+    .withPort(port)
+    .withHttpApp(app)
+    .build
+
+  override def run: IO[Unit] = server.useForever.as(ExitCode.Success)
