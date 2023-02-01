41f285ec430b188af90c656068eb4dedc188dd3a
woz <woz@example.com>
2023-02-01 18:17:52 +0900

    simple actor app

---
+++ dapr/actor_app/sample1/sample.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type daprConfig struct {
+	Entities []string `json:"entities"`
+}
+
+func main() {
+	port := os.Getenv("APP_HTTP_PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	http.HandleFunc("/dapr/config", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("called dapr config")
+
+		config := daprConfig{[]string{"sampleActor"}}
+
+		json.NewEncoder(w).Encode(config)
+	})
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("called healthz")
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.HandleFunc("/actors/", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.TrimPrefix(r.URL.Path, "/actors/")
+
+		log.Printf("called actors: %s", path)
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
