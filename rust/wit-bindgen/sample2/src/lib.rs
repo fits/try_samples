bc26f75f1568dd224dc69898a812e5081613a952
woz <woz@example.com>
2023-05-03 10:55:40 +0900

    update

--- rust/wit-bindgen/sample2/src/lib.rs
+++ rust/wit-bindgen/sample2/src/lib.rs
@@ -5,30 +5,33 @@ use types::{EmptyCart, ActiveCart, CartItem};
 
 struct Component;
 
+export_cart_world!(Component);
+
+fn add_cart_item(state: Cart, citem: CartItem) -> Option<Cart> {
+    match state {
+        Cart::EmptyCart(EmptyCart { id }) => {
+            Some(Cart::ActiveCart(ActiveCart { id: id.clone(), items: vec![citem] }))
+        }
+        Cart::ActiveCart(ActiveCart { id, items }) => {
+            let new_items = [items.clone(), vec![citem]].concat();
+            Some(Cart::ActiveCart(ActiveCart { id: id.clone(), items: new_items }))
+        }
+    }
+}
+
 impl CartWorld for Component {
     fn create(id: CartId) -> Cart {
         Cart::EmptyCart(EmptyCart { id: id.clone() })
     }
 
     fn add_item(state: Cart, item: ItemIdResult, qty: Quantity) -> Option<Cart> {
-        let price = find_price(&item);
-
-        if let Some(p) = price {
-            let ci = CartItem { item: item.clone(), qty, unit_price: p };
-
-            match state {
-                Cart::EmptyCart(EmptyCart { id }) => {
-                    Some(Cart::ActiveCart(ActiveCart { id: id.clone(), items: vec![ci] }))
-                }
-                Cart::ActiveCart(ActiveCart { id, items }) => {
-                    let new_items = [items.clone(), vec![ci]].concat();
-                    Some(Cart::ActiveCart(ActiveCart { id: id.clone(), items: new_items }))
-                }
-            }
-        } else {
-            None
+        if qty == 0 {
+            return None
         }
+
+        find_price(&item)
+            .and_then(|p|
+                add_cart_item(state, CartItem { item: item.clone(), qty, unit_price: p })
+            )
     }
 }
-
-export_cart_world!(Component);
