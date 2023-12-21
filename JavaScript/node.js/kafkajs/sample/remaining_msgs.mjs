59440e32ab589d1e737bb0ec724c78bc7c11f052
woz <woz@example.com>
2023-12-21 18:14:04 +0900

    kafka consumed

---
+++ JavaScript/node.js/kafkajs/sample/remaining_msgs.mjs
@@ -0,0 +1,49 @@
+import { Kafka } from 'kafkajs'
+
+const clientId = process.env.KAFKA_CLIENT_ID ?? 'admin1'
+const broker = process.env.KAFKA_BROKER ?? 'localhost:9092'
+
+const topic = process.argv[2]
+const groupId = process.argv[3]
+
+const kafka = new Kafka({
+    clientId,
+    brokers: [ broker ]
+})
+
+const admin = kafka.admin()
+
+const run = async () => {
+    await admin.connect()
+
+    try {
+        const res = {}
+
+        const ts = await admin.fetchTopicOffsets(topic)
+
+        ts.forEach(t => {
+            res[t.partition] = { current: t.high }
+        })
+
+        const os = await admin.fetchOffsets({
+            groupId,
+            topics: [ topic ]
+        })
+
+        os[0].partitions.forEach(p => {
+            const tmp = res[p.partition]
+            res[p.partition] = Object.assign(tmp, { consume: p.offset, diff: tmp.current - p.offset })
+        })
+
+        console.log(res)
+
+        const remain = Object.values(res).reduce((acc, v) => acc + v.diff, 0)
+
+        console.log(`remaining messages: ${remain}`)
+
+    } finally {
+        await admin.disconnect()
+    }
+}
+
+run().catch(err => console.error(err))
