77133a6ab2112a29ed69951229142178244c63f6
woz <woz@example.com>
2024-01-18 20:13:10 +0900

    update

--- python/diffusers/img2img/sd1_mps.py
+++ python/diffusers/img2img/sd1_mps.py
@@ -1,27 +1,29 @@
-from diffusers import AutoPipelineForImage2Image, DPMSolverMultistepScheduler
+from diffusers import AutoPipelineForImage2Image, DPMSolverMultistepScheduler, AutoencoderKL
 from diffusers.models.attention_processor import AttnProcessor2_0
 from diffusers.utils import load_image
 
 import sys
 
 steps = 25
+local = True
 
-model = 'runwayml/stable-diffusion-v1-5'
+ng_prompt = 'ugly, deformed, disfigured, poor details, bad anatomy'
 
 inputfile = sys.argv[1]
-destfile = sys.argv[2]
-prompt = sys.argv[3]
+prompt = sys.argv[2]
+destfile = sys.argv[3]
 
-pipe = AutoPipelineForImage2Image.from_pretrained(model, use_safetensors=True, local_files_only=True, requires_safety_checker=False, safety_checker=None)
+pipe = AutoPipelineForImage2Image.from_pretrained('runwayml/stable-diffusion-v1-5', use_safetensors=True, local_files_only=local, requires_safety_checker=False, safety_checker=None)
 pipe = pipe.to('mps')
 
 pipe.unet.set_attn_processor(AttnProcessor2_0())
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe.vae = AutoencoderKL.from_pretrained('stabilityai/sd-vae-ft-mse').to('mps')
 
 pipe.enable_attention_slicing()
 
-input = load_image(inputfile)
+input = load_image(inputfile).resize((512, 512))
 
-img = pipe(prompt, image=input, num_inference_steps=steps, guidance_scale=3.0, strength=0.7).images[0]
+img = pipe(prompt, negative_prompt=ng_prompt, image=input, num_inference_steps=steps, guidance_scale=8.0, strength=0.6).images[0]
 
 img.save(destfile)
