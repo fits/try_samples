63c821face100e58d10b4ebc9b9672353f7154e0
woz <woz@example.com>
2024-01-26 09:30:24 +0900

    fix

--- JavaScript/node.js/node-forge/x509_sample/gen_rsa2pem.mjs
+++ JavaScript/node.js/node-forge/x509_sample/gen_rsa2pem.mjs
@@ -3,7 +3,5 @@ const { pki } = forge
 
 const keys = pki.rsa.generateKeyPair({bits: 2048})
 
-pki.publicKeyToPem
-
 console.log( pki.publicKeyToPem(keys.publicKey) )
 console.log( pki.privateKeyToPem(keys.privateKey) )
