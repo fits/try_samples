faa002a99ac5a5d244ffe754d4f8cf1bb27bc9b5
woz <woz@example.com>
2024-04-26 01:07:07 +0900

    Dictionary LINQ

---
+++ csharp/linq/dic_sample/Program.cs
@@ -0,0 +1,21 @@
+﻿var ds1 = new Dictionary<string, int>
+{
+    {"a", 1},
+    {"b", 2},
+    {"c", 3},
+    {"d", 4},
+    {"e", 5},
+    {"f", 6}
+};
+
+var ds2 = ds1
+    .Where(p => p.Value % 2 == 1)
+    .Select(p => new { p.Key, Value = p.Value.ToString() })
+    .ToDictionary(p => p.Key, p => p.Value);
+
+Console.WriteLine(ds2);
+
+foreach (var d in ds2)
+{
+    Console.WriteLine(d);
+}
