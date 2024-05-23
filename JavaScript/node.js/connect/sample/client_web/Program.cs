b889c02ead20078b9b4275872f59883281fcb350
woz <woz@example.com>
2024-05-23 11:56:00 +0900

    gRPC-Web client

---
+++ JavaScript/node.js/connect/sample/client_web/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+var channel = GrpcChannel.ForAddress(
+    "https://localhost:5001", 
+    new GrpcChannelOptions { HttpHandler = new GrpcWebHandler(httpHandler) }
+);
+
+var client = new Process.ProcessClient(channel);
+
+var res = client.Exec(new Input { Command = "test" });
+
+Console.WriteLine($"{res}");
