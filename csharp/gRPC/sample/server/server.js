b4f4a6ec74b0aafb70e8d6a6d5771d1975152a21
woz <woz@example.com>
2024-05-29 01:58:54 +0900

    https sample

--- csharp/gRPC/sample/server/server.js
+++ csharp/gRPC/sample/server/server.js
@@ -1,5 +1,43 @@
 const protoLoader = require('@grpc/proto-loader')
 const grpc = require('@grpc/grpc-js')
+const { pki } = require('node-forge')
+
+const createCred = () => {
+    if (process.argv.length < 3) {
+        return grpc.ServerCredentials.createInsecure()
+    }
+
+    const keys = pki.rsa.generateKeyPair({bits: 2048})
+
+    const cert = pki.createCertificate()
+    
+    cert.publicKey = keys.publicKey
+    
+    cert.validity.notBefore = new Date()
+    
+    const expiryDate = new Date()
+    expiryDate.setFullYear(expiryDate.getFullYear() + 1)
+    
+    cert.validity.notAfter = expiryDate
+    
+    const attrs = [
+        { name: 'commonName', value: 'example.org' },
+        { name: 'countryName', value: 'US' }
+    ]
+    
+    cert.setSubject(attrs)
+    cert.setIssuer(attrs)
+    
+    cert.sign(keys.privateKey)
+
+    return grpc.ServerCredentials.createSsl(
+        null,
+        [{
+            private_key: Buffer.from(pki.privateKeyToPem(keys.privateKey)),
+            cert_chain: Buffer.from(pki.certificateToPem(cert)),
+        }]
+    )
+}
 
 const pd = protoLoader.loadSync('../proto/sample.proto')
 const proto = grpc.loadPackageDefinition(pd)
@@ -16,9 +54,7 @@ server.addService(proto.Process.service, {
     },
 })
 
-const cred = grpc.ServerCredentials.createInsecure()
-
-server.bindAsync('0.0.0.0:5001', cred, (err, port) => {
+server.bindAsync('0.0.0.0:5001', createCred(), (err, port) => {
     if (err != null) {
         console.error(err)
         return
