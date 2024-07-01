e8980ce7d6fb184b651f83aac61bd06350eb6261
woz <woz@example.com>
2024-07-02 02:13:58 +0900

    golang struct

---
+++ go/cel/sample2/go.mod
@@ -0,0 +1,15 @@
+module sample
+
+go 1.22.4
+
+require github.com/google/cel-go v0.20.1
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/stoewer/go-strcase v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ go/cel/sample2/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/ext"
+)
+
+type Item struct {
+	ID    string
+	Value int
+}
+
+func main() {
+	env, err := cel.NewEnv(
+		cel.Variable("item", cel.ObjectType("main.Item")),
+		ext.NativeTypes(reflect.TypeOf(&Item{})),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ast, iss := env.Compile(`"id=" + item.ID + ", value=" + string(item.Value)`)
+
+	if iss.Err() != nil {
+		log.Fatal(iss.Err())
+	}
+
+	prg, err := env.Program(ast)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	item := Item{"item-1", 1000}
+
+	out, _, err := prg.Eval(map[string]any{
+		"item": item,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(out)
+}
