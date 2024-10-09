eebaa1604323621fc1ec14f19d9b9232febbcaac
woz <woz@example.com>
2024-10-09 21:26:46 +0900

    add diff

--- rust/git2-rs/commit_files/src/main.rs
+++ rust/git2-rs/commit_files/src/main.rs
@@ -1,4 +1,4 @@
-use git2::Repository;
+use git2::{DiffFindOptions, Repository};
 use std::env;
 
 fn main() -> Result<(), git2::Error> {
@@ -24,6 +24,45 @@ fn main() -> Result<(), git2::Error> {
                 entry.id(),
                 entry.kind(),
                 entry.name().unwrap_or("")
+            );
+        }
+
+        let old_tree = if commit.parent_count() == 1 {
+            Some(commit.parent(0)?.tree()?)
+        } else {
+            None
+        };
+
+        let diff = repo.diff_tree_to_tree(old_tree.as_ref(), Some(&tree), None)?;
+
+        println!("  diff:");
+
+        for d in diff.deltas() {
+            println!(
+                "    status={:?}, oldfile={}, newfile={}, nfiles={}",
+                d.status(),
+                d.old_file().path().and_then(|p| p.to_str()).unwrap_or(""),
+                d.new_file().path().and_then(|p| p.to_str()).unwrap_or(""),
+                d.nfiles()
+            );
+        }
+
+        let mut diff2 = repo.diff_tree_to_tree(old_tree.as_ref(), Some(&tree), None)?;
+
+        println!("  diff (detect renamed):");
+
+        let mut opts = DiffFindOptions::new();
+        opts.renames(true);
+
+        diff2.find_similar(Some(&mut opts))?;
+
+        for d in diff2.deltas() {
+            println!(
+                "    status={:?}, oldfile={}, newfile={}, nfiles={}",
+                d.status(),
+                d.old_file().path().and_then(|p| p.to_str()).unwrap_or(""),
+                d.new_file().path().and_then(|p| p.to_str()).unwrap_or(""),
+                d.nfiles()
             );
         }
     }
