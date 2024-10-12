f85377ebe2188ca8cb7da4940ab5d42227a85311
woz <woz@example.com>
2024-10-13 00:54:28 +0900

    print updated file

---
+++ rust/git2-rs/commit_files2/src/main.rs
@@ -0,0 +1,53 @@
+use git2::{Delta, DiffFile, DiffFindOptions, Repository};
+use std::env;
+
+fn main() -> Result<(), git2::Error> {
+    let path = env::args().skip(1).next().unwrap_or(".".into());
+
+    let repo = Repository::open(path)?;
+    let mut walk = repo.revwalk()?;
+
+    walk.push_head()?;
+
+    for oid in walk {
+        let commit = repo.find_commit(oid?)?;
+        let tree = commit.tree()?;
+
+        let old_tree = if commit.parent_count() == 1 {
+            Some(commit.parent(0)?.tree()?)
+        } else {
+            None
+        };
+
+        let mut diff = repo.diff_tree_to_tree(old_tree.as_ref(), Some(&tree), None)?;
+
+        let mut opts = DiffFindOptions::new();
+        opts.renames(true);
+
+        diff.find_similar(Some(&mut opts))?;
+
+        for d in diff.deltas() {
+            match d.status() {
+                Delta::Added | Delta::Deleted | Delta::Modified => {
+                    if let Some(f) = path_to_str(&d.new_file()) {
+                        println!("{}", f);
+                    }
+                }
+                Delta::Renamed => {
+                    println!(
+                        "{}, {}",
+                        path_to_str(&d.old_file()).unwrap_or(""),
+                        path_to_str(&d.new_file()).unwrap_or(""),
+                    );
+                }
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn path_to_str<'a>(f: &'a DiffFile) -> Option<&'a str> {
+    f.path().and_then(|x| x.to_str())
+}
