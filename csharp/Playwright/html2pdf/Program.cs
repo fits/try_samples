17624f4de2ea3ce4e048e83d9e8c24c0e79e7f7a
woz <woz@example.com>
2024-10-25 15:37:47 +0900

    html to pdf by Playwright for .NET

---
+++ csharp/Playwright/html2pdf/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Playwright;
+
+var html = args[0];
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+
+var page = await browser.NewPageAsync();
+await page.SetContentAsync(html);
+
+var data = await page.PdfAsync();
+
+File.WriteAllBytes("output.pdf", data);
