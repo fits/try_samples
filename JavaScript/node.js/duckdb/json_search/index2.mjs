1a70a1d6a7c8337ea26953c9b0c61e3a5e3ad459
woz <woz@example.com>
2024-11-27 01:28:58 +0900

    unnest SQL

---
+++ JavaScript/node.js/duckdb/json_search/index2.mjs
@@ -0,0 +1,39 @@
+import { DuckDBInstance } from '@duckdb/node-api'
+
+const jsonFile = 'data/products.jsonl'
+
+const instance = await DuckDBInstance.create()
+const con = await instance.connect()
+
+const runQuery = async (query) => {
+    const reader = await con.runAndReadAll(query)
+
+    console.log(reader.getRows())
+}
+
+await runQuery(`SELECT id, title FROM '${jsonFile}' WHERE id = 5`)
+
+await runQuery(`SELECT id, title, meta.barcode, meta FROM '${jsonFile}' WHERE meta.barcode='6707669443381'`)
+
+await runQuery(`
+    SELECT
+        id, title 
+    FROM
+        (SELECT id, title, unnest(reviews) as reviews FROM '${jsonFile}')
+    WHERE
+        reviews.rating = 1
+`)
+
+await runQuery(`
+    SELECT DISTINCT
+        id, title 
+    FROM
+        (SELECT id, title, unnest(reviews) as reviews FROM '${jsonFile}')
+    WHERE
+        reviews.rating = 1
+`)
+
+await runQuery(`
+    WITH x AS (SELECT id, title, unnest(reviews) as reviews FROM '${jsonFile}')
+    SELECT DISTINCT id, title FROM x WHERE reviews.rating = 1 ORDER BY id
+`)
