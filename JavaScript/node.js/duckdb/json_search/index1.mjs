4ff88207c70876c3d4bd7f412c7e43235433d259
woz <woz@example.com>
2024-11-27 01:41:41 +0900

    delete unnecessary line

--- JavaScript/node.js/duckdb/json_search/index1.mjs
+++ JavaScript/node.js/duckdb/json_search/index1.mjs
@@ -3,10 +3,6 @@ import { DuckDBInstance } from '@duckdb/node-api'
 const instance = await DuckDBInstance.create()
 const con = await instance.connect()
 
-const reader = await con.runAndReadAll(`
-    SELECT * FROM data1.jsonl WHERE value > 30
-`)
-
 const runQuery = async (query) => {
     const reader = await con.runAndReadAll(query)
 
