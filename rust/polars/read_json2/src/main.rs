9d208f2358bbd6b670fdec6f7a2460594c7ba65f
woz <woz@example.com>
2024-12-05 01:12:28 +0900

    nest field

--- rust/polars/read_json2/src/main.rs
+++ rust/polars/read_json2/src/main.rs
@@ -10,9 +10,25 @@ fn main() -> Result<()> {
     println!("{:?}", df.schema());
     dbg!(&df);
 
-    let df2 = df.lazy().filter(col("price").gt(lit(1500))).collect()?;
+    let df2 = df
+        .clone()
+        .lazy()
+        .filter(col("price").gt(lit(1500)))
+        .collect()?;
 
     dbg!(&df2);
+
+    let df3 = df
+        .lazy()
+        .filter(
+            col("attrs")
+                .struct_()
+                .field_by_name("category")
+                .eq(lit("A1")),
+        )
+        .collect()?;
+
+    dbg!(&df3);
 
     Ok(())
 }
