a881cb08c9f2aeee346d215675484187375fa68d
woz <woz@example.com>
2024-12-11 00:53:20 +0900

    add Array pattern

--- rust/datafusion/udf_sample/src/main.rs
+++ rust/datafusion/udf_sample/src/main.rs
@@ -1,3 +1,4 @@
+use datafusion::arrow::array::{ArrayRef, AsArray, RecordBatch, StringArray};
 use datafusion::arrow::datatypes::DataType;
 use datafusion::common::internal_err;
 use datafusion::logical_expr::{ColumnarValue, Volatility};
@@ -17,6 +18,17 @@ fn append(args: &[ColumnarValue]) -> datafusion::error::Result<ColumnarValue> {
         ) => {
             let r = a.clone().unwrap_or_default() + &b.clone().unwrap_or_default();
             Ok(ColumnarValue::Scalar(ScalarValue::from(r)))
+        }
+        (ColumnarValue::Array(a_array), ColumnarValue::Scalar(ScalarValue::Utf8(b))) => {
+            let b = b.clone().unwrap_or_default();
+
+            let r = a_array
+                .as_string::<i32>()
+                .iter()
+                .map(|x| x.map(|y| y.to_string() + &b))
+                .collect::<StringArray>();
+
+            Ok(ColumnarValue::Array(Arc::new(r)))
         }
         _ => {
             internal_err!("invalid argument types")
@@ -42,6 +54,13 @@ async fn main() -> datafusion::error::Result<()> {
     run_query(&ctx, "SELECT append('ab', 123)").await?;
     run_query(&ctx, "SELECT append(120, 345)").await?;
     run_query(&ctx, "SELECT append('test-', [1, 2, 3])").await?;
+
+    let name: ArrayRef = Arc::new(StringArray::from(vec![Some("a1"), Some("b2"), Some("c3"), None, Some("d4")]));
+    let batch = RecordBatch::try_from_iter(vec![("name", name)])?;
+
+    ctx.register_batch("data", batch)?;
+
+    run_query(&ctx, "SELECT append(name, '-suffix') FROM data").await?;
 
     Ok(())
 }
