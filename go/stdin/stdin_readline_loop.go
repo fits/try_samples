249f6a6f8ecb3e63ad896bed219c541edbc7664c
woz <woz@example.com>
2025-01-24 10:31:55 +0900

    stdin scan

---
+++ go/stdin/stdin_readline_loop.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+
+	for {
+		s.Scan()
+		t := s.Text()
+
+		if len(t) > 0 {
+			fmt.Printf("* input=%s\n", t)
+		} else {
+			break
+		}
+	}
+}
