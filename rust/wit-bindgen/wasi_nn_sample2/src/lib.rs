4ca420c15f7467f332d42b6f12e90eeb41a33da8
woz <woz@example.com>
2025-04-27 20:24:17 +0900

    querystring parameters

--- rust/wit-bindgen/wasi_nn_sample2/src/lib.rs
+++ rust/wit-bindgen/wasi_nn_sample2/src/lib.rs
@@ -3,6 +3,7 @@ use wasi::http::types::{ErrorCode, Headers, OutgoingResponse};
 use wasi::nn::graph::{ExecutionTarget, GraphEncoding, load};
 use wasi::nn::tensor::{Tensor, TensorType};
 
+use std::collections::HashMap;
 use std::fs;
 
 wit_bindgen::generate!({
@@ -13,8 +14,9 @@ wit_bindgen::generate!({
 struct Host;
 
 impl Guest for Host {
-    fn handle(_request: IncomingRequest, response_out: ResponseOutparam) -> () {
-        let res = evaluate()
+    fn handle(request: IncomingRequest, response_out: ResponseOutparam) -> () {
+        let res = to_params(&request)
+            .and_then(|(a, b)| evaluate(a, b))
             .and_then(|x| to_output(&x))
             .map_err(|e| ErrorCode::InternalError(Some(e.to_string())));
 
@@ -26,7 +28,29 @@ impl Guest for Host {
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn evaluate() -> Result<f32> {
+fn to_params(request: &IncomingRequest) -> Result<(f32, f32)> {
+    let path_query = request.path_with_query().ok_or("failed path")?;
+    let (_, query) = path_query.split_once('?').ok_or("no querystring")?;
+
+    let params = query
+        .split('&')
+        .filter_map(|x| x.split_once('='))
+        .map(|(k, v)| (k.to_string(), v.to_string()))
+        .collect::<HashMap<String, String>>();
+
+    let a = extract_to(&params, "a")?;
+    let b = extract_to(&params, "b")?;
+
+    Ok((a, b))
+}
+
+fn extract_to(params: &HashMap<String, String>, key: &str) -> Result<f32> {
+    let v = params.get(key).ok_or(format!("no params: {}", key))?;
+
+    v.parse::<f32>().map_err(|e| e.into())
+}
+
+fn evaluate(a: f32, b: f32) -> Result<f32> {
     let model = fs::read("model/sample.onnx")?;
 
     let graph = load(&[model], GraphEncoding::Onnx, ExecutionTarget::Cpu)
@@ -36,7 +60,7 @@ fn evaluate() -> Result<f32> {
         .init_execution_context()
         .map_err(|e| format!("{:?}", e))?;
 
-    let data = [1.0f32.to_le_bytes(), 2.0f32.to_le_bytes()].concat();
+    let data = [a.to_le_bytes(), b.to_le_bytes()].concat();
 
     let tensor = Tensor::new(&[1, 2], TensorType::Fp32, &data);
 
