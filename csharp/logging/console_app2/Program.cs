e839741d9d3ceb5963cff83b56d2c07b4a3f6bbc
woz <woz@example.com>
2025-05-27 18:07:18 +0900

    record or class logging

---
+++ csharp/logging/console_app2/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+using ILoggerFactory factory = LoggerFactory.Create(b =>
+{
+    b.AddConsole();
+});
+
+var logger = factory.CreateLogger("logging test");
+
+var d1 = new Data("test1", 123);
+
+logger.LogInformation("a. d1={d}", d1);
+logger.LogInformation($"b. d1={d1}");
+Console.WriteLine("c. d1={0}", d1);
+
+var d2 = new Data2("test2", 456);
+
+logger.LogInformation("a. d2={d}", d2);
+logger.LogInformation($"b. d2={d2}");
+Console.WriteLine("c. d2={0}", d2);
+
+record Data(string id, int value);
+
+public class Data2(string id, int value)
+{
+    public String Id => id;
+    public int Value => value;
+}
