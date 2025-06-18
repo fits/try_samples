47881374f3c3cd361fd93052b741758b2aafa68e
woz <woz@example.com>
2025-06-18 12:37:24 +0900

    JsonSerializer.Deserialize test

---
+++ csharp/json/from_json_test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.ComponentModel.DataAnnotations;
+
+var s = """
+{
+    "Id": "A01",
+    "Date": "2025-07-01T10:00:00+09:00"
+}
+""";
+
+var a = JsonSerializer.Deserialize<A>(s);
+Console.WriteLine($"{a}");
+
+var b = JsonSerializer.Deserialize<B>(s);
+b?.Show();
+
+try
+{
+    var c = JsonSerializer.Deserialize<C>(s);
+    c?.Show();
+} catch(Exception ex)
+{
+    Console.WriteLine($"Deserialize Failed : {ex}");
+}
+
+record A(string Code, int Value);
+
+class B
+{
+    [Required]
+    public string Code { get; set; } = string.Empty;
+
+    public int Value { get; set; }
+
+    public void Show()
+    {
+        Console.WriteLine($"code={Code}, value={Value}");
+    }
+}
+
+class C
+{
+    public required string Code { get; set; } = string.Empty;
+
+    public int Value { get; set; }
+
+    public void Show()
+    {
+        Console.WriteLine($"code={Code}, value={Value}");
+    }
+}
