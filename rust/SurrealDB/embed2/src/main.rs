66fe357a89c0a550b2fbdfccbd8396df45089d04
woz <woz@example.com>
2025-06-28 12:10:17 +0900

    query test

---
+++ rust/SurrealDB/embed2/src/main.rs
@@ -0,0 +1,47 @@
+use surrealdb::Surreal;
+use surrealdb::engine::local::Mem;
+use surrealdb::sql::Thing;
+
+use serde::Deserialize;
+
+use serde_json::Value;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Document {
+    id: Option<Thing>,
+}
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let file = "./data/items.jsonl";
+
+    let db = Surreal::new::<Mem>(()).await?;
+    db.use_ns("test1").use_db("db").await?;
+
+    for line in BufReader::new(File::open(file)?).lines() {
+        if let Ok(s) = line {
+            let v: Value = serde_json::from_str(&s)?;
+            let r: Option<Document> = db.create("items").content(v).await?;
+
+            println!("* registerd: {:?}", r);
+        }
+    }
+
+    let q1 = "SELECT * FROM items WHERE id < items:4";
+    let q2 = "SELECT * FROM items WHERE attrs.category = 'A1'";
+    let q3 = "SELECT * FROM items WHERE variants[WHERE color = 'white']";
+
+    let mut res = db.query(q1).query(q2).query(q3).await?;
+
+    println!("* q1 = {:?}", res.take::<Vec<Document>>(0)?);
+    println!("* q2 = {:?}", res.take::<Vec<Document>>(1)?);
+    println!("* q3 = {:?}", res.take::<Vec<Document>>(2)?);
+
+    Ok(())
+}
