8c61fd3538d74bda545677406a361fa41ed5c128
woz <woz@example.com>
2025-09-05 17:36:24 +0900

    validate token jwk

---
+++ csharp/JWT/validate_token_jwk/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+var jwkFile = args[0];
+var token = args[1];
+
+var jwk = File.ReadAllText(jwkFile);
+
+var opts = new TokenValidationParameters
+{
+    ValidateAudience = false,
+    ValidateIssuer = false,
+    ValidateLifetime = false,
+    IssuerSigningKey = new JsonWebKey(jwk)
+};
+
+var handler = new JwtSecurityTokenHandler();
+
+handler.ValidateToken(token, opts, out SecurityToken validatedToken);
+
+Console.WriteLine($"valid token = {validatedToken}");
