492dc2bd324c12698bf425247c6ea797f13dde08
woz <woz@example.com>
2025-09-05 09:09:33 +0900

    ValidateToken

---
+++ csharp/JWT/validate_token/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+var token = args[0];
+
+var opts = new TokenValidationParameters
+{
+    SignatureValidator = (string t, TokenValidationParameters p) => new JwtSecurityToken(t),
+    ValidateAudience = false,
+    ValidateIssuer = false,
+    ValidateLifetime = false,
+};
+
+var handler = new JwtSecurityTokenHandler();
+
+handler.ValidateToken(token, opts, out SecurityToken validatedToken);
+
+Console.WriteLine($"valid token = {validatedToken}");
