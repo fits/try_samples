224708b3e3c535015c419c004b37f27d0b1a0c83
woz <woz@example.com>
2025-09-05 15:52:53 +0900

    use pem keyfile

---
+++ csharp/JWT/validate_token2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+var keyFile = args[0];
+var token = args[1];
+
+var key = File.ReadAllText(keyFile);
+
+using var rsa = new RSACryptoServiceProvider();
+rsa.ImportFromPem(key);
+
+var opts = new TokenValidationParameters
+{
+    ValidateAudience = false,
+    ValidateIssuer = false,
+    ValidateLifetime = false,
+    IssuerSigningKey = new RsaSecurityKey(rsa)
+};
+
+var handler = new JwtSecurityTokenHandler();
+
+handler.ValidateToken(token, opts, out SecurityToken validatedToken);
+
+Console.WriteLine($"valid token = {validatedToken}");
