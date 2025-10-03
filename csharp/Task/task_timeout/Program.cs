9f03152ede8149cdd271222e667123eff51bc723
woz <woz@example.com>
2025-10-03 10:58:39 +0900

    Task timeout

---
+++ csharp/Task/task_timeout/Program.cs
@@ -0,0 +1,26 @@
+﻿await TimeoutTest();
+
+async Task TimeoutTest()
+{
+    try
+    {
+        await Process(1000).WaitAsync(TimeSpan.FromSeconds(2));
+
+        Console.WriteLine("-----");
+
+        await Process(3000).WaitAsync(TimeSpan.FromSeconds(2));
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+async Task Process(int workTime)
+{
+    Console.WriteLine($"start process: {workTime}");
+
+    await Task.Delay(workTime);
+
+    Console.WriteLine($"end process: {workTime}");
+}
