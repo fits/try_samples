73d6dd8b8c64edc4b352f339333b09df4e24db28
woz <woz@example.com>
2026-03-08 10:45:28 +0900

    add empty interface

---
+++ csharp/Fody/simple/SimpleWeavers/InterfaceWeaver.cs
@@ -0,0 +1,20 @@
+﻿using Fody;
+using Mono.Cecil;
+
+namespace SimpleWeavers;
+
+public class InterfaceWeaver : BaseModuleWeaver
+{
+    public override void Execute()
+    {
+        WriteInfo($"weaver execute start: assembly={AssemblyFilePath}");
+
+        var type = new TypeDefinition("WeavingTest", "ITest", TypeAttributes.Public | TypeAttributes.Interface | TypeAttributes.Abstract);
+
+        ModuleDefinition.Types.Add(type);
+
+        WriteInfo("weaver execute end");
+    }
+
+    public override IEnumerable<string> GetAssembliesForScanning() => [];
+}
---
+++ csharp/Fody/simple/TestProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+var svc = new ItemService();
+var item = svc.Create("item-1");
+
+Console.WriteLine($"result={item}");
+
+Console.WriteLine("-----");
+
+var asm = Assembly.GetExecutingAssembly();
+
+Console.WriteLine($"assembly={asm}");
+
+foreach (var t in asm.GetTypes())
+{
+    Console.WriteLine($"type={t.FullName}");
+}
+
+
+public record Item(String Name);
+
+public interface IItemService
+{
+    Item Create(String name);
+}
+
+public class ItemService : IItemService
+{
+    public Item Create(String name)
+    {
+        return new Item(name);
+    }
+}
