92fb883bad2439544e220630f35c76d70c75fafc
woz <woz@example.com>
2026-03-08 21:41:17 +0900

    dump type, method, instruction

---
+++ csharp/Mono.Cecil/DumpInstruction/Program.cs
@@ -0,0 +1,18 @@
+﻿using Mono.Cecil;
+
+var assm = AssemblyDefinition.ReadAssembly(args[0]);
+
+foreach (var t in assm.MainModule.Types)
+{
+    Console.WriteLine($"----- type={t.FullName} -----");
+
+    foreach (var m in t.Methods)
+    {
+        Console.WriteLine($"method={m.Name}, {m.FullName}");
+
+        foreach (var inst in m.Body.Instructions)
+        {
+            Console.WriteLine($"  instruction={inst}");
+        }
+    }
+}
