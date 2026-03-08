954120cd461cc407acf97c82ef90c9880f7ba186
woz <woz@example.com>
2026-03-08 21:58:10 +0900

    method's last arg replacement

---
+++ csharp/Fody/method_args_replace/MethodWeavers/CallerTypeWeaver.cs
@@ -0,0 +1,39 @@
+﻿using Fody;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace MethodWeavers;
+
+public class CallerTypeWeaver : BaseModuleWeaver
+{
+    public override void Execute()
+    {
+        var ts = ModuleDefinition.GetTypes().Where(x => x.BaseType != null && !x.IsEnum && !x.IsInterface);
+
+        foreach (var t in ts)
+        {
+            foreach (var m in t.Methods)
+            {
+                var ilProc = m.Body.GetILProcessor();
+
+                foreach (var inst in m.Body.Instructions)
+                {
+                    if (inst.OpCode == OpCodes.Call && inst.Operand is MethodReference mr )
+                    {
+                        if (mr.DeclaringType.FullName == "TestProject.LogUtil" && mr.Name == "Info")
+                        {
+                            var prevInst = inst.Previous;
+
+                            if (prevInst.OpCode == OpCodes.Ldstr)
+                            {
+                                prevInst.Operand = t.FullName;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public override IEnumerable<string> GetAssembliesForScanning() => [];
+}
---
+++ csharp/Fody/method_args_replace/TestProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using TestProject;
+
+var svc = new Service();
+svc.Create("test-1");
+
+LogUtil.Info("main process");
+
+namespace TestProject
+{
+    public class LogUtil
+    {
+        public static void Info(string msg, string callerType = "")
+        {
+            Console.WriteLine($"message='{msg}', caller-type='{callerType}'");
+        }
+    }
+
+    public class BaseService
+    {
+        protected void Before()
+        {
+            LogUtil.Info("called before");
+        }
+    }
+
+    public class Service : BaseService
+    {
+        public bool Create(String name)
+        {
+            base.Before();
+
+            LogUtil.Info($"created: {name}");
+
+            return true;
+        }
+    }
+}
