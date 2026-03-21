1c3460dab3e1f14510b95e5078669ad55933189f
woz <woz@example.com>
2026-03-21 09:44:00 +0900

    blog sample

---
+++ blog/20260321/fody_callertype/caller_test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+var svc = new ItemService();
+svc.Create("check1");
+
+var traceInfo = (string msg) => TraceLogger.Info(msg);
+traceInfo("check2");
+
+public static class TraceLogger
+{
+    public static void Info(string msg,
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string filePath = "",
+        [CallerLineNumber] int lineNumber = 0)
+    {
+        Console.WriteLine($"message='{msg}', method='{memberName}', file='{Path.GetFileName(filePath)}', lineNo={lineNumber}");
+    }
+}
+
+public class ItemService
+{
+    public bool Create(string id)
+    {
+        TraceLogger.Info($"Create:{id}");
+        return true;
+    }
+}
---
+++ blog/20260321/fody_callertype/caller_type/SampleProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using SampleProject;
+using CustomAttributes;
+
+var svc = new Service();
+svc.Create("check1");
+
+TraceLogger.Info("check2");
+
+var traceInfo = (string msg) => TraceLogger.Info(msg);
+traceInfo("check3");
+
+TraceLogger.Info2("check4");
+
+
+namespace SampleProject
+{
+    public class TraceLogger
+    {
+        public static void Info(string msg, [CallerType] string callerType = "")
+        {
+            Console.WriteLine($"message='{msg}', caller-type='{callerType}'");
+        }
+
+        public static void Info2(string msg, [CallerType] string callerType = "", string opt = "")
+        {
+            Console.WriteLine($"message='{msg}', caller-type='{callerType}', opt='{opt}'");
+        }
+    }
+
+    public class BaseService
+    {
+        protected void Before(string id)
+        {
+            TraceLogger.Info($"Before:{id}");
+        }
+    }
+
+    public class Service : BaseService
+    {
+        public bool Create(String id)
+        {
+            base.Before(id);
+
+            TraceLogger.Info($"Create:{id}");
+
+            new InnerService().After(id);
+
+            return true;
+        }
+
+        class InnerService
+        {
+            public void After(string id)
+            {
+                TraceLogger.Info($"After:{id}");
+            }
+        }
+    }
+}
