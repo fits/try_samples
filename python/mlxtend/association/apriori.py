e2f09e61cca9593645cc1e012897bf00de94698a
woz <woz@example.com>
2026-04-14 14:40:04 +0900

    association rules

---
+++ python/mlxtend/association/apriori.py
@@ -0,0 +1,24 @@
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+import pandas as pd
+import sys
+
+data_file = sys.argv[1]
+min_support = float(sys.argv[2])
+min_threshold = float(sys.argv[3])
+
+with open(data_file) as f:
+    dataset = [line.strip().split(',') for line in f]
+
+te = TransactionEncoder()
+
+te_a = te.fit(dataset).transform(dataset)
+
+df = pd.DataFrame(te_a, columns=te.columns_)
+
+freq = apriori(df, min_support=min_support, use_colnames=True)
+
+rules = association_rules(freq, metric='lift', min_threshold=min_threshold)
+
+print( rules[['antecedents', 'consequents', 'lift', 'confidence', 'support']] )
---
+++ python/mlxtend/association/topn_comb.js
@@ -0,0 +1,39 @@
+import * as R from 'https://deno.land/x/ramda/mod.ts'
+import { TextLineStream } from 'jsr:@std/streams/text-line-stream'
+
+const n = Deno.args[0]
+
+const powerSet = R.reduce(
+    (acc, v) => R.chain(R.concat, R.map(R.append(v)))(acc),
+    [[]],
+)
+
+const comb = R.pipe(
+    R.sort(R.ascend(R.identity)),
+    powerSet,
+    R.filter(x => x.length > 0),
+)
+
+const parseCsv = R.pipe(
+    R.split(','),
+    R.map(R.trim),
+)
+
+const topN = n => R.pipe(
+    R.countBy(R.join(',')),
+    R.toPairs,
+    R.sort(R.descend(R.nth(1))),
+    R.take(n)
+)
+
+const stream = Deno.stdin.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream())
+
+let data = []
+
+for await (const line of stream) {
+    data = R.concat(data, comb(parseCsv(line)))
+}
+
+console.log(topN(n)(data))
